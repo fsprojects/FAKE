54461bf669e54d0c56f8d5ba8a1278d785c7ea76
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-03-14 15:11:17 +0100

    Putting template functions into own module.

--- src/app/FakeLib/HTMLHelpWorkShopHelper.fs
+++ src/app/FakeLib/HTMLHelpWorkShopHelper.fs
@@ -20,28 +20,4 @@ let CompileHTMLHelpProject helpCompiler projectFile =
   let name = fi.Name.Split('.').[0]
   traceEndTask "HTMLHelpWorkshop" projectFile
   [ sprintf "%s\\%s.chm" fi.Directory.FullName name;
-    sprintf "%s\\%s.hh" fi.Directory.FullName name]      
-    
-/// Loads all templates (lazy - line by line!)    
-let loadTemplates seq =
-  seq |> Seq.map (fun fileName -> fileName,ReadFile fileName)
-
-/// replaces a bunch of the keywords in all files (lazy - line by line!)
-let replaceKeywords m seq =
-  seq |> Seq.map (fun (fileName,file) -> 
-     fileName, 
-       file 
-         |> Seq.map (fun (line:string) -> 
-              m |> Seq.fold (fun (acc:string) (k:string,r:string) -> acc.Replace(k,r)) line))
-
-  
-/// saves all files (lazy - file by file!)
-let saveFiles seq =
-  seq |> Seq.iter (fun (fileName,file) -> WriteFile fileName (file |> Seq.toList))
-  
-
-let processTemplates replacements files =
-  files
-    |> loadTemplates
-    |> replaceKeywords replacements
-    |> saveFiles
+    sprintf "%s\\%s.hh" fi.Directory.FullName name]         
---
+++ src/app/FakeLib/TemplateHelper.fs
@@ -0,0 +1,26 @@
+﻿[<AutoOpen>]
+module Fake.TemplateHelper
+
+/// Loads all templates (lazy - line by line!)    
+let loadTemplates seq =
+  seq |> Seq.map (fun fileName -> fileName,ReadFile fileName)
+
+/// replaces a bunch of the keywords in all files (lazy - line by line!)
+let replaceKeywords m seq =
+  seq |> Seq.map (fun (fileName,file) -> 
+     fileName, 
+       file 
+         |> Seq.map (fun (line:string) -> 
+              m |> Seq.fold (fun (acc:string) (k:string,r:string) -> acc.Replace(k,r)) line))
+  
+/// saves all files (lazy - file by file!)
+let saveFiles seq =
+  seq |> Seq.iter (fun (fileName,file) -> WriteFile fileName (file |> Seq.toList))
+  
+
+/// Replaces the templates with the given replacements
+let processTemplates replacements files =
+  files
+    |> loadTemplates
+    |> replaceKeywords replacements
+    |> saveFiles
