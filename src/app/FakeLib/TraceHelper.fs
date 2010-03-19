2ed1ff5131a264ec63986d515f31b9b2a3b91b81
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-03-19 09:53:48 +0100

    Adding tracefn and logfn functions.

--- src/app/FakeLib/TraceHelper.fs
+++ src/app/FakeLib/TraceHelper.fs
@@ -31,55 +31,72 @@ let fakePath = productName.GetType().Assembly.Location
 let fakeVersion = productName.GetType().Assembly.GetName().Version 
 
 /// logs the specified string to the console
-let logMessageToConsole important s =     
+let logMessageToConsole important newLine s =     
   if important && buildServer <> CCNet then
-    eprintfn "%s" (toRelativePath s)  
+    if newLine then
+      eprintfn "%s" (toRelativePath s)
+    else 
+      eprintf "%s" (toRelativePath s)
   else
-    printfn "%s" (toRelativePath s)
-    
-let xmlMessage s =
-  sprintf "<message level=\"Info\"><![CDATA[%s]]></message>" s |> appendXML
+    if newLine then
+      printfn "%s" (toRelativePath s)
+    else 
+      printf "%s" (toRelativePath s)
+
+let xmlMessage message =
+    message
+      |> sprintf "<message level=\"Info\"><![CDATA[%s]]></message>"
+      |> appendXML
     
 /// logs the specified string        
-let logMessage important s =
+let logMessage important newLine message =
     match traceMode with
-    | Console -> logMessageToConsole important s
-    | Xml     -> xmlMessage s
+    | Console -> logMessageToConsole important newLine message
+    | Xml     -> xmlMessage message
 
 /// Logs the specified string        
-let log = logMessage false
+let log = logMessage false true
+
+/// Logs the specified message
+let logfn fmt = Printf.ksprintf log fmt
+
+/// Logs the specified message (without line break)
+let logf fmt = Printf.ksprintf (logMessage false false) fmt
 
 /// Logs the specified string if the verbose mode is activated.
 let logVerbose s = if verbose then log s
 
 /// Writes a trace to the command line (in the given color)
-let logColored important s color =
+let logColored important color newLine message =
   Console.ForegroundColor <- color    
-  logMessage important s
+  logMessage important newLine message
   Console.ForegroundColor <- ConsoleColor.White     
     
 /// Writes a trace to the command line (in green)
-let trace s = 
-  logColored false s ConsoleColor.Green    
+let trace s = logColored false ConsoleColor.Green true s
+
+/// Writes a message to the command line (in green)
+let tracefn fmt = Printf.ksprintf trace fmt
+
+/// Writes a message to the command line (in green) and without a line break
+let tracef fmt = Printf.ksprintf (logColored false ConsoleColor.Green false) fmt
 
 /// Writes a trace to the command line (in green) if the verbose mode is activated.
 let traceVerbose s = if verbose then trace s
 
 /// Writes a trace to stderr (in green)  
-let traceImportant s = 
-  logColored true s ConsoleColor.Green    
+let traceImportant s = logColored true ConsoleColor.Green true s
   
 /// Writes a trace to the command line (in yellow)
-let traceFAKE s = 
-  logColored true s ConsoleColor.Yellow
+let traceFAKE fmt = Printf.ksprintf (logColored true ConsoleColor.Yellow true) fmt
 
 /// Traces an error (in red)
-let traceError s = 
+let traceError error = 
   match traceMode with
-  | Console -> logColored true s ConsoleColor.Red 
+  | Console -> logColored true ConsoleColor.Red true error
   | Xml     ->  
      appendXML "<failure><builderror>"
-     xmlMessage s
+     xmlMessage error
      appendXML "</builderror></failure>"
   
 
@@ -89,11 +106,13 @@ let TraceEnvironmentVariables() =
     EnvironTarget.Process;
     EnvironTarget.User]
     |> List.iter (fun mode ->    
-        traceFAKE (sprintf "Environment-Settings (%A):" mode)
-        environVars mode |> List.iter (sprintf "  %A" >> trace))        
+        traceFAKE "Environment-Settings (%A):" mode
+        environVars mode |> List.iter (tracefn "  %A"))        
  
 /// Gets the FAKE Version string
-let fakeVersionStr = sprintf "FAKE - F# Make - Version %s" (fakeVersion.ToString())
+let fakeVersionStr = 
+    fakeVersion.ToString()
+      |> sprintf "FAKE - F# Make - Version %s" 
 
 /// Traces the begin of the build
 let traceStartBuild _ =
@@ -128,7 +147,7 @@ let closeAllOpenTags() = openTags |> Seq.iter closeTag
 let traceStartTarget name dependencyString =
   match traceMode with
   | Console -> 
-      trace <| sprintf "Starting Target: %s %s" name dependencyString
+      tracefn "Starting Target: %s %s" name dependencyString
   | Xml     -> 
       openTag "target"
       appendXML <| sprintf "<target name=\"%s\">" name
@@ -139,7 +158,7 @@ let traceStartTarget name dependencyString =
 /// Traces the end of a target   
 let traceEndTarget name =
   match traceMode with
-  | Console -> sprintf "Finished Target: %s" name |> trace
+  | Console -> tracefn "Finished Target: %s" name
   | Xml     -> closeTag "target"
   ReportProgressFinish <| sprintf "Target: %s" name       
   
@@ -158,7 +177,4 @@ let traceEndTask task  description =
   match traceMode with
   | Console -> ()
   | Xml     -> closeTag "task" 
-  ReportProgressFinish <| sprintf "Task: %s %s" task description
-   
-    
-       
+  ReportProgressFinish <| sprintf "Task: %s %s" task description       
