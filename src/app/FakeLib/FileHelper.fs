3ff44dc430ba7f61edf9625a52469416df4e431d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-07 12:37:56 +0200

    Revert "Fixing issue with FullName."
    This reverts commit b77de80ff8616f72b0559f6b3b95c274b4db492e.

--- src/app/FakeLib/FileHelper.fs
+++ src/app/FakeLib/FileHelper.fs
@@ -236,7 +236,7 @@ let FilesAreEqual (first:FileInfo) (second:FileInfo) =
     eq
 
 /// Converts a file to it's full file system name
-let FullName fileName = (new FileInfo(fileName)).FullName
+let FullName fileName = Path.GetFullPath fileName
   
 /// Compares the given files for changes
 /// If delete = true then equal files will be removed  
