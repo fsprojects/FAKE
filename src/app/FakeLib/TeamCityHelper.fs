8ceaa091ecad34d89762d1e247e41e618717cc78
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-16 10:02:05 +0200

    Putting TeamCity REST API into own module.

--- src/app/FakeLib/TeamCityHelper.fs
+++ src/app/FakeLib/TeamCityHelper.fs
@@ -50,21 +50,28 @@ let IgnoreTestCase name message =
   
 /// Finishes the test suite.
 let FinishTestSuite testSuiteName =
-  sendToTeamCity "##teamcity[testSuiteFinished name='%s']" (EncapsulateApostrophe testSuiteName)
+    EncapsulateApostrophe testSuiteName
+      |> sendToTeamCity "##teamcity[testSuiteFinished name='%s']" 
 
 /// Starts the test suite.
 let StartTestSuite testSuiteName =
-  sendToTeamCity "##teamcity[testSuiteStarted name='%s']" (EncapsulateApostrophe testSuiteName)
+    EncapsulateApostrophe testSuiteName
+      |> sendToTeamCity "##teamcity[testSuiteStarted name='%s']"
 
 /// Reports the progress.
 let ReportProgress message =
-  sendToTeamCity "##teamcity[progressMessage '%s']" (EncapsulateApostrophe message)
+    EncapsulateApostrophe message
+      |> sendToTeamCity "##teamcity[progressMessage '%s']"
 
+/// Reports the progress start.
 let ReportProgressStart message =
-  sendToTeamCity "##teamcity[progressStart '%s']" (EncapsulateApostrophe message)
+    EncapsulateApostrophe message
+      |> sendToTeamCity "##teamcity[progressStart '%s']"
 
+/// Reports the progress end.
 let ReportProgressFinish message =
-  sendToTeamCity "##teamcity[progressFinish '%s']" (EncapsulateApostrophe message)
+    EncapsulateApostrophe message
+      |> sendToTeamCity "##teamcity[progressFinish '%s']"
 
 /// Tests the failed.
 let TestFailed name message details =  
@@ -84,87 +91,7 @@ let ComparisonFailure name message details expected actual =
     (EncapsulateApostrophe actual)
     |> sendStrToTeamCity 
        
-let showRecentlyFailedTests() =
-    let s = System.Configuration.ConfigurationManager.AppSettings.["teamcity.tests.recentlyFailedTests.file"]    
-    ReadFile s
-      |> Seq.iter (printfn "%s")      
-
-let prepareURL restURL (serverURL:string) = 
-  serverURL.Trim('/') + restURL
-
-/// Returns the REST version of the TeamCity server
-let getRESTVersion serverURL username password =      
- serverURL
-   |> prepareURL "/httpAuth/app/rest/version"
-   |> REST.ExecuteGetCommand username password 
-
-type VCSRoot =
- {URL: string;
-  Properties: Map<string,string>;
-  VCSName: string;
-  Name: string}
-
-type Build =
-  { ID:string; 
-    Number:string;
-    Status:string;
-    WebURL:string }
-
-type BuildConfiguration =
-  { ID: string;
-    Name: string; 
-    WebURL: string;
-    ProjectID: string; 
-    Paused: bool;
-    Description: string;
-    Builds: Build seq}
-
-let getFirstNode serverURL username password url =
-    serverURL
-      |> prepareURL url
-      |> REST.ExecuteGetCommand username password 
-      |> XMLDoc
-      |> DocElement
- 
-/// Gets a projects from the TeamCity server
-let getBuildConfig serverURL username password id =
-  id 
-    |> sprintf "/httpAuth/app/rest/buildTypes/id:%s"
-    |> getFirstNode serverURL username password 
-    |> parse "buildType" (fun n ->             
-          {ID = getAttribute "id" n;
-           Name = getAttribute "name" n;
-           Description = getAttribute "description" n;
-           WebURL = getAttribute "webUrl" n;
-           Paused = getAttribute "paused" n |> System.Boolean.Parse;
-           ProjectID = parseSubNode "project" (getAttribute "id") n;
-           Builds =[]})
-
-type Project = 
-  {ID: string;
-   Name: string;
-   Description: string;
-   WebURL: string;
-   Archived: bool;
-   BuildConfigs: string seq}
-
-/// Gets a projects from the TeamCity server
-let getProject serverURL username password id =
-  id 
-    |> sprintf "/httpAuth/app/rest/projects/id:%s"
-    |> getFirstNode serverURL username password 
-    |> parse "project" (fun n ->             
-          {ID = getAttribute "id" n;
-           Name = getAttribute "name" n;
-           Description = getAttribute "description" n;
-           WebURL = getAttribute "webUrl" n;
-           Archived = getAttribute "archived" n |> System.Boolean.Parse;
-           BuildConfigs = 
-             parseSubNode "buildTypes" getChilds n
-               |> Seq.map  (getAttribute "id") })
-
-/// Gets all projects on the TeamCity 
-let getProjects serverURL username password =  
-  getFirstNode serverURL username password "/httpAuth/app/rest/projects"  
-    |> parse "projects" getChilds
-    |> Seq.map (getAttribute "id")
+/// Gets the recently failed tests
+let getRecentlyFailedTests() =
+    System.Configuration.ConfigurationManager.AppSettings.["teamcity.tests.recentlyFailedTests.file"]    
+      |> ReadFile
---
+++ src/app/FakeLib/TeamCityRESTHelper.fs
@@ -0,0 +1,79 @@
+﻿[<AutoOpen>]
+module Fake.TeamCityRESTHelper
+
+let prepareURL restURL (serverURL:string) = serverURL.Trim '/' + restURL
+
+/// Returns the REST version of the TeamCity server
+let getRESTVersion serverURL username password =      
+   serverURL
+     |> prepareURL "/httpAuth/app/rest/version"
+     |> REST.ExecuteGetCommand username password 
+
+type VCSRoot =
+    { URL: string;
+      Properties: Map<string,string>;
+      VCSName: string;
+      Name: string}
+
+type Build =
+    { ID:string; 
+      Number:string;
+      Status:string;
+      WebURL:string }
+
+type BuildConfiguration =
+    { ID: string;
+      Name: string; 
+      WebURL: string;
+      ProjectID: string; 
+      Paused: bool;
+      Description: string;
+      Builds: Build seq }
+
+let getFirstNode serverURL username password url =
+      serverURL
+        |> prepareURL url
+        |> REST.ExecuteGetCommand username password 
+        |> XMLDoc
+        |> DocElement
+ 
+/// Gets a projects from the TeamCity server
+let getBuildConfig serverURL username password id =
+    sprintf "/httpAuth/app/rest/buildTypes/id:%s" id
+      |> getFirstNode serverURL username password 
+      |> parse "buildType" (fun n ->             
+            { ID = getAttribute "id" n;
+              Name = getAttribute "name" n;
+              Description = getAttribute "description" n;
+              WebURL = getAttribute "webUrl" n;
+              Paused = getAttribute "paused" n |> System.Boolean.Parse;
+              ProjectID = parseSubNode "project" (getAttribute "id") n;
+              Builds =[] })
+
+type Project =
+    { ID: string;
+      Name: string;
+      Description: string;
+      WebURL: string;
+      Archived: bool;
+      BuildConfigs: string seq}
+
+/// Gets a projects from the TeamCity server
+let getProject serverURL username password id =
+    sprintf "/httpAuth/app/rest/projects/id:%s" id
+      |> getFirstNode serverURL username password 
+      |> parse "project" (fun n ->             
+             { ID = getAttribute "id" n;
+               Name = getAttribute "name" n;
+               Description = getAttribute "description" n;
+               WebURL = getAttribute "webUrl" n;
+               Archived = getAttribute "archived" n |> System.Boolean.Parse;
+               BuildConfigs = 
+                   parseSubNode "buildTypes" getChilds n
+                     |> Seq.map (getAttribute "id") })
+
+/// Gets all projects on the TeamCity 
+let getProjects serverURL username password =  
+    getFirstNode serverURL username password "/httpAuth/app/rest/projects"  
+      |> parse "projects" getChilds
+      |> Seq.map (getAttribute "id")
