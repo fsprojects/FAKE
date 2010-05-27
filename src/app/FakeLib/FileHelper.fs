ce36866aadb760050ce5406af8734dee3930116a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-27 14:30:08 +0200

    Cleanup.

--- src/app/FakeLib/FileHelper.fs
+++ src/app/FakeLib/FileHelper.fs
@@ -219,7 +219,7 @@ let ReadCSVFile(file:string) =
 let AppendTextFiles newFileName files =    
     let fi = fileInfo newFileName
     if fi.Exists then failwithf "File %s already exists." (fi.FullName)
-    use writer = new StreamWriter(fi.FullName, false, Encoding.Default);
+    use writer = new StreamWriter(fi.FullName, false, Encoding.Default)
   
     files 
       |> Seq.iter (fun file ->       
