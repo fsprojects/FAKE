793ee9241e2b2dc948c5402ba03ca1899c9b9ad2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-05-27 13:54:09 +0200

    Mailbox cleanup.

--- src/app/FakeLib/StringHelper.fs
+++ src/app/FakeLib/StringHelper.fs
@@ -20,7 +20,7 @@ let ReadFile (file:string) =
 let WriteToFile append fileName (lines: seq<string>) =    
     let fi = fileInfo fileName
 
-    use writer =  new StreamWriter(fileName,append && fi.Exists,Encoding.Default) 
+    use writer = new StreamWriter(fileName,append && fi.Exists,Encoding.Default) 
     lines |> Seq.iter writer.WriteLine
 
 /// Writes string to a file
