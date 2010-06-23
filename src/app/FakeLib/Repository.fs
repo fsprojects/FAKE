54487ab51075cc13137e93e8ed487dd10cd3b510
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-06-23 11:16:28 +0200

    Adding Git submodule support.

---
+++ src/app/FakeLib/Repository.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+module Fake.Git.Repository
+
+let init bare shared =
+    match bare, shared with
+    | true, true -> CommandHelper.gitCommand "init --bare --shared=all"
+    | true, false -> CommandHelper.gitCommand "init --bare"
+    | false, true -> CommandHelper.gitCommand "init --shared=all"
+    | _ -> CommandHelper.gitCommand "init"
