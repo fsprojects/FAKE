f62beea82a7b006fa8c850a6017eab7cc147984d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-06-23 11:53:20 +0200

    Adding getMergeMessage()

---
+++ src/app/FakeLib/Git/Merge.fs
@@ -0,0 +1,11 @@
+﻿[<AutoOpen>]
+module Fake.Git.Merge
+
+open Fake
+
+let getMergeMessage() =
+    let file = CommandHelper.getWorkingDirGitDir() + "\\MERGE_MSG";
+    if System.IO.File.Exists file then
+        System.IO.File.ReadAllText file
+    else
+        ""
