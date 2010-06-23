186312266b5e5eaf368b193c33976600bcb9a5ed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-06-23 11:55:55 +0200

    Adding sanity checks for git.

---
+++ src/app/FakeLib/Git/SanityChecks.fs
@@ -0,0 +1,36 @@
+﻿[<AutoOpen>]
+module Fake.Git.SanityChecks
+
+open Fake
+
+let checkRevisionExists revision1 =
+  let ok1,_,errors1 = CommandHelper.runGitCommand <| sprintf "log %s" revision1
+  
+  if not ok1 || errors1 <> "" then
+      failwithf "Revision %s is not found in the current repository." revision1
+
+/// Checks if the file exists on disk.
+let checkFileExists fileName =
+    let fi = new System.IO.FileInfo(fileName)
+    if not fi.Exists then
+        failwithf "File %s does not exist." fileName
+
+/// Checks if the given branch exists.
+let checkIfBranchExists branch = 
+    if not (getAllBranches() |> List.exists ((=) branch)) then
+        failwithf "Branch %s doesn't exists." branch
+
+/// Checks if the given branch is absent.
+let checkIfBranchIsAbsent branch = 
+    if getAllBranches() |> List.exists ((=) branch) then
+        failwithf "Branch %s exists but should be absent." branch
+
+/// Checks if the given branch is a local branch.
+let checkIsLocalBranch branch = 
+    if not (getLocalBranches() |> List.exists ((=) branch)) then
+        failwithf "Branch %s doesn't exists locally." branch
+
+/// Checks if the given branch is a remote branch.
+let checkIsRemoteBranch branch = 
+    if not (getRemoteBranches() |> List.exists ((=) branch)) then
+        failwithf "Branch %s doesn't exists remotely." branch
