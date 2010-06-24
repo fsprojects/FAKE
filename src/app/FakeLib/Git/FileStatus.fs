3d6bec11b799618488bd33538811a559fff43874
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-06-24 09:02:46 +0200

    Cleanup

--- src/app/FakeLib/Git/FileStatus.fs
+++ src/app/FakeLib/Git/FileStatus.fs
@@ -9,7 +9,7 @@ type FileStatus =
 | Modified
 | Deleted
   with 
-    static member Get s = 
+    static member parse s = 
       match s with
       | "A" -> Added
       | "M" -> Modified
@@ -17,23 +17,23 @@ type FileStatus =
       | _ -> failwith <| sprintf "Unknown file status %s" s
  
 /// Gets the changed files between the given revisions
-let getChangedFiles revision1 revision2 =
+let getChangedFiles revision1 revision2 =    
     checkRevisionExists revision1
     if revision2 <> "" then
         checkRevisionExists revision2
 
-    let ok,msg,errors = runGitCommand <| sprintf "diff %s %s --name-status" revision1 revision2
+    let _,msg,_ = runGitCommand <| sprintf "diff %s %s --name-status" revision1 revision2
     msg
       |> Seq.map (fun line -> 
             let a = line.Split('\t')
-            FileStatus.Get a.[0],a.[1])
+            FileStatus.parse a.[0],a.[1])
 
 /// Gets the changed files since the given revision incl. changes in the working copy
 let getChangedFilesInWorkingCopy revision = getChangedFiles revision ""
 
 /// Gets all conflicted files
 let getConflictedFiles() =
-    let ok,msg,errors = runGitCommand "ls-files --unmerged"
+    let _,msg,_ = runGitCommand "ls-files --unmerged"
     msg 
       |> Seq.map (fun file -> file.LastIndexOfAny([| ' '; '\t'|]),file)
       |> Seq.filter (fun (index,file) -> index > 0)
