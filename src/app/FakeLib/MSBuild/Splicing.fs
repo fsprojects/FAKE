64694e3cd910a18526e41a46f1d25be31cdc258d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-03 12:27:44 +0200

    Splicing of assembly references.

--- src/app/FakeLib/MSBuild/Splicing.fs
+++ src/app/FakeLib/MSBuild/Splicing.fs
@@ -2,9 +2,25 @@
 module Fake.MSBuild.Splicing
 
 open Fake
+open System.Xml
+open System.Xml.XPath
+open System.Xml.Linq
 
-let removeAssemblyReference project =
-    let doc = XMLDoc project
-    let node = doc.SelectSingleNode "ItemGroup/Reference"
-    node.Value
+let removeAssemblyReference project filterF =
+    let doc = XDocument.Parse(project,LoadOptions.PreserveWhitespace)
+    let ns = "http://schemas.microsoft.com/developer/msbuild/2003"
 
+    let xname name = XName.Get(name,ns)
+
+    let references =
+        doc
+          .Descendants(xname "Project")
+          .Descendants(xname "ItemGroup")
+          .Descendants(xname "Reference")
+         |> Seq.filter(fun e -> 
+                let a = e.Attribute(XName.Get "Include")
+                a <> null && filterF (a.Value))
+    references.Remove()
+
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+      doc.ToString(SaveOptions.DisableFormatting)
