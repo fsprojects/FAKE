018341619f5f29f0d69c1a09f8470a372daf46dc
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-03 12:27:44 +0200

    Slicing of assembly references.

---
+++ src/test/Test.FAKECore/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.FSharp.Core;
+
+namespace Test.FAKECore
+{
+    public static class Extensions
+    {
+        public static FSharpFunc<T, T2> Convert<T, T2>(this Func<T, T2> func)
+        {
+            return FSharpFunc<T, T2>.FromConverter(new Converter<T, T2>(func));
+        }
+    }
+}
---
+++ src/test/Test.FAKECore/SideBySideSpecification/TestSplicing.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Fake.MSBuild;
+using NUnit.Framework;
+using Test.Git;
+
+namespace Test.FAKECore.SideBySideSpecification
+{
+    [TestFixture]
+    public class TestSplicing
+    {
+        [Test]
+        public void CanSpliceNUnitReference()
+        {
+            var project1 = File.ReadAllText(@"SideBySideSpecification\Project1.txt");
+            var project1Result = File.ReadAllText(@"SideBySideSpecification\Project1_WithoutNUnit.txt");
+
+            var result = Splicing.removeAssemblyReference(project1,
+                                                          Extensions.Convert<string, bool>(s => s.StartsWith("nunit")));
+            result.ShouldEqual(project1Result);
+        }
+    }
+}
