3f98a2a73c84dec93096f4bc3c6174f64181c5b4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-03 12:58:12 +0200

    CanSpliceTestFiles - Cleanup.

--- src/app/FakeLib/MSBuild/Splicing.fs
+++ src/app/FakeLib/MSBuild/Splicing.fs
@@ -6,41 +6,35 @@ open System.Xml
 open System.Xml.XPath
 open System.Xml.Linq
 
-let removeAssemblyReference project filterF =
-    let doc = XDocument.Parse(project,LoadOptions.PreserveWhitespace)
-    let ns = "http://schemas.microsoft.com/developer/msbuild/2003"
 
-    let xname name = XName.Get(name,ns)
+type MSBuildProject = XDocument
 
+let normalize (project:MSBuildProject) =
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+        project.ToString(SaveOptions.DisableFormatting) 
+
+
+let ns = "http://schemas.microsoft.com/developer/msbuild/2003"
+let xname name = XName.Get(name,ns)
+
+let loadProject (projectFileName:string) : MSBuildProject = 
+    MSBuildProject.Load(projectFileName,LoadOptions.PreserveWhitespace)
+
+let removeFilteredElement (doc:XDocument) elementName filterF =
     let references =
         doc
           .Descendants(xname "Project")
           .Descendants(xname "ItemGroup")
-          .Descendants(xname "Reference")
+          .Descendants(xname elementName)
          |> Seq.filter(fun e -> 
                 let a = e.Attribute(XName.Get "Include")
                 a <> null && filterF (a.Value))
     references.Remove()
+    doc
 
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-      doc.ToString(SaveOptions.DisableFormatting)
+let removeAssemblyReference (doc:XDocument) filterF =
+    removeFilteredElement doc "Reference" filterF
 
 
-let removeFiles project filterF =
-    let doc = XDocument.Parse(project,LoadOptions.PreserveWhitespace)
-    let ns = "http://schemas.microsoft.com/developer/msbuild/2003"
-
-    let xname name = XName.Get(name,ns)
-
-    let references =
-        doc
-          .Descendants(xname "Project")
-          .Descendants(xname "ItemGroup")
-          .Descendants(xname "Compile")
-         |> Seq.filter(fun e -> 
-                let a = e.Attribute(XName.Get "Include")
-                a <> null && filterF (a.Value))
-    references.Remove()
-
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-      doc.ToString(SaveOptions.DisableFormatting)
+let removeFiles (doc:XDocument) filterF =
+    removeFilteredElement doc "Compile" filterF
