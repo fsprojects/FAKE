991e49ea28e3b56c3bd987850d643227840ccbc7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-06 10:56:34 +0200

    Adding StringHelperTest

---
+++ src/test/Test.FAKECore/StringHelperTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Fake;
+using NUnit.Framework;
+using Test.Git;
+
+namespace Test.FAKECore
+{
+    [TestFixture]
+    public class StringHelperTest
+    {
+        [Test]
+        public void CanReadTestFile()
+        {
+            var lines = StringHelper.ReadFile(@"TestData\AllObjects.txt");
+            lines.Count().ShouldEqual(3578);
+        }
+
+        [Test]
+        public void CanSeparateEmptyLines()
+        {
+            var lines = new List<string>();
+            StringHelper.separated("test", lines).ShouldBeEmpty();
+        }
+
+        [Test]
+        public void CanSeparateOneLine()
+        {
+            var lines = new List<string> {"first"};
+            StringHelper.separated("test", lines).ShouldEqual("first");
+        }
+
+
+        [Test]
+        public void CanSeparateThreeLines()
+        {
+            var lines = new List<string> {"first", "second", "third"};
+            StringHelper.separated("-", lines).ShouldEqual("first-second-third");
+        }
+
+        [Test]
+        public void CanSeparateThreeLinesWithLineEnds()
+        {
+            var lines = new List<string> {"first", "second", "third"};
+            StringHelper.toLines(lines).ShouldEqual("first\r\nsecond\r\nthird");
+        }
+
+        [Test]
+        public void CanSeparateTwoLine()
+        {
+            var lines = new List<string> {"first", "second"};
+            StringHelper.separated(" ", lines).ShouldEqual("first second");
+        }
+    }
+}
--- src/test/Test.Git/ShouldExtensions.cs
+++ src/test/Test.Git/ShouldExtensions.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Test.Git
 {
@@ -16,6 +18,56 @@ namespace Test.Git
         {
             Assert.AreEqual(expected, item);
             return item;
+        }
+
+        /// <summary>
+        ///   Asserts that the extended boolean value is
+        ///   <c>true</c>
+        ///   .
+        /// </summary>
+        public static void ShouldBeTrue(this bool item)
+        {
+            Assert.True(item);
+        }
+
+        /// <summary>
+        ///   Asserts that the extended boolean value is
+        ///   <c>false</c>
+        ///   .
+        /// </summary>
+        public static void ShouldBeFalse(this bool item)
+        {
+            Assert.False(item);
+        }
+
+        /// <summary>
+        ///   Asserts that the collection supplied by
+        ///   <paramref name = "collection" />
+        ///   is empty.
+        /// </summary>
+        public static void ShouldBeEmpty<T>(this IEnumerable<T> collection)
+        {
+            collection.Any().ShouldBeFalse();
+        }
+
+        /// <summary>
+        ///   Shoulds the be empty.
+        /// </summary>
+        /// <param name = "text">The text.</param>
+        public static void ShouldBeEmpty(this string text)
+        {
+            Assert.IsEmpty(text);
+        }
+
+        /// <summary>
+        ///   Asserts that the collection supplied by
+        ///   <paramref name = "collection" />
+        ///   is not empty.
+        /// </summary>
+        public static IEnumerable<T> ShouldNotBeEmpty<T>(this IEnumerable<T> collection)
+        {
+            collection.Any().ShouldBeTrue();
+            return collection;
         }
     }
 }
