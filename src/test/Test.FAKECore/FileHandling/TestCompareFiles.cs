b38fd8778cdf93ae61b9122c8c8e39cd4e7b3119
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-07 08:54:58 +0200

    CompareFiles function should not calc iterations. Added test cases.

---
+++ src/test/Test.FAKECore/FileHandling/TestCompareFiles.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Fake;
+using NUnit.Framework;
+using Test.Git;
+
+namespace Test.FAKECore.FileHandling
+{
+    [TestFixture]
+    public class TestCompareFiles
+    {
+        [Test]
+        public void CanFindDifferencesInFiles()
+        {
+            var fi = new FileInfo(@"TestData\AllObjects.txt");
+            var fi2 = new FileInfo(@"TestData\AllObjects_2.txt");
+            FileHelper.FilesAreEqual(fi, fi2).ShouldBeFalse();
+        }        
+        
+        [Test]
+        public void CanCompareIdenticalFiles()
+        {
+            var fi = new FileInfo(@"TestData\AllObjects.txt");
+            var fi2 = new FileInfo(@"TestData\AllObjects.txt");
+            FileHelper.FilesAreEqual(fi, fi2).ShouldBeTrue();
+        }
+    }
+}
