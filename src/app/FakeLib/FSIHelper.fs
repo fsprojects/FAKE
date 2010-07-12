6c880f20568a84ce2233a78b65d202b6a065d144
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-12 16:46:03 +0200

    Fixing EnvironmentVariable bug.

--- src/app/FakeLib/FSIHelper.fs
+++ src/app/FakeLib/FSIHelper.fs
@@ -12,11 +12,11 @@ let runBuildScript script args =
     let result = execProcess (fun info ->  
       info.FileName <- fsiPath
       info.Arguments <- script           
-      Seq.iter info.EnvironmentVariables.Add args      
-      if not <| info.EnvironmentVariables.ContainsKey "MSBuild" then
-          info.EnvironmentVariables.Add("MSBuild", msBuildExe)
-      if not <| info.EnvironmentVariables.ContainsKey "FSI" then
-          info.EnvironmentVariables.Add("FSI", fsiPath) )
+      args
+        |> Seq.iter (fun (k,v) -> setEnvironVar k v)
+
+      setEnvironVar "MSBuild" msBuildExe
+      setEnvironVar "FSI" fsiPath)
     
     System.Threading.Thread.Sleep 1000
     result
