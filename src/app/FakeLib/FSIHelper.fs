0294dc0156635d6b7c41e1b781618ed81788a369
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-14 10:29:34 +0200

    Set EnvironmentVariables on ProcessStartinfo

--- src/app/FakeLib/FSIHelper.fs
+++ src/app/FakeLib/FSIHelper.fs
@@ -10,13 +10,19 @@ let runBuildScript script args =
     traceFAKE "Running Buildscript: %s" script
   
     let result = execProcess (fun info ->  
-      info.FileName <- fsiPath
-      info.Arguments <- script           
-      args
-        |> Seq.iter (fun (k,v) -> setEnvironVar k v)
+        info.FileName <- fsiPath
+        info.Arguments <- script     
+            
+        let setVar (k,v) =
+            if info.EnvironmentVariables.ContainsKey k then
+                info.EnvironmentVariables.[k] <- v
+            else 
+                info.EnvironmentVariables.Add(k,v)
 
-      setEnvironVar "MSBuild" msBuildExe
-      setEnvironVar "FSI" fsiPath) System.TimeSpan.MaxValue
+        args |> Seq.iter setVar
+
+        setVar("MSBuild",msBuildExe)
+        setVar("FSI",fsiPath)) System.TimeSpan.MaxValue
     
     System.Threading.Thread.Sleep 1000
     result
