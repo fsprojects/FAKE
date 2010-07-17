5e3724f5f76ba76d23ac70df4f56f77ef2d5e057
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-07-17 21:22:06 +0200

    Adding a SCP task.

---
+++ src/app/FakeLib/SCPHelper.fs
@@ -0,0 +1,19 @@
+﻿[<AutoOpen>]
+module Fake.SCPHelper
+
+/// <summary>Performs a SCP copy.</summary>
+/// <param name="source">The source directory (fileName)</param>
+/// <param name="destination">The target directory (fileName)</param>
+let SCP scpTool source destination =
+    tracefn "SCP %s %s" source destination
+    
+    let args = sprintf "-r \".\" %s" (destination |> toParam)
+
+    tracefn "%s %s" scpTool args
+    let result = 
+        ExecProcess (fun info ->
+            info.FileName <- scpTool
+            info.WorkingDirectory <- source |> FullName
+            info.Arguments <- args) System.TimeSpan.MaxValue
+               
+    if result <> 0 then failwithf "Error during SCP From: %s To: %s" source destination
