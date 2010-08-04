d5a920e1786c4548f4e5c10b397ffdf2ed0eca08
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-08-04 20:39:14 +0200

    Starting a GemCreationHelper.

---
+++ src/app/FakeLib/GemHelper.fs
@@ -0,0 +1,43 @@
+﻿namespace Fake
+
+type GemParams =
+    { ProjectName: string;
+      ToolPath: string;
+      Platform: string;
+      Version: string;
+      Summary: string;
+      Description: string;
+      WorkingDir: string;}
+
+[<AutoOpen>]
+module GemHelper =
+    open System
+    open System.Text
+   
+    /// Gem default params
+    let GemDefaults =   
+        { ProjectName = String.Empty;
+          ToolPath = "gem.bat";
+          Platform = "Gem::Platform::RUBY";
+          Version = String.Empty;
+          Summary = String.Empty;
+          Description = String.Empty;
+          WorkingDir = currentDirectory}
+
+    let CreateGemSpecification setParams =
+        let p = setParams {GemDefaults with Version = buildVersion}
+
+        let sb = new StringBuilder()
+        let append text  = sb.AppendLine text |> ignore
+        let appends text = Printf.kprintf append text
+        let appendIf p text = if isNullOrEmpty p |> not then append text
+
+        append   "Gem::Specification.new do |spec|"
+        appends  "  spec.platform    = %s" p.Platform
+        appends  "  spec.name        = '%s'" p.ProjectName
+        appends  "  spec.version     = '%s'" p.Version
+        appendIf p.Summary <| sprintf "  spec.summary     = '%s'" p.Summary
+        appendIf p.Description <| sprintf "  spec.description = '%s'" p.Description
+        append "end"
+
+        sb.ToString()
---
+++ src/test/Test.FAKECore/GemCreation/TestGemCreation.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Fake;
+using NUnit.Framework;
+using Test.Git;
+
+namespace Test.FAKECore.GemCreation
+{
+    [TestFixture]
+    public class TestGemCreation
+    {
+        [Test]
+        public void CanCreateGemSpecifactionWithVersion()
+        {
+            Func<GemParams, GemParams> func =
+                defaultParam =>
+                new GemParams("fake",
+                              defaultParam.ToolPath,
+                              defaultParam.Platform,
+                              "1.2.3.4",
+                              defaultParam.Summary,
+                              defaultParam.Description,
+                              defaultParam.WorkingDir);
+
+            GemHelper.CreateGemSpecification(func.Convert())
+                .ShouldEqual(
+                    "Gem::Specification.new do |spec|\r\n" +
+                    "  spec.platform    = Gem::Platform::RUBY\r\n" +
+                    "  spec.name        = 'fake'\r\n" +
+                    "  spec.version     = '1.2.3.4'\r\n" +
+                    "end\r\n");
+        }
+
+        [Test]
+        public void CanCreateGemSpecifactionWithVersionAndSummary()
+        {
+            Func<GemParams, GemParams> func =
+                defaultParam =>
+                new GemParams("fake",
+                              defaultParam.ToolPath,
+                              defaultParam.Platform,
+                              "1.2.3.4",
+                              "My summary",
+                              defaultParam.Description,
+                              defaultParam.WorkingDir);
+
+            GemHelper.CreateGemSpecification(func.Convert())
+                .ShouldEqual(
+                    "Gem::Specification.new do |spec|\r\n" +
+                    "  spec.platform    = Gem::Platform::RUBY\r\n" +
+                    "  spec.name        = 'fake'\r\n" +
+                    "  spec.version     = '1.2.3.4'\r\n" +
+                    "  spec.summary     = 'My summary'\r\n" +
+                    "end\r\n");
+        }
+
+        [Test]
+        public void CanCreateGemSpecifactionWithVersionAndDescription()
+        {
+            Func<GemParams, GemParams> func =
+                defaultParam =>
+                new GemParams("fake",
+                              defaultParam.ToolPath,
+                              defaultParam.Platform,
+                              "1.2.3.4",
+                              defaultParam.Summary,
+                              "My description",
+                              defaultParam.WorkingDir);
+
+            GemHelper.CreateGemSpecification(func.Convert())
+                .ShouldEqual(
+                    "Gem::Specification.new do |spec|\r\n" +
+                    "  spec.platform    = Gem::Platform::RUBY\r\n" +
+                    "  spec.name        = 'fake'\r\n" +
+                    "  spec.version     = '1.2.3.4'\r\n" +
+                    "  spec.description = 'My description'\r\n" +
+                    "end\r\n");
+        }
+    }
+}
