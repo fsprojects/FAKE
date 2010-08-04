5e262b199151341c345782fd2539e607956dbeff
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-08-04 09:23:07 +0200

    Added Rebase.continueRebase and Rebase.skip for git.

--- src/app/Fake.Git/Rebase.fs
+++ src/app/Fake.Git/Rebase.fs
@@ -7,5 +7,11 @@ let start repositoryDir onTopOfBranch =
     sprintf "rebase %s" onTopOfBranch
       |> gitCommand repositoryDir
 
-/// Aborts a running rebase
+/// Restore the original branch and abort the rebase operation. 
 let abort repositoryDir = gitCommand repositoryDir "rebase --abort"
+
+/// Restart the rebasing process after having resolved a merge conflict. 
+let continueRebase repositoryDir = gitCommand repositoryDir "rebase --continue"
+
+/// Restart the rebasing process by skipping the current patch. 
+let skip repositoryDir = gitCommand repositoryDir "rebase --skip"
