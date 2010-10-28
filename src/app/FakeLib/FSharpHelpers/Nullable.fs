0faaf4ec74b79035de9cd3796ee15a2943a32ce5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-10-28 10:09:22 +0200

    Moved Nullable.fs into main project.

--- src/app/FakeLib/FSharpHelpers/Nullable.fs
+++ src/app/FakeLib/FSharpHelpers/Nullable.fs
@@ -3,10 +3,13 @@
 open System 
 
 module Option =
+    /// Converts a nullable object into an F# option
     let fromNullable (n: _ Nullable) = 
         if n.HasValue
             then Some n.Value
             else None
+
+    /// Converts an F# option into a nullable object
     let toNullable =
         function
         | None -> Nullable()
