47443aba206f950f8803a6f6f7d7bdfbfbdcc4a9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2010-11-03 09:24:12 +0100

    New sample: "Run specific target"

---
+++ Samples/RunSpecificTarget/build.fsx
@@ -0,0 +1,27 @@
+// include Fake libs
+#I @"..\..\tools\FAKE"
+#r "FakeLib.dll"
+open Fake 
+
+// *** Define Targets ***
+Target "Clean" (fun () -> 
+    trace " --- Cleaning stuff --- "
+)
+
+Target "Build" (fun () -> 
+    trace " --- Building the app --- "
+)
+
+Target "Deploy" (fun () -> 
+    trace " --- Deploying app --- "
+)
+
+// *** Define Dependencies ***
+"Build"  <== ["Clean"]
+"Deploy"  <== ["Build"]
+
+// *** Start Build ***
+if not (hasBuildParam "target") then 
+  Run "Deploy" 
+else 
+  Run <| getBuildParam "target"
