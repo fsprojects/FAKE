90088839d25500a28e1779909c9f2e2afb5e79bf
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-02-16 11:45:35 +0100

    Adding MSpec runner support.

---
+++ src/app/FakeLib/MSpecHelper.fs
@@ -0,0 +1,44 @@
+﻿[<AutoOpen>]
+module Fake.MSpecHelper
+
+open System
+open System.IO
+open System.Text
+
+type MSpecParams =
+    { ToolPath: string;
+      HtmlOutputDir: string;
+      WorkingDir:string; 
+      TimeOut: TimeSpan}
+
+/// MSpec default params  
+let MSpecDefaults =
+    { ToolPath = currentDirectory @@ "tools" @@ "MSpec" @@ "mspec.exe";
+      HtmlOutputDir = null;
+      WorkingDir = null;
+      TimeOut = TimeSpan.FromMinutes 5.}
+
+let MSpec setParams assemblies = 
+    let details = separated ", " assemblies
+    traceStartTask "MSpec" details
+    let parameters = setParams MSpecDefaults
+    assemblies
+      |> Seq.iter (fun assembly ->
+          let commandLineBuilder =
+              let html = isNullOrEmpty parameters.HtmlOutputDir |> not
+
+              new StringBuilder()
+                |> appendIfTrue (buildServer = TeamCity) "--teamcity"
+                |> appendIfTrue html "-t" 
+                |> appendIfTrue html (sprintf "--html\" \"%s" <| parameters.HtmlOutputDir.TrimEnd Path.DirectorySeparatorChar) 
+                |> appendFileNamesIfNotNull [assembly]
+
+          printfn "%A" <| commandLineBuilder.ToString()
+          if not (execProcess3 (fun info ->  
+              info.FileName <- parameters.ToolPath
+              info.WorkingDirectory <- parameters.WorkingDir
+              info.Arguments <- commandLineBuilder.ToString()) parameters.TimeOut)
+          then
+              failwith "MSpec test failed.")
+                  
+    traceEndTask "MSpec" details
