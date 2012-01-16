787c66977268a7713e496bb6326e0f956b911211
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-01-16 16:29:56 +0100

    Detect Jenkins

--- src/app/FakeLib/BuildServerHelper.fs
+++ src/app/FakeLib/BuildServerHelper.fs
@@ -4,7 +4,8 @@ module Fake.BuildServerHelper
 /// The BuildServer type.
 type BuildServer =
 | TeamCity
-| CCNet       
+| CCNet
+| Jenkins
 | LocalBuild
 
 /// The trace Mode type.
@@ -23,7 +24,10 @@ let localBuildLabel = "LocalBuild"
 let mutable xmlOutputFile = getBuildParamOrDefault "logfile" @".\output\Results.xml"
 
 /// Build number retrieved from TeamCity
-let tcBuildNumber = environVar "BUILD_NUMBER" 
+let tcBuildNumber = environVar "BUILD_NUMBER"
+
+/// Build number retrieved from Jenkins
+let jenkinsBuildNumber = tcBuildNumber
 
 /// CruiseControl.NET Build label
 let ccBuildLabel = environVar "CCNETLABEL"
@@ -31,6 +35,7 @@ let ccBuildLabel = environVar "CCNETLABEL"
 /// Determines the current BuildVersion and if it is a local build
 let buildVersion,buildServer =     
     let getVersion = getBuildParamOrDefault "buildVersion"
+    if hasBuildParam "jenkins_home" then getVersion jenkinsBuildNumber,Jenkins else
     if not (isNullOrEmpty tcBuildNumber) then getVersion tcBuildNumber,TeamCity else
     if not (isNullOrEmpty ccBuildLabel) then getVersion ccBuildLabel,CCNet else 
     getVersion localBuildLabel,LocalBuild
