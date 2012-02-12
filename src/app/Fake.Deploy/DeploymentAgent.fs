eee59ed0d6c9e236ba57610a2574a6179dbb5a82
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-02-12 16:39:20 +0100

    Reduced indentation

--- src/app/Fake.Deploy/DeploymentAgent.fs
+++ src/app/Fake.Deploy/DeploymentAgent.fs
@@ -1,73 +1,70 @@
 ﻿module Fake.DeploymentAgent
     
-    open System
-    open System.Net
-    open System.Threading
-    open System.Diagnostics
-    open Fake.DeploymentHelper
+open System
+open System.Net
+open System.Threading
+open System.Diagnostics
+open Fake.DeploymentHelper
 
-    let mutable logger : (string * EventLogEntryType) -> unit = ignore 
+let mutable logger : (string * EventLogEntryType) -> unit = ignore 
 
-    let private listener port = 
-        let l = new HttpListener()
-        l.Prefixes.Add(sprintf "http://localhost:%s/fake/" port)
-        l.Start()
-        l
+let private listener port = 
+    let l = new HttpListener()
+    l.Prefixes.Add(sprintf "http://localhost:%s/fake/" port)
+    l.Start()
+    l
 
-    let cts = new CancellationTokenSource()
+let cts = new CancellationTokenSource()
 
-    let writeResponse (ctx : HttpListenerContext) (str : string) = 
-        let response = Text.Encoding.UTF8.GetBytes(str)
-        ctx.Response.ContentLength64 <- response.Length |> int64
-        ctx.Response.ContentEncoding <- Text.Encoding.UTF8
-        ctx.Response.Close(response, true)
+let writeResponse (ctx : HttpListenerContext) (str : string) = 
+    let response = Text.Encoding.UTF8.GetBytes(str)
+    ctx.Response.ContentLength64 <- response.Length |> int64
+    ctx.Response.ContentEncoding <- Text.Encoding.UTF8
+    ctx.Response.Close(response, true)
 
-    let handleSuccess package (ctx : HttpListenerContext) = 
-        let msg = sprintf "Successfully deployed %s" (package.ToString())
-        logger (msg, EventLogEntryType.Information)
-        DeploymentResponse.Sucessful(package.Id, package.Version) 
-        |> Json.serialize
-        |> writeResponse ctx
+let handleSuccess package (ctx : HttpListenerContext) = 
+    let msg = sprintf "Successfully deployed %s" (package.ToString())
+    logger (msg, EventLogEntryType.Information)
+    DeploymentResponse.Sucessful(package.Id, package.Version) 
+    |> Json.serialize
+    |> writeResponse ctx
 
-    let handleFailure package (ctx : HttpListenerContext) exn = 
-        let msg = sprintf "Deployment failed: %s " (package.ToString())
-        logger (msg, EventLogEntryType.Information)
-        DeploymentResponse.Failure(package.Id, package.Version, exn) 
-        |> Json.serialize
-        |> writeResponse ctx
+let handleFailure package (ctx : HttpListenerContext) exn = 
+    let msg = sprintf "Deployment failed: %s " (package.ToString())
+    logger (msg, EventLogEntryType.Information)
+    DeploymentResponse.Failure(package.Id, package.Version, exn) 
+    |> Json.serialize
+    |> writeResponse ctx
 
-    let handleRequest (ctx : HttpListenerContext) = 
-        if ctx.Request.HttpMethod = "POST" && ctx.Request.ContentType = "application/fake" && ctx.Request.HasEntityBody
-        then 
-            try
-                use sr = new IO.StreamReader(ctx.Request.InputStream, Text.Encoding.UTF8)
-                let package = sr.ReadToEnd() |> Json.deserialize
-                match runDeployment package with
-                | Choice1Of2(result, package) -> handleSuccess package ctx
-                | Choice2Of2(e) -> handleFailure package ctx e
-            with e ->
-                let msg = sprintf "Fake Deploy Request Error:\n\n%A" e
-                logger (msg, EventLogEntryType.Error)
-                writeResponse ctx msg
+let handleRequest (ctx : HttpListenerContext) = 
+    if ctx.Request.HttpMethod = "POST" && ctx.Request.ContentType = "application/fake" && ctx.Request.HasEntityBody
+    then 
+        try
+            use sr = new IO.StreamReader(ctx.Request.InputStream, Text.Encoding.UTF8)
+            let package = sr.ReadToEnd() |> Json.deserialize
+            match runDeployment package with
+            | Choice1Of2(result, package) -> handleSuccess package ctx
+            | Choice2Of2(e) -> handleFailure package ctx e
+        with e ->
+            let msg = sprintf "Fake Deploy Request Error:\n\n%A" e
+            logger (msg, EventLogEntryType.Error)
+            writeResponse ctx msg
                 
 
-    let start log port =
-        logger <- log
-        let listenerLoop = 
-            async {
-                try
-                    use l = listener port
-                    log (sprintf "Fake Deploy now listening @ %s" (String.Join(",", l.Prefixes |> Seq.map id |> Array.ofSeq)), EventLogEntryType.Information)
-                    while true do
-                        handleRequest (l.GetContext())
-                with e ->
-                    logger (sprintf "Fake Deploy Listener Error:\n\n%A" e, EventLogEntryType.Error)
+let start log port =
+    logger <- log
+    let listenerLoop = 
+        async {
+            try
+                use l = listener port
+                log (sprintf "Fake Deploy now listening @ %s" (String.Join(",", l.Prefixes |> Seq.map id |> Array.ofSeq)), EventLogEntryType.Information)
+                while true do
+                    handleRequest (l.GetContext())
+            with e ->
+                logger (sprintf "Fake Deploy Listener Error:\n\n%A" e, EventLogEntryType.Error)
                     
-            }
-        Async.Start(listenerLoop, cts.Token)
+        }
+    Async.Start(listenerLoop, cts.Token)
 
-    let stop() = 
-        cts.Cancel()
-
-
-
+let stop() = 
+    cts.Cancel()
--- src/app/Fake.Deploy/DeploymentHelper.fs
+++ src/app/Fake.Deploy/DeploymentHelper.fs
@@ -1,147 +1,139 @@
 ﻿module Fake.DeploymentHelper
     
-    open System
-    open System.IO
-    open System.Net
-    open Fake
+open System
+open System.IO
+open System.Net
+open Fake
 
-    type DeploymentResponse = {
-            Success : bool
-            Id : string
-            Version : string
-            Error : obj
-        }
-        with 
-            static member Sucessful(packageId, version) = 
-                {
-                    Success = true;
-                    Id = packageId;
-                    Version = version;
-                    Error = null;
-                }
+type DeploymentResponse = {
+        Success : bool
+        Id : string
+        Version : string
+        Error : obj
+    }
+    with 
+        static member Sucessful(packageId, version) = 
+            {
+                Success = true;
+                Id = packageId;
+                Version = version;
+                Error = null;
+            }
 
-            static member Failure(packageId, version, error) =
-                {
-                    Success = false;
-                    Id = packageId;
-                    Version = version;
-                    Error = error;
-                }
+        static member Failure(packageId, version, error) =
+            {
+                Success = false;
+                Id = packageId;
+                Version = version;
+                Error = error;
+            }
 
-            override x.ToString() = 
-                if x.Success 
-                then sprintf "Deployment of %s %s successful" x.Id x.Version
-                else sprintf "Deployment of %s %s failed\n\n%A" x.Id x.Version x.Error
+        override x.ToString() = 
+            if x.Success 
+            then sprintf "Deployment of %s %s successful" x.Id x.Version
+            else sprintf "Deployment of %s %s failed\n\n%A" x.Id x.Version x.Error
 
-    type DeploymentPackage = {
-            Id : string
-            Version : string
-            Script : byte[]
-            Package : byte[]
-        }
-        with
-            member x.TargetDir =
-                x.Id + "_" + (x.Version.Replace('.','_'))
+type DeploymentPackage = {
+        Id : string
+        Version : string
+        Script : byte[]
+        Package : byte[]
+    }
+    with
+        member x.TargetDir =
+            x.Id + "_" + (x.Version.Replace('.','_'))
 
-            override x.ToString() = 
-                x.Id + " " + x.Version
+        override x.ToString() = 
+            x.Id + " " + x.Version
 
-    let createDeploymentPackageFromZip packageName version fakescript archive output =
-        ensureDirectory output
-        let package = {
-            Id = packageName
-            Version = version
-            Script =  fakescript |> FullName |> ReadFileAsBytes
-            Package = archive |> FullName  |> ReadFileAsBytes
-        }
+let createDeploymentPackageFromZip packageName version fakescript archive output =
+    ensureDirectory output
+    let package = {
+        Id = packageName
+        Version = version
+        Script =  fakescript |> FullName |> ReadFileAsBytes
+        Package = archive |> FullName  |> ReadFileAsBytes
+    }
 
-        let fileName = output @@ (packageName + ".fakepkg")
+    let fileName = output @@ (packageName + ".fakepkg")
 
-        package
-          |> Json.serialize
-          |> Text.Encoding.UTF8.GetBytes
-          |> WriteBytesToFile fileName
+    package
+        |> Json.serialize
+        |> Text.Encoding.UTF8.GetBytes
+        |> WriteBytesToFile fileName
 
-        File.Delete archive
+    File.Delete archive
 
-    let createDeploymentPackageFromDirectory packageName version fakescript dir output =
-        let archive = packageName + ".zip"
-        let files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories)
-        Zip dir archive files
-        createDeploymentPackageFromZip packageName version fakescript archive output
+let createDeploymentPackageFromDirectory packageName version fakescript dir output =
+    let archive = packageName + ".zip"
+    let files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories)
+    Zip dir archive files
+    createDeploymentPackageFromZip packageName version fakescript archive output
 
-    let ensureDeployDir (package : DeploymentPackage) = 
-        let path = "Work" @@ package.TargetDir
-        ensureDirectory path
-        path,package
+let ensureDeployDir (package : DeploymentPackage) = 
+    let path = "Work" @@ package.TargetDir
+    ensureDirectory path
+    path,package
 
-    let unpack (dir,package) =
-        let archive = package.Id + ".zip"
+let unpack (dir,package) =
+    let archive = package.Id + ".zip"
 
-        package.Package |> WriteBytesToFile archive
-        Unzip dir archive
-        File.Delete archive
+    package.Package |> WriteBytesToFile archive
+    Unzip dir archive
+    File.Delete archive
 
-        let script = dir @@ (package.Id + ".fsx")
-        package.Script |> WriteBytesToFile script
-        script, package
+    let script = dir @@ (package.Id + ".fsx")
+    package.Script |> WriteBytesToFile script
+    script, package
 
-    let prepare = ensureDeployDir >> unpack
+let prepare = ensureDeployDir >> unpack
     
-    let doDeployment package = 
-       let (script, _) = prepare package
-       let workingDirectory = DirectoryName script
-       let fakeLibTarget = workingDirectory @@ "FakeLib.dll"
-       if  not <| File.Exists fakeLibTarget then File.Copy("FakeLib.dll", fakeLibTarget)
-       (FSIHelper.runBuildScriptAt workingDirectory true (FullName script) Seq.empty, package)
+let doDeployment package = 
+    let (script, _) = prepare package
+    let workingDirectory = DirectoryName script
+    let fakeLibTarget = workingDirectory @@ "FakeLib.dll"
+    if  not <| File.Exists fakeLibTarget then File.Copy("FakeLib.dll", fakeLibTarget)
+    (FSIHelper.runBuildScriptAt workingDirectory true (FullName script) Seq.empty, package)
        
-    let runDeployment package = 
-        try
-            doDeployment package |> Choice1Of2
-        with e ->
-            Choice2Of2(e)
+let runDeployment package = 
+    try
+        doDeployment package |> Choice1Of2
+    with e ->
+        Choice2Of2(e)
 
-    let runDeploymentFromPackage packagePath = 
-        try
-            ReadFileAsString packagePath
-            |> Json.deserialize
-            |> runDeployment
-        with e -> 
-            Choice2Of2(e)
-
-
-    let postDeploymentPackage url packagePath = 
-        let result = ref None
-        let waitHandle = new Threading.AutoResetEvent(false)
-        let handle (event : UploadDataCompletedEventArgs) =
-            if event.Cancelled 
-            then 
-                result := Some <| Choice2Of2(OperationCanceledException() :> exn)
-                waitHandle.Set() |> ignore
-            elif event.Error <> null
-            then 
-                result := Some <| Choice2Of2(event.Error)
-                waitHandle.Set() |> ignore
-            else
-                use ms = new MemoryStream(event.Result)
-                use sr = new StreamReader(ms, Text.Encoding.UTF8)
-                let res = sr.ReadToEnd()
-                result := Json.deserialize<DeploymentResponse> res |> Choice1Of2 |> Some 
-                waitHandle.Set() |> ignore
-
-        let uri = new Uri(url, UriKind.Absolute)
-        let client = new WebClient()
-        let mutable uploaded = false
-        client.Headers.Add(HttpRequestHeader.ContentType, "application/fake")
-        client.UploadDataCompleted |> Event.add handle
-        client.UploadDataAsync(uri, "POST", ReadFileAsBytes packagePath)
-        waitHandle.WaitOne() |> ignore
-        !result
-        
-        
-
-     
-        
+let runDeploymentFromPackage packagePath = 
+    try
+        ReadFileAsString packagePath
+        |> Json.deserialize
+        |> runDeployment
+    with e -> 
+        Choice2Of2(e)
 
 
+let postDeploymentPackage url packagePath = 
+    let result = ref None
+    let waitHandle = new Threading.AutoResetEvent(false)
+    let handle (event : UploadDataCompletedEventArgs) =
+        if event.Cancelled 
+        then 
+            result := Some <| Choice2Of2(OperationCanceledException() :> exn)
+            waitHandle.Set() |> ignore
+        elif event.Error <> null
+        then 
+            result := Some <| Choice2Of2(event.Error)
+            waitHandle.Set() |> ignore
+        else
+            use ms = new MemoryStream(event.Result)
+            use sr = new StreamReader(ms, Text.Encoding.UTF8)
+            let res = sr.ReadToEnd()
+            result := Json.deserialize<DeploymentResponse> res |> Choice1Of2 |> Some 
+            waitHandle.Set() |> ignore
 
+    let uri = new Uri(url, UriKind.Absolute)
+    let client = new WebClient()
+    let mutable uploaded = false
+    client.Headers.Add(HttpRequestHeader.ContentType, "application/fake")
+    client.UploadDataCompleted |> Event.add handle
+    client.UploadDataAsync(uri, "POST", ReadFileAsBytes packagePath)
+    waitHandle.WaitOne() |> ignore
+    !result
