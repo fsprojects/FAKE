e9be21c05eb4274fc11cd6f666d9e8ea8c3ae4cd
Ben Winkel <funnelweb007@gmail.com>
2012-10-21 23:45:37 +0300

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,15 +14,15 @@ $(window).resize(sectionHeight);
 
 $(document).ready(function(){
   $("section h1, section h2").each(function(){
-    $("nav").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
     $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav li:first-child a").parent().addClass("active");
+    $("nav ul li:first-child a").parent().addClass("active");
   });
   
-  $("nav li").on("click", "a", function(event) {
+  $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
-    $("nav li a").parent().removeClass("active");
+    $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
     event.preventDefault();    
   });
