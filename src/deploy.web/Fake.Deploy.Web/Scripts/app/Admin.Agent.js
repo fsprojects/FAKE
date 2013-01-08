11910d3ec65dad15cd1313a41b6e2c775136c7a1
Colin <colinbul@googlemail.com>
2013-01-08 21:11:57 +0000

    Added unstaged files

---
+++ src/deploy.web/Fake.Deploy.Web/Scripts/app/Admin.Agent.js
@@ -0,0 +1,35 @@
+﻿function AgentAdminViewModel() {
+	var self = this;
+
+	self.environments = ko.observableArray();
+
+	self.build = function () {
+		$.ajax({
+			type: "GET",
+			url: '/api/v1/environment/',
+			dataType: 'json',
+			contentType: 'application/json'
+		}).done(function (data) {
+			$.each(data, function (i, d) {
+				var inst = ko.mapping.fromJS(d)
+				self.environments.push(inst);
+			});
+		}).fail(function (msg) {
+			toastr.error('Failed to get environments', 'Error');
+		});
+	};
+
+	self.registerAgent = function (form) {
+		$.ajax({
+			type: "POST",
+			url: '/api/v1/agent/',
+			dataType: 'json',
+			data: $(form).serialize(),
+			contentType: 'application/x-www-form-urlencoded'
+		}).done(function (data) {
+			toastr.info('Successfully registered agent')
+		}).fail(function (msg) {
+			toastr.error('Failed to register agent', 'Error');
+		});
+	}
+}
---
+++ src/deploy.web/Fake.Deploy.Web/Scripts/app/Admin.Environment.js
@@ -0,0 +1,23 @@
+﻿function EnvironmentAdminViewModel() {
+    var self = this;
+
+    self.saveEnvironment = function (form) {
+        var data = $(form).serializeObject();
+        data.Id = null;
+        data.Agents = [];
+        var jsonStr = JSON.stringify(data);
+        $.ajax({
+            type: "POST",
+            url: '/api/v1/environment',
+            dataType: 'json',
+            data: jsonStr,
+            contentType: 'application/json'
+        }).done(function (data) {
+            toastr.info('Environment saved', 'Info');
+        }).fail(function (msg) {
+            toastr.error('Failed to save environment', 'Error');
+        });
+
+        return false;
+    };
+}
