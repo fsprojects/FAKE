a409efb38d7b1382f053984fec5dc817d5f03a1f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-09 19:22:06 +0100

    Adding RestorePackaes as a target

---
+++ RestorePackages.cmd
@@ -0,0 +1,6 @@
+@echo off
+cls
+
+"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"  
+"tools\FAKE\tools\Fake.exe" "build.fsx" "target=RestorePackages"
+exit /b %errorlevel%
