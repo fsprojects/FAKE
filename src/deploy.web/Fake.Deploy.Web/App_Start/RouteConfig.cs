659d2007e705908895d9635f432fab3b5e34632f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-09 19:33:50 +0100

    Cleaning namespaces

--- src/deploy.web/Fake.Deploy.Web/App_Start/RouteConfig.cs
+++ src/deploy.web/Fake.Deploy.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Fake.Deploy.Web
@@ -16,8 +12,8 @@ namespace Fake.Deploy.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                );
         }
     }
 }
