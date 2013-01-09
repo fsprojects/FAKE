d6dc651324315c80ce3d1d90be3ad3cad40736e1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-09 14:07:45 +0100

    Restore packages in Calculator sample

--- Samples/Calculator/completeBuild.fsx
+++ Samples/Calculator/completeBuild.fsx
@@ -1,16 +1,19 @@
-// include Fake libs
-#I "tools\FAKE"
-#r "FakeLib.dll"
+// include Fake lib
+#r @"tools\FAKE\tools\FakeLib.dll"
 
 open Fake
+
+RestorePackages()
 
 // Directories
 let buildDir  = @".\build\"
 let testDir   = @".\test\"
 let deployDir = @".\deploy\"
+let packagesDir = @".\packages"
 
 // tools
-let nunitPath = @".\Tools\NUnit"
+let nunitVersion = GetPackageVersion packagesDir "NUnit.Runners"
+let nunitPath = sprintf @"./packages/NUnit.Runners.%s/tools/" nunitVersion
 let fxCopRoot = @".\Tools\FxCop\FxCopCmd.exe"
     
 // version info
@@ -42,7 +45,6 @@ Target "SetVersions" (fun _ ->
             Guid = "EE5621DB-B86B-44eb-987F-9C94BCC98441";
             OutputFileName = @".\src\app\CalculatorLib\Properties\AssemblyInfo.cs"})          
 )
-
 
 Target "CompileApp" (fun _ ->    
     !+ @"src\app\**\*.csproj" 
