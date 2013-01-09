9400f536389040bb192a7cbe97eedbee51e5310a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-09 20:39:58 +0100

    Fixing init

--- src/deploy.web/Fake.Deploy.Web.App/InitialData.fs
+++ src/deploy.web/Fake.Deploy.Web.App/InitialData.fs
@@ -4,31 +4,33 @@ open System
 open Fake.Deploy.Web.Model
 open Raven.Client.Indexes
 
-let private defaultEnvironments() = 
-    let agent1 = Agent.Create("http://localhost:8080","localhost")
-
-    [
-        { Id = "environments-1";
-          Name = "Development";
-          Description = "Development Environment";
-          Agents = [agent1.Ref]}
-        { Id = "environments-2";
-          Name = "Integration";
-          Description = "Integration Environment";
-          Agents = [] }
-        { Id = "environments-3";
-          Name = "Staging";
-          Description = "User Acceptance and pre-Production environment";
-          Agents = [] }
-        { Id = "environments-4"; 
-          Name = "Production";
-          Description = "Production environment";
-          Agents = [] }
-    ]
 
 let private createIndexes (assems : seq<Reflection.Assembly>) =
     assems |> Seq.iter (fun ass -> IndexCreation.CreateIndexes(ass, documentStore))
 
 let Init() = 
     createIndexes [Reflection.Assembly.GetExecutingAssembly()]
-    defaultEnvironments() |> Save
+
+    let agent1 = Agent.Create("http://localhost:8080","localhost")
+    let agents = [agent1]
+
+    let environments = 
+        [ { Id = "environments-1";
+            Name = "Development";
+            Description = "Development Environment";
+            Agents = [agent1.Ref]}
+          { Id = "environments-2";
+            Name = "Integration";
+            Description = "Integration Environment";
+            Agents = [] }
+          { Id = "environments-3";
+            Name = "Staging";
+            Description = "User Acceptance and pre-Production environment";
+            Agents = [] }
+          { Id = "environments-4"; 
+            Name = "Production";
+            Description = "Production environment";
+            Agents = [] } ]
+
+    Save agents
+    Save environments
