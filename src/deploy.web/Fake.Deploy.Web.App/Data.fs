c3dd6805bee390b2c20347a326c6576318481fb9
Colin <colinbul@googlemail.com>
2013-01-20 10:33:43 +0000

    Started refactoring to an IDataProvider pattern

---
+++ src/deploy.web/Fake.Deploy.Web.App/Data.fs
@@ -0,0 +1,52 @@
+﻿module Fake.Deploy.Web.Data
+
+    open System
+    open System.IO
+    open System.Configuration
+    open System.Reflection
+    open Fake.Deploy.Web
+
+    let getProviderPath(name) = 
+        Path.Combine("DataProviders", name + ".dll")
+
+    let provider = 
+        let connStr = ConfigurationManager.ConnectionStrings.["Fake.Deploy"]
+        let providerAss, providerType = 
+            match connStr.ProviderName.Split([|','|], StringSplitOptions.RemoveEmptyEntries) with
+            | [|ass; ty|] -> ass, ty
+            | _ -> failwith "Fake.Deploy connection string has an invalid providerName. Should be of form: (Assmbly.Path, ProviderType)"
+        let loadedAssembly = Assembly.LoadFile(getProviderPath(providerAss))
+        let providerType = loadedAssembly.GetType(providerType, true)
+        let instance = Activator.CreateInstance(providerType) :?> IDataProvider
+
+    let init() =
+        InitialData.Init(provider)
+
+    let getEnvironment (id : string) = 
+        provider.GetEnvironments([id]) |> Seq.head
+
+    let saveEnvironment (env : Environment) = 
+        provider.SaveEnvironments [env]
+
+    let deleteEnvironment (id : string) =
+        provider.DeleteEnvironment id
+
+    let getEnvironments() = 
+        provider.GetEnvironments([])
+    
+    let saveAgent (environmentId : string) (agent : Agent) = 
+        let env = getEnvironment environmentId
+        env.AddAgents([agent])
+        saveEnvironment(env)
+        provider.SaveAgents([agent])
+
+    let getAgents() = 
+        provider.GetAgents([])
+
+    let deleteAgent (id : string) =
+        provider.DeleteAgent(id)
+
+    let getAgent (id : string) =
+        provider.GetAgents [id] |> Seq.head
+
+
---
+++ src/deploy.web/Fake.Deploy.Web.DataProviders/Fake.Deploy.Web.RavenDb/DataProvider.fs
@@ -0,0 +1,102 @@
+﻿namespace Fake.Deploy.Web.RavenDb
+
+open System
+open Fake.Deploy.Web
+open Raven.Imports.Newtonsoft.Json
+open Raven.Client.Embedded
+open Raven.Client
+
+module internal Provider = 
+        
+    let load<'a>(ids : string seq) (documentStore : IDocumentStore) =
+        use session = documentStore.OpenSession()   
+        session.Load<'a>(ids)
+        |> Array.map box
+        |> Array.choose (function
+                       | null -> None
+                       | a -> Some(a |> unbox<'a>))
+
+    let save (instances : seq<_>) (documentStore : IDocumentStore)  =
+        use session = documentStore.OpenSession()
+        let count = ref 0
+        for inst in instances do
+            session.Store(inst)
+            incr(count)
+            if !count > 29 then session.SaveChanges()
+        session.SaveChanges()
+
+    let delete<'a> (ids : seq<string>) (documentStore : IDocumentStore) =
+        use session = documentStore.OpenSession()
+        let count = ref 0
+        for id in ids do
+            let x = session.Load<'a>(id)        
+            session.Delete(x)
+            if !count > 29 then session.SaveChanges()
+        session.SaveChanges()
+
+    let getEnvironment (id : seq<string>) = 
+        load<Environment> id
+
+    let saveEnvironment (env : Environment)  = 
+        save [env]
+       
+    let deleteEnvironment (id : string) =
+        delete<Environment> [id]
+
+    let getEnvironments (documentStore : IDocumentStore) = 
+        use session = documentStore.OpenSession()
+        query {
+            for env in session.Query<Environment>() do
+            select env
+        } 
+        |> Seq.sortBy (fun e -> e.Id)
+        |> Seq.toArray
+    
+    let saveAgent (environmentId : string) (agent : Agent) (documentStore : IDocumentStore) = 
+        use session = documentStore.OpenSession()
+        let env = session.Load<Environment>(environmentId)
+        env.AddAgents([agent])
+        session.Store(env)
+        session.Store(agent)
+        session.SaveChanges()
+
+    let getAgents (documentStore : IDocumentStore) = 
+        use session = documentStore.OpenSession()
+        query {
+            for env in session.Query<Agent>() do
+            select env
+        } 
+        |> Seq.sortBy (fun e -> e.Id)
+        |> Seq.toArray
+
+    let deleteAgent (id : string) =
+        delete<Agent> [id]
+
+    let getAgent ids =
+        load<Agent> ids
+
+type RavenDbDataProvider() = 
+
+    let mutable documentStore : IDocumentStore = 
+       
+
+    interface IDataProvider with
+        member x.Initialize() =
+            documentStore <- 
+                 let ds = new EmbeddableDocumentStore(ConnectionStringName = "RavenDB")
+                 ds.Conventions.IdentityPartsSeparator <- "-"
+                 ds.Conventions.CustomizeJsonSerializer <- new Action<_>(fun s -> s.Converters.Add(new RavenUnionTypeConverter()))
+                 ds.Initialize()
+                
+        member x.GetEnvironments(ids) =
+            match ids |> Seq.toList with
+            | [] -> Provider.getEnvironments()
+            | a -> Provider.getEnvironment ids
+        member x.SaveEnvironments(envs) = Provider.save envs
+        member x.DeleteEnvironment(id) = Provider.deleteEnvironment id
+        member x.GetAgents(ids) =
+            match ids |> Seq.toList with
+            | [] -> Provider.getAgents()
+            | a -> Provider.getAgent ids
+        member x.SaveAgents(agents) = Provider.save agents
+        member x.DeleteAgent(id) = Provider.deleteAgent id
