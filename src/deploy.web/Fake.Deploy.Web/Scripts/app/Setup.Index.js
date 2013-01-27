3045058125a1356fe8bc843cf13df3fff71b80ed
Colin <colinbul@googlemail.com>
2013-01-27 18:12:43 +0000

    Improved setup feedback

---
+++ src/deploy.web/Fake.Deploy.Web/Scripts/app/Setup.Index.js
@@ -0,0 +1,21 @@
+﻿function IndexViewModel() {
+    var self = this;
+
+    self.setup = function (form) {
+        $('#initDialog').modal('show');
+        var data = $(form).serializeObject();
+        var jsonStr = JSON.stringify(data);
+        $.ajax({
+            type: "POST",
+            url: '/Setup/SaveSetupInformation',
+            data: jsonStr,
+            contentType: 'application/json'
+        }).done(function (d) {
+            $('#initDialog').modal('hide');
+            window.navigate('/Home/Index');
+        }).fail(function (msg) {
+            toastr.error('Initialisation Failed', 'Error');
+            $('#initDialog').modal('hide');
+        });
+    }
+}
