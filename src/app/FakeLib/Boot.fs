1fcd8369a226ed1348be6685498aa3d3bf5fa5a2
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2013-04-03 11:43:45 -0400

    Fixed top sort to sort assemblies by dependency order for #r directive to work properly. This implies a dep on Mono.Cecil

--- src/app/FakeLib/Boot.fs
+++ src/app/FakeLib/Boot.fs
@@ -74,7 +74,7 @@ type Config =
             IncludesFile = sourceDir +/ ".build" +/ "boot.fsx"
             NuGetDependencies = []
             NuGetPackagesDirectory = sourceDir +/ "packages"
-            NuGetSourceUrl = "https://nuget.org/api/v2/"
+            NuGetSourceUrl = "http://nuget.org/api/v2/"
             SourceDirectory = sourceDir
         }
 
@@ -84,7 +84,8 @@ module private Implementation =
     open System.Collections.Generic
 
     let GetManager (config: Config) =
-        let repo = PackageRepositoryFactory.Default.CreateRepository(config.NuGetSourceUrl)
+        let factory = PackageRepositoryFactory.Default
+        let repo = factory.CreateRepository(config.NuGetSourceUrl)
         PackageManager(repo, config.NuGetPackagesDirectory)
 
     let Install (mgr: PackageManager) (dep: NuGetDependency) : unit =
@@ -96,41 +97,103 @@ module private Implementation =
         | SemanticVersion v ->
             mgr.InstallPackage(dep.PackageId, SemanticVersion.Parse v)
 
-    let FindPackage (mgr: PackageManager) (dep: NuGetDependency) =
-        let repo = mgr.LocalRepository
-        match dep.Version with
-        | Latest ->
-            repo.FindPackage(dep.PackageId)
-        | LatestPreRelease ->
-            repo.FindPackage(dep.PackageId, Unchecked.defaultof<SemanticVersion>, true, true)
-        | SemanticVersion v ->
-            repo.FindPackage(dep.PackageId, SemanticVersion.Parse v)
+    let TopologicalSort<'K,'T when 'K : equality>
+            (getKey: 'T -> 'K)
+            (getPreceding: 'T -> seq<'T>)
+            (roots: seq<'T>) : seq<'T> =
+        let visited = HashSet()
+        let trace = Queue()
+        let rec visit (node: 'T) =
+            let key = getKey node
+            if visited.Add(key) then
+                Seq.iter visit (getPreceding node)
+                trace.Enqueue(node)
+        Seq.iter visit roots
+        trace.ToArray() :> seq<_>
+
+    let Memoize (f: 'A -> 'B) : ('A -> 'B) =
+        let d = Dictionary()
+        fun k ->
+            match d.TryGetValue(k) with
+            | true, v -> v
+            | _ ->
+                let v = f k
+                d.[k] <- v
+                v
+
+    let SortAssemblyFilesByReferenceOrder (paths: seq<string>) : seq<string> =
+        let getDefn =
+            Memoize (fun (path: string) ->
+                Mono.Cecil.AssemblyDefinition.ReadAssembly(path))
+        let getFullName path =
+            let aD = getDefn path
+            aD.FullName
+        let getPreceding path =
+            let aD = getDefn path
+            if aD.MainModule.HasAssemblyReferences then
+                paths
+                |> Seq.filter (fun path ->
+                    let fullName = getFullName path
+                    aD.MainModule.AssemblyReferences
+                    |> Seq.exists (fun ref ->
+                        ref.FullName = fullName))
+            else Seq.empty
+        TopologicalSort getFullName getPreceding paths
+
+    let MostRecent (pkgs: seq<IPackage>) : seq<IPackage> =
+        pkgs
+        |> Seq.groupBy (fun pkg -> pkg.Id)
+        |> Seq.map (fun (_, packages) ->
+            packages
+            |> Seq.maxBy (fun pkg -> pkg.Version))
+
+    let CompleteAndSortPackages
+            (config: Config)
+            (mgr: PackageManager)
+            (pkgs: seq<IPackage>) : seq<IPackage> =
+        let getKey (pkg: IPackage) = pkg.Id
+        let getPreceding (pkg: IPackage) =
+            pkg.GetCompatiblePackageDependencies(config.FrameworkName)
+            |> Seq.choose (fun dep ->
+                let r = mgr.LocalRepository
+                let pkg = r.FindPackage(dep.Id, dep.VersionSpec, true, true)
+                match pkg with
+                | null -> None
+                | _ -> Some pkg)
+            |> MostRecent
+        TopologicalSort getKey getPreceding pkgs
+
+    let IsRequiredPackage (config: Config) (pkg: IPackage) : bool =
+        config.NuGetDependencies
+        |> List.exists (fun dep ->
+            dep.PackageId = pkg.Id
+            &&
+            match dep.Version with
+            | Latest -> pkg.IsReleaseVersion()
+            | LatestPreRelease -> true
+            | SemanticVersion v -> SemanticVersion.Parse(v) = pkg.Version)
+
+    let IsSupported (config: Config) (frameworks: seq<FrameworkName>) =
+        frameworks
+        |> Seq.exists (fun x -> x = config.FrameworkName)
 
     let ComputeRefs (config: Config) (mgr: PackageManager) : seq<string> =
         let assemblyRefs = HashSet()
         let pkgRefs = Queue()
-        let visitedPackages = HashSet()
-        let refs = Queue()
-        let rec visitPackage (pkg: IPackage) =
-            let pkgKey = (pkg.Id, pkg.Version)
-            if visitedPackages.Add(pkgKey) then
-                let dir = mgr.PathResolver.GetPackageDirectory(pkg)
-                for pd in pkg.GetCompatiblePackageDependencies(config.FrameworkName) do
-                    mgr.LocalRepository.FindPackage(pd.Id, pd.VersionSpec, true, true)
-                    |> visitPackage
-                for r in pkg.FrameworkAssemblies do
-                    if  r.SupportedFrameworks
-                        |> Seq.exists (fun x -> x = config.FrameworkName)
-                    then
-                        assemblyRefs.Add(r.AssemblyName) |> ignore
-                for r in pkg.AssemblyReferences do
-                    if  r.SupportedFrameworks
-                        |> Seq.exists (fun x -> x = config.FrameworkName)
-                    then
-                        pkgRefs.Enqueue(config.NuGetPackagesDirectory +/ dir +/ r.Path)
-        for d in config.NuGetDependencies do
-            let pkg = FindPackage mgr d
-            visitPackage pkg
+        mgr.LocalRepository.GetPackages()
+        |> Seq.filter (IsRequiredPackage config)
+        |> MostRecent
+        |> CompleteAndSortPackages config mgr
+        |> Seq.iter (fun pkg ->
+            let dir = mgr.PathResolver.GetPackageDirectory(pkg)
+            pkg.FrameworkAssemblies
+            |> Seq.filter (fun r -> IsSupported config r.SupportedFrameworks)
+            |> Seq.iter (fun r -> assemblyRefs.Add(r.AssemblyName) |> ignore)
+            pkg.AssemblyReferences
+            |> Seq.filter (fun r -> IsSupported config r.SupportedFrameworks)
+            |> Seq.map (fun r -> config.NuGetPackagesDirectory +/ dir +/ r.Path)
+            |> SortAssemblyFilesByReferenceOrder
+            |> Seq.iter pkgRefs.Enqueue)
         Seq.append assemblyRefs pkgRefs
 
     let GenerateBootScriptText (refs: seq<string>) : string =
@@ -151,11 +214,13 @@ module private Implementation =
             writeRef r
         w.ToString()
 
+    let UTF8 = UTF8Encoding(false)
+
     let MakeBootScriptFile (config: Config) (refs: seq<string>) : unit =
         let dir = Path.GetDirectoryName(config.IncludesFile)
         if not (Directory.Exists dir) then
             Directory.CreateDirectory dir |> ignore
-        File.WriteAllText(config.IncludesFile, GenerateBootScriptText refs, UTF8Encoding(false))
+        File.WriteAllText(config.IncludesFile, GenerateBootScriptText refs, UTF8)
 
 /// The main function intended to be executed in the BOOT phase of
 /// boostrapping scripts.
