67ba0ce67272ebd544dfc7636d7cad9e5ccc8266
Morgan Persson <lazydev@gmail.com>
2013-05-16 16:59:34 +0200

    List all msbuild errors in exception raised when msbuild fails

--- src/app/FakeLib/MsBuildLogger.fs
+++ src/app/FakeLib/MsBuildLogger.fs
@@ -4,18 +4,45 @@ open Microsoft.Build
 open Microsoft.Build.Framework
 open System
 open System.Collections.Generic
+open System.IO
 
-type TeamCityLogger () =
+type MSBuildLogger () = 
     let mutable Verbosity = LoggerVerbosity.Normal
     let mutable Parameters = ""
+
+    abstract member RegisterEvents : IEventSource -> unit
+    default t.RegisterEvents e = ()
+
+    member this.errToStr (a:BuildErrorEventArgs) = 
+        sprintf "%s: %s(%d,%d): %s [%s]" a.Code a.File a.LineNumber a.ColumnNumber a.Message a.ProjectFile
 
     interface ILogger with
         member this.Parameters with get() = Parameters and set(value) = Parameters <- value
         member this.Verbosity with get() = Verbosity and set(value) = Verbosity <- value
         member this.Shutdown () = ()
-        member this.Initialize(eventSource) = 
-            eventSource.ErrorRaised.Add(fun a ->
-                let str = sprintf "%s(%d,%d): ERROR %s: `%s` [%s]" a.File a.LineNumber a.ColumnNumber a.Code a.Message a.ProjectFile
-                printfn "==> %s" str
-                str |> TeamCityHelper.sendTeamCityError
-            )
+        member this.Initialize(eventSource) = this.RegisterEvents(eventSource)
+
+type TeamCityLogger () =
+    inherit MSBuildLogger()
+        override this.RegisterEvents(eventSource) = 
+            eventSource.ErrorRaised.Add(fun a -> this.errToStr a |> TeamCityHelper.sendTeamCityError )
+
+let ErrorLoggerFile = Path.Combine(Path.GetTempPath(), "Fake.Errors.txt")
+
+type ErrorLogger () =
+    inherit MSBuildLogger()
+
+    let errors = new List<BuildErrorEventArgs>()
+
+    
+    override this.RegisterEvents(eventSource) = 
+        eventSource.ErrorRaised.Add(fun a -> errors.Add a)
+
+        eventSource.BuildFinished.Add(fun a ->
+            let errMsg = 
+                errors 
+                |> Seq.map this.errToStr
+                |> fun e -> String.Join(Environment.NewLine, e)
+                |> fun e -> if a.Succeeded then "" else e
+            File.WriteAllText(ErrorLoggerFile, errMsg)
+        )
