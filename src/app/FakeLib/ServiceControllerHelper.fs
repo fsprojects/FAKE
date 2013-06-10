7d4fd310a0c62e12b2532d7da0ac0d453f0a0991
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-06-10 10:22:18 +0200

    If a service is started or stopped wait for process completion - cleanup

--- src/app/FakeLib/ServiceControllerHelper.fs
+++ src/app/FakeLib/ServiceControllerHelper.fs
@@ -1,53 +1,53 @@
-﻿namespace Fake
+﻿[<AutoOpen>]
+module Fake.ServiceControllerHelpers
 
-[<AutoOpen>]
-module ServiceControllerHelpers = 
-    
-    open System
-    open System.ServiceProcess
+open System
+open System.ServiceProcess
+open System.Threading
 
-    let checkServiceExists name = 
-        ServiceController.GetServices() |> Seq.exists (fun s -> s.DisplayName = name) 
+let getServices name = ServiceController.GetServices() |> Seq.filter (fun s -> s.DisplayName = name)
+let getService name = ServiceController.GetServices() |> Seq.tryFind (fun s -> s.DisplayName = name)     
+let checkServiceExists name = ServiceController.GetServices() |> Seq.exists (fun s -> s.DisplayName = name) 
 
-    let startService name =
-        ServiceController.GetServices() 
-        |> Seq.filter (fun s -> s.DisplayName = name)
-        |> Seq.iter (fun s -> if s.Status <> ServiceControllerStatus.Running then s.Start())
+let startService name =
+    getServices name
+    |> Seq.iter (fun s -> if s.Status <> ServiceControllerStatus.Running then s.Start())
 
-    let ensureServiceHasStarted name timeout =
-        let startTime = DateTime.Now 
-        let endTime = startTime.Add(timeout)
-        let mutable result = false
-        let mutable continueLooping = true
-        tracefn "Waiting for %s to start (Timeout: %A secs)" name timeout.TotalSeconds
-        while DateTime.Now < endTime && continueLooping do
-            
-            let service = ServiceController.GetServices() 
-                          |> Seq.tryFind (fun s -> s.DisplayName = name)
-            result <- match service with
-                      | Some(sc) -> sc.Status = ServiceControllerStatus.Running
-                      | None -> failwith "Could not find service %s" name
-            continueLooping <- not(result) 
-            System.Threading.Thread.Sleep(1000)
-        if result then ()
-        else failwithf "The service %s has not been started (check the logs for errors)" name
+let stopService name =
+    getServices name
+    |> Seq.iter (fun s -> if s.Status <> ServiceControllerStatus.Stopped then s.Stop())
+
+let ensureServiceHasStarted name timeout =
+    let endTime = DateTime.Now.Add timeout
+    let mutable continueLooping = true
+    tracefn "Waiting for %s to start (Timeout: %A secs)" name timeout.TotalSeconds
+
+    let checkStatus() = 
+        match getService name with
+        | Some sc -> sc.Status = ServiceControllerStatus.Running
+        | None -> failwith "Could not find service %s" name
+
+    while DateTime.Now < endTime && continueLooping do
+        continueLooping <- not (checkStatus())
+        Thread.Sleep 1000
+
+    if continueLooping then 
+        failwithf "The service %s has not been started (check the logs for errors)" name
 
 
-    let ensureServiceHasStopped name timeout =
-        let startTime = DateTime.Now 
-        let endTime = startTime.Add(timeout)
-        let mutable result = false
-        let mutable continueLooping = true
-        tracefn "Waiting for %s to stop (Timeout: %A secs)" name timeout.TotalSeconds
-        while DateTime.Now < endTime && continueLooping do
-            
-            let service = ServiceController.GetServices() 
-                          |> Seq.tryFind (fun s -> s.DisplayName = name)
-            result <- match service with
-                      | Some(sc) -> sc.Status = ServiceControllerStatus.Stopped
-                      | None -> true
-            continueLooping <- not(result) 
-            System.Threading.Thread.Sleep(1000)
+let ensureServiceHasStopped name timeout =
+    let endTime = DateTime.Now.Add timeout
+    let mutable continueLooping = true
+    tracefn "Waiting for %s to stop (Timeout: %A secs)" name timeout.TotalSeconds
 
-        if result then ()
-        else failwithf "The service %s has not stopped (check the logs for errors)" name
+    let checkStatus() = 
+        match getService name with
+        | Some sc -> sc.Status = ServiceControllerStatus.Stopped
+        | None -> true
+
+    while DateTime.Now < endTime && continueLooping do
+        continueLooping <- not (checkStatus())
+        Thread.Sleep 1000
+
+    if continueLooping then 
+        failwithf "The service %s has not stopped (check the logs for errors)" name
