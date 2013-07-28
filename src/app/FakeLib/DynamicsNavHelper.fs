d196c8f16b1c73061a619e6b9cdec2c86ab782ac
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-07-28 17:24:53 +0200

    Dynamics NAV - match all kinds of line breaks -fox

--- src/app/FakeLib/DynamicsNavHelper.fs
+++ src/app/FakeLib/DynamicsNavHelper.fs
@@ -313,8 +313,8 @@ let DateRegex = new Regex(@"\r\n\s\s\s\sDate\=(?<Date>[^;]*);(?:\r\n|\r|\n)", Re
 let TimeRegex = new Regex(@"\r\n\s\s\s\sTime\=(?<Time>[^;]*);(?:\r\n|\r|\n)", RegexOptions.Compiled)
 
 let replaceDateTimeInString (dateTime:DateTime) text = 
-    let t1 = DateRegex.Replace(text, String.Format("\r\n    Date={0};(?:\r\n|\r|\n)", dateTime.Date.ToString("dd.MM.yy")))
-    TimeRegex.Replace(t1, String.Format("\r\n    Time={0};(?:\r\n|\r|\n)", dateTime.ToString("HH:mm:ss")))
+    let t1 = DateRegex.Replace(text, String.Format("\r\n    Date={0};\r\n", dateTime.Date.ToString("dd.MM.yy")))
+    TimeRegex.Replace(t1, String.Format("\r\n    Time={0};\r\n", dateTime.ToString("HH:mm:ss")))
 
 let removeModifiedFlag text = ModifiedRegex.Replace(text, String.Empty)
 
