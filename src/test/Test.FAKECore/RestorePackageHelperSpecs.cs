dd8a0506faf5ad9cba554901cd9106e6555b5bed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-08-06 17:58:07 +0200

    Resharper cleanup

--- src/test/Test.FAKECore/RestorePackageHelperSpecs.cs
+++ src/test/Test.FAKECore/RestorePackageHelperSpecs.cs
@@ -4,29 +4,11 @@ using Machine.Specifications;
 using Microsoft.FSharp.Collections;
 using Microsoft.FSharp.Core;
 
-
 namespace Test.FAKECore
 {
     public class when_restoring_package_by_id
     {
-        private static string Run(RestorePackageHelper.RestoreSinglePackageParams packageParams)
-        {
-            Func<RestorePackageHelper.RestoreSinglePackageParams, RestorePackageHelper.RestoreSinglePackageParams> f = _ => packageParams;
-            var fs = FuncConvert.ToFSharpFunc(new Converter<RestorePackageHelper.RestoreSinglePackageParams, RestorePackageHelper.RestoreSinglePackageParams>(f));
-            var result = RestorePackageHelper.buildNuGetArgs(fs, "thePackage");
-            return result;
-        }
-
-        private static readonly TimeSpan OneMinute = new TimeSpan(0, 1, 0);
-
-        It should_restore_package_by_package_id_with_defaults =
-            () =>
-            {
-                var packageParams = RestorePackageHelper.RestoreSinglePackageDefaults;
-                var result = Run(packageParams);
-                result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
-                result.ShouldEndWith("\\test\\packages\"");
-            };
+        static readonly TimeSpan OneMinute = new TimeSpan(0, 1, 0);
 
         It should_restore_package_by_package_id_with_ExcludeVersion =
             () =>
@@ -42,6 +24,38 @@ namespace Test.FAKECore
                 var result = Run(packageParams);
                 result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
                 result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\"");
+            };
+
+        It should_restore_package_by_package_id_with_ExcludeVersion_and_PreReleasePackage =
+            () =>
+            {
+                var packageParams = new RestorePackageHelper.RestoreSinglePackageParams(
+                    "NuGet.exe",
+                    FSharpList<string>.Empty,
+                    OneMinute,
+                    @".\myPackageFolder\",
+                    new FSharpOption<Version>(new Version("1.2.3.4")),
+                    true,
+                    true);
+                var result = Run(packageParams);
+                result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
+                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\" \"-PreRelease\"");
+            };
+
+        It should_restore_package_by_package_id_with_PreReleasePackage =
+            () =>
+            {
+                var packageParams = new RestorePackageHelper.RestoreSinglePackageParams(
+                    "NuGet.exe",
+                    FSharpList<string>.Empty,
+                    OneMinute,
+                    @".\myPackageFolder\",
+                    null,
+                    false,
+                    true);
+                var result = Run(packageParams);
+                result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
+                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-PreRelease\"");
             };
 
         It should_restore_package_by_package_id_with_Version =
@@ -76,36 +90,21 @@ namespace Test.FAKECore
                 result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\" \"-Version\" \"1.2.3.4\"");
             };
 
-        It should_restore_package_by_package_id_with_PreReleasePackage =
+        It should_restore_package_by_package_id_with_defaults =
             () =>
             {
-                var packageParams = new RestorePackageHelper.RestoreSinglePackageParams(
-                    "NuGet.exe",
-                    FSharpList<string>.Empty,
-                    OneMinute,
-                    @".\myPackageFolder\",
-                    null,
-                    false,
-                    true);
+                var packageParams = RestorePackageHelper.RestoreSinglePackageDefaults;
                 var result = Run(packageParams);
                 result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
-                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-PreRelease\"");
+                result.ShouldEndWith("\\test\\packages\"");
             };
 
-        It should_restore_package_by_package_id_with_ExcludeVersion_and_PreReleasePackage =
-            () =>
-            {
-                var packageParams = new RestorePackageHelper.RestoreSinglePackageParams(
-                    "NuGet.exe",
-                    FSharpList<string>.Empty,
-                    OneMinute,
-                    @".\myPackageFolder\",
-                    new FSharpOption<Version>(new Version("1.2.3.4")),
-                    true,
-                    true);
-                var result = Run(packageParams);
-                result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
-                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\" \"-PreRelease\"");
-            };
+        static string Run(RestorePackageHelper.RestoreSinglePackageParams packageParams)
+        {
+            Func<RestorePackageHelper.RestoreSinglePackageParams, RestorePackageHelper.RestoreSinglePackageParams> f = _ => packageParams;
+            var fs = FuncConvert.ToFSharpFunc(new Converter<RestorePackageHelper.RestoreSinglePackageParams, RestorePackageHelper.RestoreSinglePackageParams>(f));
+            var result = RestorePackageHelper.buildNuGetArgs(fs, "thePackage");
+            return result;
+        }
     }
 }
