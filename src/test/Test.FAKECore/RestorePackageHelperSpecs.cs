66b3a2ec03ef6bbbcad6e58bc122c9151067af8d
Sergey Shishkin <sergei.shishkin@gmail.com>
2013-08-06 17:08:33 +0200

    Fix NuGet command line parameter PreRelease

--- src/test/Test.FAKECore/RestorePackageHelperSpecs.cs
+++ src/test/Test.FAKECore/RestorePackageHelperSpecs.cs
@@ -89,7 +89,7 @@ namespace Test.FAKECore
                     true);
                 var result = Run(packageParams);
                 result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
-                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-IncludePreRelease\"");
+                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-PreRelease\"");
             };
 
         It should_restore_package_by_package_id_with_ExcludeVersion_and_PreReleasePackage =
@@ -105,7 +105,7 @@ namespace Test.FAKECore
                     true);
                 var result = Run(packageParams);
                 result.ShouldStartWith(" \"install\" \"thePackage\" \"-OutputDirectory\" \"");
-                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\" \"-IncludePreRelease\"");
+                result.ShouldEndWith("\\test\\myPackageFolder\\\" \"-ExcludeVersion\" \"-PreRelease\"");
             };
     }
 }
