c281a0e6bd05990fd702a76c91cafd6ec1cd2573
Morgan Persson <lazydev@gmail.com>
2013-09-07 20:52:29 +0200

    Say 'Quering status...' instead of 'Offline' before getting agents status.

--- src/deploy.web/Fake.Deploy.Web/Scripts/app/Home.Agent.js
+++ src/deploy.web/Fake.Deploy.Web/Scripts/app/Home.Agent.js
@@ -1,4 +1,9 @@
-﻿function AgentStatusModel(available, msg) {
+﻿var agentStatus = {
+    Unknown: '#C0C0C0',
+    Online: 'green',
+    Offline: 'red'
+};
+function AgentStatusModel(available, msg) {
     var self = this;
 
     self.statusMessage = ko.observable(msg);
@@ -9,7 +14,7 @@ function AgentViewModel() {
     var self = this;
 
     self.agent = ko.observable();
-    self.agentStatus = ko.observable(new AgentStatusModel(false, 'Offline / Unreachable'));
+    self.agentStatus = ko.observable(new AgentStatusModel(agentStatus.Unknown, 'Querying status...'));
     self.deployments = ko.observableArray();
     self.agentDetails = ko.observable();
 
@@ -24,10 +29,10 @@ function AgentViewModel() {
         }).done(function (data) {
             var a = ko.mapping.fromJS(data);
             self.agentDetails(a);
-            self.agentStatus(new AgentStatusModel(true, 'Online'));
+            self.agentStatus(new AgentStatusModel(agentStatus.Online, 'Online'));
             self.refreshDeploymentsForAgent();
         }).fail(function (msg) {
-            self.agentStatus(new AgentStatusModel(false, 'Offline / Unreachable'));
+            self.agentStatus(new AgentStatusModel(agentStatus.Offline, 'Offline / Unreachable'));
         });
     };
 
@@ -94,7 +99,7 @@ function AgentViewModel() {
                 self.recentMessages([]);
                 if (data != null) {
                     $.each(data.result.Messages, function (i, msg) {
-                        self.recentMessages.push(msg)
+                        self.recentMessages.push(msg);
                     });
                 }
 
@@ -108,7 +113,7 @@ function AgentViewModel() {
                 self.recentMessages([]);
                 if (data != null) {
                     $.each(data.result.Messages, function (i, msg) {
-                        self.recentMessages.push(msg)
+                        self.recentMessages.push(msg);
                     });
                 }
             }
@@ -134,7 +139,7 @@ function AgentViewModel() {
                 self.recentMessages([]);
                 if (d != null) {
                     $.each(d.Messages, function (i, msg) {
-                        self.recentMessages.push(ko.mapping.fromJS(msg))
+                        self.recentMessages.push(ko.mapping.fromJS(msg));
                     });
                 }
                 self.refreshDeploymentsForAgent();
@@ -144,7 +149,7 @@ function AgentViewModel() {
                 if (d != null) {
                     toastr.error(data.appName + ' rollback failed: ' + msg.statusText, 'Error');
                     $.each(d.Messages, function (i, msg) {
-                        self.recentMessages.push(ko.mapping.fromJS(msg))
+                        self.recentMessages.push(ko.mapping.fromJS(msg));
                     });
 
                     self.recentMessages.push(
