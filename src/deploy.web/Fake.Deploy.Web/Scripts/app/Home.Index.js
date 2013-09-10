106031fb8687d792f1b22880e70c5236d6fa80e2
Morgan Persson <lazydev@gmail.com>
2013-09-10 17:11:14 +0200

    show agent status on home page

--- src/deploy.web/Fake.Deploy.Web/Scripts/app/Home.Index.js
+++ src/deploy.web/Fake.Deploy.Web/Scripts/app/Home.Index.js
@@ -16,6 +16,20 @@
 
     self.environments = ko.observableArray();
 
+    self.getAgentStatus = function(agent) {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/agent/?agentId=' + agent.Id(),
+            //url: 'http://localhost:8080/' + 'fake/statistics',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (data) {
+            agent.Status("Online");
+        }).fail(function (msg) {
+            agent.Status("Offline");
+        });
+    };
+
     self.build = function() {
         $.ajax({
             type: "GET",
@@ -24,8 +38,14 @@
             contentType: 'application/json'
         }).done(function (data) {
             $.each(data, function (i, d) {
+                for (var j in d.Agents) {
+                    d.Agents[j].Status = "";
+                };
                 var inst = ko.mapping.fromJS(d);
                 self.environments.push(inst);
+                for (var j in inst.Agents()) {
+                    self.getAgentStatus(inst.Agents()[j]);
+                };
             });
             $('#agents li:nth-child(4n+1)').css('margin-left', '0');
             var tab = getActiveTab() || '#envTabs a:first';
