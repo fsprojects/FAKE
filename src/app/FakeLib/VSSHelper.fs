e6b2038a99ce6fa3cf33ba6096824d5a5cf69b61
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-16 17:41:24 +0200

    API docs for VSS helper, but stop using this ancient source control system. Really.

--- src/app/FakeLib/VSSHelper.fs
+++ src/app/FakeLib/VSSHelper.fs
@@ -1,6 +1,8 @@
 ﻿[<AutoOpen>]
-module Fake.VSSHelper 
+/// Contains helper functions for [Microsoft Visual SourceSafe](http://en.wikipedia.org/wiki/Microsoft_Visual_SourceSafe)
+module Fake.VSSHelper
 
+/// Retrieves the given label of the given project from Microsoft Visual SourceSafe
 let getVSSProjectWithLabel toolPath srcSafeIni username password project (localSpec:string) label =    
     let args' = 
         sprintf "get %s -R \"-GL%s\" -I-Y -Y%s,%s -GTM" 
@@ -19,6 +21,7 @@ let getVSSProjectWithLabel toolPath srcSafeIni username password project (localS
         info.WorkingDirectory <- currentDirectory
         info.Arguments <- args) System.TimeSpan.MaxValue true
     if result <> 0 then failwith "Could not get sources from VSS"
-    
+
+/// Retrieves the given project from Microsoft Visual SourceSafe    
 let getVSSProject toolPath srcSafeIni username password project localSpec = 
     getVSSProjectWithLabel toolPath srcSafeIni username password project localSpec ""
