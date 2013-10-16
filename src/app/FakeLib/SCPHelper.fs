ad9e5a426996c7373c95d7ada07df88de4fb59f9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-16 15:04:46 +0200

    SCPHelper docu

--- src/app/FakeLib/SCPHelper.fs
+++ src/app/FakeLib/SCPHelper.fs
@@ -1,34 +1,31 @@
-﻿namespace Fake
+﻿[<AutoOpen>]
+module Fake.SCPHelper 
 
+/// The SCP parameter type
 type SCPParams =
     { ToolPath:string;
       PrivateKeyPath:string}
 
-[<AutoOpen>]
-module SCPHelper =
+/// The SCP default parameters
+let SCPDefaults:SCPParams =
+    { ToolPath = "scp.exe"
+      PrivateKeyPath = null}
 
-    /// SCP default params  
-    let SCPDefaults:SCPParams =
-        { ToolPath = "scp.exe"
-          PrivateKeyPath = null}
-
-    /// <summary>Performs a SCP copy.</summary>
-    /// <param name="source">The source directory (fileName)</param>
-    /// <param name="destination">The target directory (fileName)</param>
-    let SCP setParams source destination =
-        let (p:SCPParams) = setParams SCPDefaults
-        tracefn "SCP %s %s" source destination
+/// Performs a SCP copy from the gicen source directory to the target directory
+let SCP setParams source target =
+    let (p:SCPParams) = setParams SCPDefaults
+    tracefn "SCP %s %s" source target
     
-        let args = 
-          sprintf "-r %s \".\" %s"
+    let args = 
+        sprintf "-r %s \".\" %s"
             (if isNullOrEmpty p.PrivateKeyPath then "" else sprintf "-i \"%s\"" p.PrivateKeyPath)
-            (destination |> toParam)
+            (toParam target)
 
-        tracefn "%s %s" p.ToolPath args
-        let result = 
-            ExecProcess (fun info ->
-                info.FileName <- p.ToolPath
-                info.WorkingDirectory <- source |> FullName
-                info.Arguments <- args) System.TimeSpan.MaxValue
+    tracefn "%s %s" p.ToolPath args
+    let result = 
+        ExecProcess (fun info ->
+            info.FileName <- p.ToolPath
+            info.WorkingDirectory <- source |> FullName
+            info.Arguments <- args) System.TimeSpan.MaxValue
                
-        if result <> 0 then failwithf "Error during SCP From: %s To: %s" source destination
+    if result <> 0 then failwithf "Error during SCP From: %s To: %s" source target
