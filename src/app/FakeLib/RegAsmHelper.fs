5eb79e6d64dcf7ab2300980af93c9d31b1841403
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-20 09:36:12 +0200

    Adding API docs for RegAsm task

--- src/app/FakeLib/RegAsmHelper.fs
+++ src/app/FakeLib/RegAsmHelper.fs
@@ -1,20 +1,26 @@
 ﻿[<AutoOpen>]
+/// Contains a task which can be used to run regasm .NET assembly
 module Fake.RegAsmHelper
 
 open System
 
-type RegAsmParams =
-    { ToolPath: string
-      WorkingDir:string
-      TimeOut: TimeSpan}
+/// RegAsm parameter type
+type RegAsmParams = { 
+    ToolPath: string
+    WorkingDir:string
+    TimeOut: TimeSpan}
 
-/// RegAsm default params  
-let RegAsmDefaults =
-    { ToolPath = @"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm.exe"
-      WorkingDir = "."
-      TimeOut = TimeSpan.FromMinutes 5. }
+/// RegAsm default params
+let RegAsmDefaults = { 
+    ToolPath = @"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm.exe"
+    WorkingDir = "."
+    TimeOut = TimeSpan.FromMinutes 5. }
 
 /// Runs regasm on the given lib
+/// ## Parameters
+/// 
+///  - `setParams` - Function used to manipulate the default RegAsm parameters.
+///  - `lib` - The assembly file name.
 let RegAsm setParams lib = 
     traceStartTask "RegAsm" lib
     let parameters = setParams RegAsmDefaults
