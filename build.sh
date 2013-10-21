f10140d63afa016332dfb31cc374ed9d1ff6962e
Heather <heather@cynede.net>
2013-10-21 10:26:06 +0400

    Correct build files for mono / *nix systems

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 #getting latest FAKE via NuGet
 mono tools/NuGet/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
+mono tools/NuGet/nuget.exe install FSharp.Formatting -OutputDirectory tools -ExcludeVersion
 
 #build FAKE
 mono tools/FAKE/tools/FAKE.exe build.fsx
