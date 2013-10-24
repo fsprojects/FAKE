db27dcefc33f6be0b9179ee8e734de13be05f883
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-24 17:11:38 +0200

    API docs for TimeoutHelper

--- src/app/FakeLib/TimeoutHelper.fs
+++ src/app/FakeLib/TimeoutHelper.fs
@@ -1,7 +1,14 @@
 ﻿[<AutoOpen>]
+/// Conatins a helper can be used to implement timeouts.
 module Fake.TimeoutHelper
 
 /// Waits until the given function returns true or the timeout is reached
+/// ## Parameters
+///
+///  - `f` - This function will be startet.
+///  - `timeout` - A System.TimeSpan representing the timeout.
+///  - `testMS` - An interval at which FAKE checks if the function has succeeded.
+///  - `timeoutF` - This function will be run if the timeout has been reached.
 let waitFor f timeout (testMS:int) timeoutF = 
     let watch = new System.Diagnostics.Stopwatch()
     watch.Start()    
