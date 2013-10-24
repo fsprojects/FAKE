63852d87c029c495e46ead9f3ca9d07e7c2d1c6c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-24 14:17:06 +0200

    API docs for ConfigurationHelper

--- src/app/FakeLib/ConfigurationHelper.fs
+++ src/app/FakeLib/ConfigurationHelper.fs
@@ -8,7 +8,9 @@ open System.Linq
 open System.Xml.Linq
 open System.Xml.XPath
 
-/// Reads a config file from the given file name
+/// Reads a config file into an XElement.
+/// ## Parameters
+///  - `fileName` - The file name of the config file.
 let readConfig fileName =
     use fileStream = File.OpenRead(fileName) 
     let configElement = XElement.Load fileStream
@@ -19,6 +21,11 @@ let private getElement config xpath =
     (Extensions.XPathSelectElement(config, xpath))
 
 /// Reads a config file from the given file name, replaces an attribute using the given xPath and writes it back.
+/// ## Parameters
+///  - `fileName` - The file name of the config file.
+///  - `xpath` - An XPath term which can be used to replace the attribute.
+///  - `attribute` - The attribute name for which the value should be replaced.
+///  - `value` - The new attribute value.
 let updateConfigSetting fileName xpath attribute value =
     let config = readConfig fileName
     let node = getElement config xpath
@@ -31,9 +38,17 @@ let updateConfigSetting fileName xpath attribute value =
         config.Save(fs)
 
 /// Reads a config file from the given file name, replaces the app setting value and writes it back.
-let updateAppSetting key value file =
-    updateConfigSetting file ("appSettings/add[@key='" + key + "']") "value" value
+/// ## Parameters
+///  - `key` - The AppSettings attribute key name for which the value should be replaced.
+///  - `value` - The new AppSettings attribute value.
+///  - `fileName` - The file name of the config file.
+let updateAppSetting key value fileName =
+    updateConfigSetting fileName ("appSettings/add[@key='" + key + "']") "value" value
 
-/// Reads a config file from the given file name, replaces the connection string value and writes it back.        
-let updateConnectionString connectionStringKey value file =
-    updateConfigSetting file ("connectionStrings/add[@name='" + connectionStringKey + "']") "connectionString" value
+/// Reads a config file from the given file name, replaces the connection string value and writes it back.   
+/// ## Parameters
+///  - `connectionStringKey` - The connection string key name for which the value should be replaced.
+///  - `value` - The new connection string value.
+///  - `fileName` - The file name of the config file.     
+let updateConnectionString connectionStringKey value fileName =
+    updateConfigSetting fileName ("connectionStrings/add[@name='" + connectionStringKey + "']") "connectionString" value
