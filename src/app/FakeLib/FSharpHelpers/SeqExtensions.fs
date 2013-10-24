17d85c837fea58c7a4fc8b4ef1bd7fe55206acc2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-24 10:15:03 +0200

    API docs for Seq

--- src/app/FakeLib/FSharpHelpers/SeqExtensions.fs
+++ src/app/FakeLib/FSharpHelpers/SeqExtensions.fs
@@ -1,14 +1,17 @@
 ﻿[<AutoOpen>]
+/// Contains extensions of the defaul Seq module.
 module Fake.Seq
 
-/// Returns true if the given element exists in the sequence
+/// Returns if the given element exists in the sequence
 let contains element = Seq.exists ((=) element)
 
+/// Tries to execute the given function on the given item and ignores possible exceptions.
 let performSafe f item =
     try
       f item
     with
     | exn -> ()
 
+/// Tries to execute the given function on all items in the collection and ignores possible exceptions.
 let performSafeOnEveryItem f = Seq.iter (performSafe f)
           
