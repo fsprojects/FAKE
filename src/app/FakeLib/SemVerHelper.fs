54cb934cae0b87eed7b3c808fe12b95a5d1a4de5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-25 22:34:27 +0200

    All SemVer comparing tests are passing.

--- src/app/FakeLib/SemVerHelper.fs
+++ src/app/FakeLib/SemVerHelper.fs
@@ -5,6 +5,7 @@ open System
 open System.Text.RegularExpressions
 
 /// Contains the version information.
+[<CustomEquality; CustomComparison>]
 type SemVerInfo =
     { /// MAJOR version when you make incompatible API changes.
       Major: int
@@ -19,9 +20,42 @@ type SemVerInfo =
     override x.ToString() =
         sprintf "%d.%d.%d" x.Major x.Minor x.Patch +
          (if isNotNullOrEmpty x.PreRelease || isNotNullOrEmpty x.Build then "-" + x.PreRelease else "") +
-         (if isNotNullOrEmpty x.Build then "." + x.Build else "") 
+         (if isNotNullOrEmpty x.Build then "." + x.Build else "")
 
+    override x.Equals(yobj) =
+        match yobj with
+        | :? SemVerInfo as y -> 
+            (x.Minor,x.Minor,x.Patch,x.PreRelease,x.Build) = 
+              (y.Minor,y.Minor,y.Patch,y.PreRelease,y.Build)
+        | _ -> false
+ 
+    override x.GetHashCode() = hash (x.Minor,x.Minor,x.Patch,x.PreRelease,x.Build)
+    interface System.IComparable with
+        member x.CompareTo yobj =
+            match yobj with
+            | :? SemVerInfo as y ->
+                if x.Major <> y.Major then compare x.Major y.Major else
+                if x.Minor <> y.Minor then compare x.Minor y.Minor else
+                if x.Patch <> y.Patch then compare x.Patch y.Patch else
+                if x.PreRelease = y.PreRelease && x.Build = y.Build  then 0 else
+                if x.PreRelease = "" && x.Build = "" then 1 else
+                if y.PreRelease = "" && y.Build = "" then -1 else
+                if x.PreRelease <> y.PreRelease then compare x.PreRelease y.PreRelease else
+                if x.Build <> y.Build then 
+                    match Int32.TryParse x.Build, Int32.TryParse y.Build with
+                    | (true,b1),(true,b2) -> compare b1 b2
+                    | _ -> compare x.Build y.Build 
+                else
+                    0
+            | _ -> invalidArg "yobj" "cannot compare values of different types"
 
+/// Parses the given version string into a SemVerInfo which can be printed using ToString() or compared
+/// according to the rules described in the [SemVer docs](http://semver.org/).
+/// ## Sample
+///
+///     parse "1.0.0-rc.1"   <  parse "1.0.0"     // true
+///     parse "1.2.3-alpha"  > parse "1.2.2"      // true
+///     parse "1.5.0-beta.2" > parse "1.5.0-rc.1" // false
 let parse version =
     let splitted = split '.' version
     let l = splitted.Length
@@ -35,5 +69,5 @@ let parse version =
       Minor = if l > 1 then Int32.Parse splitted.[1] else 0
       Patch = patch
       PreRelease = preRelease
-      Build = if l > 3 then splitted.[3] else ""
+      Build = if l > 3 then splitted.[3] else "" 
     }
