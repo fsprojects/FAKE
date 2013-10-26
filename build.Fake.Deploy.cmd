6beaf4ef38313f23dae06557dcb6290223a5fbfd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-26 11:58:54 +0200

    Splitting FAKE.sln

---
+++ build.Fake.Deploy.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+:Build
+cls
+if not exist tools\FAKE\tools\Fake.exe ( 
+	"tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
+)
+if not exist tools\FSharp.Formatting\lib\net40\FSharp.CodeFormat.dll ( 
+	"tools\nuget\nuget.exe" "install" "FSharp.Formatting" "-OutputDirectory" "tools" "-ExcludeVersion"
+)
+
+SET TARGET="Default"
+
+IF NOT [%1]==[] (set TARGET="%1")
+  
+"tools\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%" BuildDeploy
+
+rem Bail if we're running a TeamCity build.
+if defined TEAMCITY_PROJECT_NAME goto Quit
+
+rem Loop the build script.
+set CHOICE=nothing
+echo (Q)uit, (Enter) runs the build again
+set /P CHOICE= 
+if /i "%CHOICE%"=="Q" goto :Quit
+
+GOTO Build
+
+:Quit
+exit /b %errorlevel%
