0b9ccaf6ad43aae10ba9c73c7f0ff76c1b5c1433
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-28 13:54:51 +0100

    API docs git Merge

--- src/app/FakeLib/Git/Merge.fs
+++ src/app/FakeLib/Git/Merge.fs
@@ -1,32 +1,34 @@
 ﻿[<AutoOpen>]
+/// Contains helper functions which allow to deal with git merge.
 module Fake.Git.Merge
 
 open Fake
 open System.IO
 
-/// Gets the current merge message
+/// Gets the current merge message.
 let getMergeMessage repositoryDir =
     let file = (findGitDir repositoryDir).FullName + "\\MERGE_MSG"
     if File.Exists file then File.ReadAllText file else ""
 
+/// Allows git to use fast-forward merges
 let FastForwardFlag = "--ff"
 
+/// Forbids git to use fast-forward merges
 let NoFastForwardFlag = "--no-ff"
 
+/// Git merge option.
 type MergeType =
 | SameCommit
 | FirstNeedsFastForward
 | SecondNeedsFastForward
 | NeedsRealMerge
 
-/// <summary>
-/// Tests whether branches and their "origin" counterparts have diverged and need
-/// merging first.
-/// </summary>
+/// Tests whether branches and their "origin" counterparts have diverged and need merging first.
+/// ## Parameters
 ///
-/// <param name="repositoryDir">The path to the repository.</param>
-/// <param name="local">The local branch name.</param>
-/// <param name="remote">The remote branch name.</param>
+///  - `repositoryDir` - The git repository.
+///  - `local` - The local branch name.
+///  - `remote` - The remote branch name.
 let compareBranches repositoryDir local remote =
     let commit1 = getSHA1 repositoryDir local
     let commit2 = getSHA1 repositoryDir remote
@@ -37,6 +39,14 @@ let compareBranches repositoryDir local remote =
     | _  -> NeedsRealMerge
 
 /// Performs a merge of the given branch with the current branch
+/// ## Parameters
+///
+///  - `repositoryDir` - The git repository.
+///  - `flags` - Additional flags.
+///  - `branch` - The branch we want to merge in.
+/// ## Sample
+///
+///     merge @"C:\code\Fake" NoFastForwardFlag "master"
 let merge repositoryDir flags branch =
     sprintf "merge %s %s" flags branch
       |> gitCommand repositoryDir
