057d0c7dbbf6cd589ed5c68bfce35069dc49d968
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-29 12:04:22 +0100

    Add the inital version from https://github.com/colinbull/FSharp.Enterprise/

---
+++ src/app/FakeLib/Globbing/FileSystem.fs
@@ -0,0 +1,85 @@
+﻿namespace Fake
+
+// Copied from https://github.com/colinbull/FSharp.Enterprise/blob/master/src/FSharp.Enterprise/FileSystem.fs
+module FileSystem = 
+    
+    open System
+    open System.IO
+    open System.Runtime.Caching
+
+    let fullPathRelativeTo rootDir file = 
+        match Path.IsPathRooted(file) with
+        | true -> file
+        | false -> 
+             match rootDir with
+             | Some(root) -> Path.Combine(root, file)
+             | None -> Path.GetFullPath(file)
+        |> fun x -> new FileInfo(x)
+
+    let tryReadFile deserialiser (path:string) =
+        if File.Exists(path)
+        then Some (File.ReadAllText(path) |> deserialiser)
+        else None
+
+    let write serialiser (path:string,payload:'a) =
+        File.WriteAllText(path, serialiser payload)
+    
+    [<AutoOpen>]
+    module Search = 
+        
+        type T = {
+            Recurse : bool
+            Root : string
+            FilePattern : string
+        }
+
+        let private cleanUpPath path = 
+            if String.IsNullOrEmpty(path) 
+            then "/" else path
+
+        let private parse (str:string) =
+            let fname = Path.GetFileName(str)
+            let recursiveDir = str.Replace(fname, "").Trim([|'\\';'/'|])  
+            let dir, filePattern =     
+                let dir = recursiveDir.Replace("**", "") |> cleanUpPath
+                if Directory.Exists(Path.GetDirectoryName(dir))
+                then 
+                    let fi = FileInfo(str)
+                    if fi.Attributes = FileAttributes.Directory
+                    then fi.FullName, "*.*" 
+                    else fi.Directory.FullName, fname
+                else dir, fname
+            let isRecursive = recursiveDir.EndsWith("**")
+            let tempPath = 
+                if isRecursive then recursiveDir.Replace("**", "") else recursiveDir
+                |> cleanUpPath
+            let root = fullPathRelativeTo (Some <| Path.GetDirectoryName(Reflection.Assembly.GetEntryAssembly().Location)) tempPath
+            let result =  { Recurse = isRecursive; Root = root.FullName; FilePattern = filePattern }
+            result
+           
+
+        let DefaultSearcher (searchParams:T) =
+            if Directory.Exists(searchParams.Root)
+            then
+                if searchParams.Recurse
+                then Directory.EnumerateFileSystemEntries(searchParams.Root, searchParams.FilePattern, SearchOption.AllDirectories)
+                else Directory.EnumerateFileSystemEntries(searchParams.Root, searchParams.FilePattern)
+            else Seq.empty
+
+        let findFiles searcher pattern  =
+            parse pattern |> (defaultArg searcher DefaultSearcher)
+
+    let FileIO serialise deserialise = 
+        IO.IO
+            (tryReadFile deserialise)
+            (write serialise)  
+            File.Delete 
+            (findFiles None)
+
+    let CachedFileIO serialise deserialise expiry = 
+        IO.CachedIO 
+            expiry
+            (tryReadFile deserialise)
+            (write serialise)  
+            File.Delete 
+            (findFiles None)
---
+++ src/app/FakeLib/Globbing/IO.fs
@@ -0,0 +1,64 @@
+﻿namespace Fake
+
+// Copied from https://github.com/colinbull/FSharp.Enterprise/blob/master/src/FSharp.Enterprise/IO.fs
+module IO =
+    
+    open System
+    open System.Runtime.Caching
+    open Caching
+
+    type SearchPattern = string
+    type Filter = (string -> bool)
+
+    type IIO<'a> =
+        abstract member Write : string * 'a -> unit
+        abstract member Read : string -> 'a option
+        abstract member ReadAll : string * Filter -> seq<'a>
+        abstract member ReadAll : seq<SearchPattern> -> seq<'a>
+        abstract member Delete : string -> unit
+    
+    let IO readOp writeOp deleteOp searchOp =
+        { new IIO<_> with
+            member f.Write(path, payload) = (path,payload) |> writeOp 
+            member f.Read(path) = readOp path
+            member f.ReadAll(path, filter) = 
+                    seq {
+                        for file in (searchOp path) |> Seq.filter filter do
+                            match f.Read(file) with
+                            | Some(a) -> yield a 
+                            | None -> ()
+                    }
+            member f.ReadAll(patterns) = 
+                patterns
+                |> Seq.collect searchOp
+                |> Seq.choose f.Read
+            member f.Delete(path) = deleteOp path
+        }
+
+    let CachedIO (expiry:TimeSpan) readOp writeOp deleteOp searchOp =
+        let onRemoved reason path payload =
+            match reason with
+            | CacheEntryRemovedReason.Expired -> (path,payload) |> writeOp
+            | _ -> ()
+        let cache : ICache<string,'a> = Caching.ObjectCache onRemoved MemoryCache.Default
+        { new IIO<'a> with
+            member f.Write(path, payload) = cache.Set(path, SlidingExpiry(payload,expiry))
+            member f.Read(path) =
+                match cache.TryGet(path) with
+                | Some(v) -> Some v
+                | None -> readOp path
+            member f.ReadAll(path, filter) = 
+                seq {
+                    for file in (searchOp path) |> Seq.filter filter do
+                        match f.Read(file) with
+                        | Some(a) -> yield a 
+                        | None -> ()
+                }
+            member f.ReadAll(patterns) = 
+                patterns
+                |> Seq.collect (searchOp)
+                |> Seq.choose f.Read
+            member f.Delete(path) =
+               cache.Remove(path) |> ignore
+               deleteOp (path) 
+        }
