479026ce9fad9b738777e1d82258ebe46dd974ce
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-30 17:23:54 +0100

    when_scanning_for_txt_files_with_a_special_subfolder - failing test

--- src/test/Test.FAKECore/Globbing/TestSample3/GlobbingSample3Specs.cs
+++ src/test/Test.FAKECore/Globbing/TestSample3/GlobbingSample3Specs.cs
@@ -34,4 +34,12 @@ namespace Test.FAKECore.Globbing.TestSample3
         It should_find_names_text = () => Files[0].ShouldEndWith("Names.text");
         It should_find__names_txt_text = () => Files[1].ShouldEndWith("Names.txt.text");
     }
+
+    public class when_scanning_for_txt_files_with_a_special_subfolder : when_extracting_zip
+    {
+        Because of = () => Files = FileSystem.Search.find("temptest/**/SubFolder2/**/*.txt").ToArray();
+
+        It should_match_1_file = () => Files.Length.ShouldEqual(1);
+        It should_find_names_txt = () => Files[0].ShouldEndWith("Names.txt");
+    }
 }
