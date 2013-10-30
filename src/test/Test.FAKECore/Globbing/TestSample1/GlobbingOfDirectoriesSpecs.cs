9891a1e4f5522aee763fcbf9169bafc0bf18ec72
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-30 15:54:37 +0100

    Removing useless test

--- src/test/Test.FAKECore/Globbing/TestSample1/GlobbingOfDirectoriesSpecs.cs
+++ src/test/Test.FAKECore/Globbing/TestSample1/GlobbingOfDirectoriesSpecs.cs
@@ -64,13 +64,4 @@ namespace Test.FAKECore.Globbing.Directories
         It should_match_3_files = () => Files.Length.ShouldEqual(1);
         It should_find_sample_app = () => Files.First().ShouldEndWith("SampleApp.dll");
     }
-
-    public class when_scanning_in_zip_with_base_dir : when_extracting_zip
-    {
-        private Because of = () => Files = Globbing("*", "SampleApp/bin");
-
-        It should_match_3_files = () => Files.Length.ShouldEqual(3);
-        It should_find_ilmerge = () => Files[0].ShouldEndWith("ilmerge.exclude");
-        It should_find_sample_app = () => Files[1].ShouldEndWith("SampleApp.dll");
-    }
 }
