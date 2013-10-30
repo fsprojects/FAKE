011c0d06a97f79860915d373bef60d1adb84a719
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-10-30 17:44:23 +0100

    when_scanning_for_txt_files_in_any_subfolder

--- src/test/Test.FAKECore/Globbing/TestSample3/GlobbingSample3Specs.cs
+++ src/test/Test.FAKECore/Globbing/TestSample3/GlobbingSample3Specs.cs
@@ -7,40 +7,54 @@ namespace Test.FAKECore.Globbing.TestSample3
 {
     public class when_extracting_zip : BaseFunctions
     {
-        private const string TempDir = "temptest";
+        const string TempDir = "temptest";
+
+        protected static string[] Files;
 
         Establish context = () =>
         {
             FileHelper.CleanDir(TempDir);
             ZipHelper.Unzip(TempDir, "Globbing/TestSample3/Sample3.zip");
         };
-
-        protected static string[] Files;
     }
 
     public class when_scanning_for_txt_files : when_extracting_zip
     {
         Because of = () => Files = FileSystem.Search.find("temptest/**/*.txt").ToArray();
 
-        It should_match_1_file = () => Files.Length.ShouldEqual(1);
         It should_find_names_txt = () => Files[0].ShouldEndWith("Names.txt");
+        It should_match_1_file = () => Files.Length.ShouldEqual(1);
+    }
+
+    public class when_scanning_for_txt_files_in_any_subfolder : when_extracting_zip
+    {
+        static string _expectedFile =
+            FileSystemHelper.FullName(".\\temptest\\Folder1\\Subfolder1\\SubFolder2\\TextFiles\\Names.txt");
+
+        Because of = () => Files = FileSystem.Search.find("./**/*.txt").ToArray();
+
+        It should_find_names_txt = () => Files.ShouldContain(_expectedFile);
     }
 
     public class when_scanning_for_txt_files_using_backslash : when_extracting_zip
     {
         Because of = () => Files = FileSystem.Search.find("temptest\\**\\*.txt").ToArray();
 
-        It should_match_1_file = () => Files.Length.ShouldEqual(1);
         It should_find_names_txt = () => Files[0].ShouldEndWith("Names.txt");
+        It should_match_1_file = () => Files.Length.ShouldEqual(1);
     }
 
     public class when_scanning_for_text_files : when_extracting_zip
     {
         Because of = () => Files = FileSystem.Search.find("temptest/**/*.text").ToArray();
 
+        It should_find__names_txt_text =
+            () => Files[1].ShouldEndWith("Folder1\\Subfolder1\\SubFolder2\\TextFiles\\Names.txt.text");
+
+        It should_find_names_text =
+            () => Files[0].ShouldEndWith("Folder1\\Subfolder1\\SubFolder2\\TextFiles\\Names.text");
+
         It should_match_2_files = () => Files.Length.ShouldEqual(2);
-        It should_find_names_text = () => Files[0].ShouldEndWith("Folder1\\Subfolder1\\SubFolder2\\TextFiles\\Names.text");
-        It should_find__names_txt_text = () => Files[1].ShouldEndWith("Folder1\\Subfolder1\\SubFolder2\\TextFiles\\Names.txt.text");
     }
 
     public class when_scanning_for_txt_files_with_a_special_subfolder_which_doesnt_exist : when_extracting_zip
@@ -54,7 +68,7 @@ namespace Test.FAKECore.Globbing.TestSample3
     {
         Because of = () => Files = FileSystem.Search.find("temptest/**/SubFolder2/**/*.txt").ToArray();
 
-        It should_match_1_file = () => Files.Length.ShouldEqual(1);
         It should_find_names_txt = () => Files[0].ShouldEndWith("Names.txt");
+        It should_match_1_file = () => Files.Length.ShouldEqual(1);
     }
 }
