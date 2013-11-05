48fbb2c655d7c3db8a32a20e4e09884bc7cdb896
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-05 19:08:58 +0100

    Dog food the generated AssemblyInfo.AssemblyVersionInformation.Version

--- src/deploy.web/Fake.Deploy.Web.App/AssemblyInfo.fs
+++ src/deploy.web/Fake.Deploy.Web.App/AssemblyInfo.fs
@@ -1,31 +1,14 @@
-module Fake.Deploy.Web.App.AssemblyInfo
-#nowarn "49" // uppercase argument names
-#nowarn "67" // this type test or downcast will always hold
-#nowarn "66" // tis upast is unnecessary - the types are identical
-#nowarn "58" // possible incorrect indentation..
-#nowarn "57" // do not use create_DelegateEvent
-#nowarn "51" // address-of operator can occur in the code
-open System
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
-exception ReturnException183c26a427ae489c8fd92ec21a0c9a59 of obj
-exception ReturnNoneException183c26a427ae489c8fd92ec21a0c9a59
 
-[<assembly: ComVisible (false)>]
-
-[<assembly: CLSCompliant (false)>]
-
-[<assembly: Guid ("2B684E7B-572B-41C1-86C9-F6A11355570E")>]
-
-[<assembly: AssemblyTitle ("FAKE - F# Make Deploy Web App")>]
-
-[<assembly: AssemblyProduct ("FAKE - F# Make Deploy Web App")>]
-
-[<assembly: AssemblyVersion ("LocalBuild")>]
-
-[<assembly: AssemblyFileVersion ("LocalBuild")>]
-
-[<assembly: AssemblyDelaySign (false)>]
+[<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Web App")>]
+[<assembly: GuidAttribute("2B684E7B-572B-41C1-86C9-F6A11355570E")>]
+[<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 
 ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1"
--- src/deploy.web/Fake.Deploy.Web/AssemblyInfo.cs
+++ src/deploy.web/Fake.Deploy.Web/AssemblyInfo.cs
@@ -5,5 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Web")]
 [assembly: GuidAttribute("27BA7705-3F57-47BE-B607-8A46B27AE876")]
 [assembly: AssemblyProductAttribute("FAKE - F# Make")]
-[assembly: AssemblyVersionAttribute("0.2")]
-[assembly: AssemblyFileVersionAttribute("0.2")]
+[assembly: AssemblyVersionAttribute("0.0.1")]
+[assembly: AssemblyFileVersionAttribute("0.0.1")]
+namespace System {
+    internal static class AssemblyVersionInformation {
+        internal const string Version = "0.0.1";
+    }
+}
