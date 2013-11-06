0b89d842a6b39090a231a911727aff16258ded42
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-06 17:00:20 +0100

    Trying the ProjectSystem approach discussed in #216

---
+++ src/test/Test.FAKECore/ProjectSystemSpecs.cs
@@ -0,0 +1,15 @@
+﻿using Fake.MsBuild;
+using Machine.Specifications;
+using NuGet;
+
+namespace Test.FAKECore
+{
+    public class when_checking_for_files
+    {
+        static IProjectSystem _project;
+        Because of = () => _project = new ProjectSystem.ProjectSystem("ProjectTestFiles/FakeLib.proj") as IProjectSystem;
+
+        It should_find_the_semver_helper = () =>
+            _project.FileExistsInProject("SemVerHelper.fs").ShouldBeTrue();
+    }
+}
