b43f3db15efa9ec072cd7a7088b177f640badcb8
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-06 17:14:00 +0100

    Try to detect subfolders

--- src/app/FakeLib/MSBuild/ProjectSystem.fs
+++ src/app/FakeLib/MSBuild/ProjectSystem.fs
@@ -3,7 +3,10 @@ module Fake.MsBuild.ProjectSystem
 open Fake
 
 type ProjectSystem(projectFile : string) = 
-    let file = ReadFileAsString projectFile
+    let file = 
+        ReadFileAsString projectFile
+        |> replace "\\" "/"
 
     member x.FileExistsInProject(path : string) = 
-        file.Contains path
+        let path = replace "\\" "/" path
+        file.Contains (sprintf "<Compile Include=\"%s\" />" path)
--- src/test/Test.FAKECore/ProjectSystemSpecs.cs
+++ src/test/Test.FAKECore/ProjectSystemSpecs.cs
@@ -5,10 +5,19 @@ namespace Test.FAKECore
 {
     public class when_checking_for_files
     {
-        Because of = () => _project = new ProjectSystem.ProjectSystem("ProjectTestFiles/FakeLib.proj") ;
+        Because of = () => _project = new ProjectSystem.ProjectSystem("ProjectTestFiles/FakeLib.proj");
 
         It should_find_the_semver_helper = () =>
             _project.FileExistsInProject("SemVerHelper.fs").ShouldBeTrue();
+
+        It should_not_find_the_SpecsRemovement_helper = () =>
+            _project.FileExistsInProject("SpecsRemovement.fs").ShouldBeFalse();
+
+        It should_find_the_SpecsRemovement_helper_in_the_MSBuild_folder = () =>
+            _project.FileExistsInProject("MSBuild/SpecsRemovement.fs").ShouldBeTrue();
+
+        It should_find_the_SpecsRemovement_helper_in_the_MSBuild_folder_with_backslash = () =>
+            _project.FileExistsInProject("MSBuild\\SpecsRemovement.fs").ShouldBeTrue();
 
         It should_not_find_the_badadadum_helper = () =>
             _project.FileExistsInProject("badadadumHelper.fs").ShouldBeFalse();
