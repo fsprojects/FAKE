d6ceecb271bd84144c15f236b77463496862fed8
Colin <colinbul@googlemail.com>
2013-11-07 07:39:50 +0000

    Fixed up FileSystem searcher so all of the tests pass

--- src/app/FakeLib/Globbing/FileSystem.fs
+++ src/app/FakeLib/Globbing/FileSystem.fs
@@ -26,48 +26,45 @@ module FileSystem =
     
     module Search = 
         
-        type T = {
-            Recurse : bool
-            Root : string
-            FilePattern : string
-        }
-
-        let private cleanUpPath path = 
-            if String.IsNullOrEmpty(path) 
-            then "/" else path
-
-        let private parse (str:string) =
-            let fname = Path.GetFileName(str)
-            let recursiveDir = str.Replace(fname, "").Trim([|'\\';'/'|])  
-            let dir, filePattern =     
-                let dir = recursiveDir.Replace("**", "") |> cleanUpPath
-                if Directory.Exists(Path.GetDirectoryName(dir))
-                then 
-                    let fi = FileInfo(str.Replace("**", "").Replace("*","f"))
-                    if fi.Attributes = FileAttributes.Directory
-                    then fi.FullName, "*.*" 
-                    else fi.Directory.FullName, fname
-                else dir, fname
-            let isRecursive = recursiveDir.EndsWith("**")
-            let tempPath = 
-                if isRecursive then recursiveDir.Replace("**", "") else recursiveDir
-                |> cleanUpPath
+        type internal T = 
+            | Directory of string
+            | Recursive
+            | FilePattern of string
+        
+        let internal exists dir root = 
+            let di = new DirectoryInfo(Path.Combine(root, dir))
+            if di.Exists
+            then Some di.FullName
+            else None
+        
+        let rec internal buildPaths acc (input : T list) =
+            match input with
+            | [] -> acc
+            | Directory(name) :: t -> 
+                match List.tryPick (exists name) acc with
+                | Some(dir) -> buildPaths [dir] t 
+                | None -> [] 
+            | Recursive :: t ->
+                let dirs = 
+                    Seq.collect (fun dir -> Directory.EnumerateDirectories(dir, "*", SearchOption.AllDirectories)) acc
+                    |> Seq.toList
+                buildPaths (acc @ dirs) t
+            | FilePattern(pattern) :: t -> 
+                Seq.collect (fun dir -> Directory.EnumerateFiles(dir, pattern)) acc
+                |> Seq.toList
+        
+        let getBaseDir () = 
             let assembly = Reflection.Assembly.GetEntryAssembly()
-            let fullPath = if assembly = null then FullName "." else Path.GetDirectoryName assembly.Location
-            let root = fullPathRelativeTo (Some fullPath) tempPath
-            let result =  { Recurse = isRecursive; Root = root.FullName; FilePattern = filePattern }
-            result
-           
+            if assembly = null then FullName "." else Path.GetDirectoryName assembly.Location
+         
+        let search baseDir (input : string) =
+            let filePattern = Path.GetFileName(input)
+            input.Split([|'/';'\\'|], StringSplitOptions.RemoveEmptyEntries)
+            |> Seq.map (function
+                        | "**" -> Recursive
+                        | a when a = filePattern -> FilePattern(a)
+                        | a -> Directory(a))
+            |> Seq.toList
+            |> buildPaths [baseDir]
 
-        let DefaultSearcher (searchParams:T) =
-            if Directory.Exists(searchParams.Root)
-            then
-                if searchParams.Recurse
-                then Directory.EnumerateFileSystemEntries(searchParams.Root, searchParams.FilePattern, SearchOption.AllDirectories)
-                else Directory.EnumerateFileSystemEntries(searchParams.Root, searchParams.FilePattern)
-            else Seq.empty
-
-        let findFiles searcher pattern  =
-            parse pattern |> (defaultArg searcher DefaultSearcher)
-
-        let find pattern = findFiles None pattern
+        let find pattern = search (getBaseDir()) pattern
