99fb5ec03f0562003dc2528c739e4a00002d3d39
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-08 11:56:20 +0100

    Add a test which shows that the ProjectFile works immutable

--- src/test/Test.FAKECore/ProjectSystemSpecs.cs
+++ src/test/Test.FAKECore/ProjectSystemSpecs.cs
@@ -27,13 +27,17 @@ namespace Test.FAKECore
     public class when_adding_files
     {
         static ProjectSystem.ProjectFile _project;
+        static ProjectSystem.ProjectFile _project2;
         Establish context = () => _project = ProjectSystem.ProjectFile.FromFile("ProjectTestFiles/FakeLib.fsproj");
-        Because of = () => _project = _project.AddFile("badadadumHelper.fs");
+        Because of = () => _project2 = _project.AddFile("badadadumHelper.fs");
 
         It should_find_the_SpecsRemovement_helper_in_the_MSBuild_folder = () =>
-            _project.Files.ShouldContain("MSBuild\\SpecsRemovement.fs");
+            _project2.Files.ShouldContain("MSBuild\\SpecsRemovement.fs");
 
         It should_find_the_badadadum_helper = () =>
-            _project.Files.ShouldContain("badadadumHelper.fs");
+            _project2.Files.ShouldContain("badadadumHelper.fs");
+
+        It should_work_immutable = () =>
+            _project.Files.ShouldNotContain("badadadumHelper.fs");
     }
 }
