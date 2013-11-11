ee3d2fe922a821fc25f8db62402d99e99f1124f3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-11 10:55:45 +0100

    Adding FileInclude operators for the experimental globbing

--- src/app/FakeLib/Globbing/FileSystem.fs
+++ src/app/FakeLib/Globbing/FileSystem.fs
@@ -1,7 +1,9 @@
 // Copied from https://github.com/colinbull/FSharp.Enterprise/blob/master/src/FSharp.Enterprise/FileSystem.fs
+/// Experimental Globbing - be careful we will break this.
 module Fake.FileSystem
     
 open System
+open System.Collections.Generic
 open System.IO
 open Fake
 
@@ -47,3 +49,33 @@ let search baseDir (input : string) =
     |> buildPaths [baseDir]
 
 let find pattern = search (getBaseDir()) pattern
+
+/// Internal representation of a file set
+type FileIncludes =
+  { BaseDirectory: string
+    Includes: string list
+    Excludes: string list }
+
+  interface IEnumerable<string> with 
+    member this.GetEnumerator() =
+        let files = 
+            seq { for pattern in this.Includes do
+                    yield! find pattern }
+        files.GetEnumerator()
+                 
+    member this.GetEnumerator() = (this :> IEnumerable<string>).GetEnumerator() :> System.Collections.IEnumerator
+
+/// Include files
+let Include x = { BaseDirectory = DefaultBaseDir; Includes = [x]; Excludes = []}
+
+/// Sets a directory as baseDirectory for fileIncludes. 
+let SetBaseDir (dir:string) (fileIncludes:FileIncludes)  = {fileIncludes with BaseDirectory = dir.TrimEnd(directorySeparator.[0])}
+  
+/// Add Include operator
+let inline (++) (x:FileIncludes) pattern = { x with Includes = pattern :: x.Includes }
+
+/// Exclude operator
+let inline (--) (x:FileIncludes) pattern = { x with Excludes = pattern :: x.Excludes }
+
+/// Includes a single pattern and scans the files - !! x = AllFilesMatching x
+let inline (!!) x = Include x
