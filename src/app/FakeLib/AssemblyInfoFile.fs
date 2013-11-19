f0e52760ac037d34aac4e437f296fb7925015950
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-11-19 15:15:22 -0800

    Code formatting the AssemblyInfoFile task

--- src/app/FakeLib/AssemblyInfoFile.fs
+++ src/app/FakeLib/AssemblyInfoFile.fs
@@ -19,51 +19,69 @@ type AssemblyInfoFileConfig =
 
 /// Represents AssemblyInfo attributes
 type Attribute(name,value,inNamespace) =
-   member this.Name = name
-   member this.Value = value
-   member this.Namespace = inNamespace
+    member this.Name = name
+    member this.Value = value
+    member this.Namespace = inNamespace
 
-   /// Creates a simple attribute with string values. Used as base for other attributes
-   static member StringAttribute(name,value,inNamespace) = Attribute(name,sprintf "\"%s\"" value,inNamespace)
-   /// Creates a simple attribute with boolean values. Used as base for other attributes
-   static member BoolAttribute(name,value,inNamespace) = Attribute(name,sprintf "%b" value,inNamespace)
+    /// Creates a simple attribute with string values. Used as base for other attributes
+    static member StringAttribute(name,value,inNamespace) = Attribute(name,sprintf "\"%s\"" value,inNamespace)
 
-   /// Creates an attribute which holds the company information
-   static member Company(value) = Attribute.StringAttribute("AssemblyCompany",value,"System.Reflection")
-   /// Creates an attribute which holds the product name
-   static member Product(value) = Attribute.StringAttribute("AssemblyProduct",value,"System.Reflection")
-   /// Creates an attribute which holds the copyright information
-   static member Copyright(value) = Attribute.StringAttribute("AssemblyCopyright",value,"System.Reflection")
-   /// Creates an attribute which holds the product title
-   static member Title(value) = Attribute.StringAttribute("AssemblyTitle",value,"System.Reflection")
-   /// Creates an attribute which holds the product description
-   static member Description(value) = Attribute.StringAttribute("AssemblyDescription",value,"System.Reflection")
-   /// Creates an attribute which holds the assembly culture information
-   static member Culture(value) = Attribute.StringAttribute("AssemblyCulture",value,"System.Reflection")
-   /// Creates an attribute which holds the assembly configuration
-   static member Configuration(value) = Attribute.StringAttribute("AssemblyConfiguration",value,"System.Reflection")
-   /// Creates an attribute which holds the trademark
-   static member Trademark(value) = Attribute.StringAttribute("AssemblyTrademark",value,"System.Reflection")
-   /// Creates an attribute which holds the assembly version
-   static member Version(value) = Attribute.StringAttribute("AssemblyVersion",NormalizeVersion value,"System.Reflection")
-   /// Creates an attribute which holds the assembly key file
-   static member KeyFile(value) = Attribute.StringAttribute("AssemblyKeyFile",value,"System.Reflection")
-   /// Creates an attribute which holds the assembly key name
-   static member KeyName(value) = Attribute.StringAttribute("AssemblyKeyName",value,"System.Reflection")
-   /// Creates an attribute which holds the "InternalVisibleTo" data
-   static member InternalsVisibleTo(value) = Attribute.StringAttribute("InternalsVisibleTo",value,"System.Runtime.CompilerServices")
-   /// Creates an attribute which holds the assembly file version
-   static member FileVersion(value) = Attribute.StringAttribute("AssemblyFileVersion",NormalizeVersion value,"System.Reflection")
-   /// Creates an attribute which holds an assembly information version
-   static member InformationalVersion(value) = Attribute.StringAttribute("AssemblyInformationalVersion",value,"System.Reflection")
-   /// Creates an attribute which holds the Guid
-   static member Guid(value) = Attribute.StringAttribute("Guid",value,"System.Runtime.InteropServices")
-   /// Creates an attribute which specifies if the assembly is visible via COM
-   static member ComVisible(?value) = Attribute.BoolAttribute("ComVisible",defaultArg value false,"System.Runtime.InteropServices")
-   /// Creates an attribute which specifies if the assembly is CLS compliant
-   static member CLSCompliant(?value) = Attribute.BoolAttribute("CLSCompliant",defaultArg value false,"System")
-   /// Creates an attribute which specifies if the assembly uses delayed signing
-   static member DelaySign(value) = Attribute.BoolAttribute("AssemblyDelaySign",defaultArg value false,"System.Reflection")
+    /// Creates a simple attribute with boolean values. Used as base for other attributes
+    static member BoolAttribute(name,value,inNamespace) = Attribute(name,sprintf "%b" value,inNamespace)
+
+    /// Creates an attribute which holds the company information
+    static member Company(value) = Attribute.StringAttribute("AssemblyCompany",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the product name
+    static member Product(value) = Attribute.StringAttribute("AssemblyProduct",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the copyright information
+    static member Copyright(value) = Attribute.StringAttribute("AssemblyCopyright",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the product title
+    static member Title(value) = Attribute.StringAttribute("AssemblyTitle",value,"System.Reflection")
+    
+    /// Creates an attribute which holds the product description
+    static member Description(value) = Attribute.StringAttribute("AssemblyDescription",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the assembly culture information
+    static member Culture(value) = Attribute.StringAttribute("AssemblyCulture",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the assembly configuration
+    static member Configuration(value) = Attribute.StringAttribute("AssemblyConfiguration",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the trademark
+    static member Trademark(value) = Attribute.StringAttribute("AssemblyTrademark",value,"System.Reflection")
+    
+    /// Creates an attribute which holds the assembly version
+    static member Version(value) = Attribute.StringAttribute("AssemblyVersion",NormalizeVersion value,"System.Reflection")
+    
+    /// Creates an attribute which holds the assembly key file
+    static member KeyFile(value) = Attribute.StringAttribute("AssemblyKeyFile",value,"System.Reflection")
+    
+    /// Creates an attribute which holds the assembly key name
+    static member KeyName(value) = Attribute.StringAttribute("AssemblyKeyName",value,"System.Reflection")
+    
+    /// Creates an attribute which holds the "InternalVisibleTo" data
+    static member InternalsVisibleTo(value) = Attribute.StringAttribute("InternalsVisibleTo",value,"System.Runtime.CompilerServices")
+    
+    /// Creates an attribute which holds the assembly file version
+    static member FileVersion(value) = Attribute.StringAttribute("AssemblyFileVersion",NormalizeVersion value,"System.Reflection")
+   
+    /// Creates an attribute which holds an assembly information version
+    static member InformationalVersion(value) = Attribute.StringAttribute("AssemblyInformationalVersion",value,"System.Reflection")
+   
+    /// Creates an attribute which holds the Guid
+    static member Guid(value) = Attribute.StringAttribute("Guid",value,"System.Runtime.InteropServices")
+   
+    /// Creates an attribute which specifies if the assembly is visible via COM
+    static member ComVisible(?value) = Attribute.BoolAttribute("ComVisible",defaultArg value false,"System.Runtime.InteropServices")
+   
+    /// Creates an attribute which specifies if the assembly is CLS compliant
+    static member CLSCompliant(?value) = Attribute.BoolAttribute("CLSCompliant",defaultArg value false,"System")
+   
+    /// Creates an attribute which specifies if the assembly uses delayed signing
+    static member DelaySign(value) = Attribute.BoolAttribute("AssemblyDelaySign",defaultArg value false,"System.Reflection")
 
 let private writeToFile outputFileName (lines: seq<string>) =    
     let fi = fileInfo outputFileName
@@ -136,9 +154,9 @@ let CreateFSharpAssemblyInfoWithConfig outputFileName attributes config =
 /// Creates a C# AssemblyInfo file with the given attributes.
 /// The generated AssemblyInfo file contains an AssemblyVersionInformation class which can be used to retrieve the current version no. from inside of an assembly.
 let CreateCSharpAssemblyInfo outputFileName attributes =
-  CreateCSharpAssemblyInfoWithConfig outputFileName attributes AssemblyInfoFileConfig.Default
+    CreateCSharpAssemblyInfoWithConfig outputFileName attributes AssemblyInfoFileConfig.Default
 
 /// Creates a F# AssemblyInfo file with the given attributes.
 /// The generated AssemblyInfo file contains an AssemblyVersionInformation class which can be used to retrieve the current version no. from inside of an assembly.
 let CreateFSharpAssemblyInfo outputFileName attributes =
-  CreateFSharpAssemblyInfoWithConfig outputFileName attributes AssemblyInfoFileConfig.Default
+    CreateFSharpAssemblyInfoWithConfig outputFileName attributes AssemblyInfoFileConfig.Default
