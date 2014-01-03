76ff33042e67804056fe3768146bad7ba1e1530e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-03 14:19:58 +0100

    Allow to stage all files in git

--- src/app/FakeLib/Git/Staging.fs
+++ src/app/FakeLib/Git/Staging.fs
@@ -2,9 +2,17 @@
 /// Contains helper functions which allow to deal with git's staging area.
 module Fake.Git.Staging
 
+open Fake
+
 /// Adds a file to the staging area
 let StageFile repositoryDir file =
-  file 
+    file 
     |> fixPath
     |> sprintf "update-index --add \"%s\""
     |> runGitCommand repositoryDir
+
+/// Adds all files to the staging area
+let StageAll repositoryDir =
+    "add . --all"
+    |> runSimpleGitCommand repositoryDir
+    |> trace
