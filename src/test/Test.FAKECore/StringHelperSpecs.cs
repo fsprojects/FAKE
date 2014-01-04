7150be7e8f0d70a2859544368679b1b1d4d8686e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-04 14:23:41 +0100

    Make tests runnable on mono

--- src/test/Test.FAKECore/StringHelperSpecs.cs
+++ src/test/Test.FAKECore/StringHelperSpecs.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Fake;
 using Machine.Specifications;
 
@@ -23,7 +22,7 @@ namespace Test.FAKECore
             () => StringHelper.NormalizeVersion("0.1.2.1.6.5").ShouldEqual("0.1.2.1");
 
         It should_remove_the_fith_part_and_trim =
-                    () => StringHelper.NormalizeVersion("0.1.2.0.6").ShouldEqual("0.1.2");
+            () => StringHelper.NormalizeVersion("0.1.2.0.6").ShouldEqual("0.1.2");
     }
 
     public class when_using_string_helper
