432a3c6124019f5fc425f72c7e2ca6eac1b9fe95
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-04 12:10:31 +0100

    Add mono build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f packages/FAKE/tools/Fake.exe ]; then
-  mono --runtime=v4.0 .NuGet/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion  -Prerelease
-  mono tools/NuGet/nuget.exe install FSharp.Formatting -OutputDirectory tools -ExcludeVersion -Prerelease
+  mono --runtime=v4.0 ./tools/NuGet/nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion  -Prerelease
+  mono --runtime=v4.0 ./tools/NuGet/nuget.exe install FSharp.Formatting -OutputDirectory tools -ExcludeVersion -Prerelease
 fi
 mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
