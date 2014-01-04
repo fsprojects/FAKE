0b800d5fb1ab7569ddd4fd2185cd9c72df911d96
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-04 16:31:15 +0100

    more tests for globbing on mono

--- src/test/Test.FAKECore/Globbing/TestSample1/FileSetSample1Specs.cs
+++ src/test/Test.FAKECore/Globbing/TestSample1/FileSetSample1Specs.cs
@@ -12,9 +12,9 @@ namespace Test.FAKECore.Globbing.TestSample1
         It should_set_the_base_directory =
             () => _files.BaseDirectory.ShouldEqual(System.IO.Directory.GetCurrentDirectory());
 
-        It should_find_ilmerge = () => _files.First().ShouldEndWith("ilmerge.exclude");
-        It should_find_sample_app = () => _files.Skip(1).First().ShouldEndWith("SampleApp.dll");
-        It should_match_2_files = () => _files.Count().ShouldEqual(2);
+        It should_find_first_file = () => _files.First().ShouldEndWith("SampleApp.csproj");
+        It should_find_second_file = () => _files.Skip(1).First().ShouldEndWith("SampleApp.sln");
+        It should_match_5_files = () => _files.Count().ShouldEqual(5);
     }
 
     public class when_enumerating_a_file_set_with_subfolder_pattern : when_extracting_zip
