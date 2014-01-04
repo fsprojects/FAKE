762f6ec3cd06d33ba6ed3dc4dc83b7b28c174d74
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-04 15:02:41 +0100

    Make tests runnable on mono

--- src/test/Test.FAKECore/AssemblyInfoSpecs.cs
+++ src/test/Test.FAKECore/AssemblyInfoSpecs.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Fake;
 using Machine.Specifications;
 
 namespace Test.FAKECore
@@ -22,8 +23,7 @@ namespace Test.FAKECore
                 AssemblyInfoFile.Attribute.Version("1.0.0.0")
             };
             AssemblyInfoFile.CreateFSharpAssemblyInfo(infoFile, attributes);
-            string expected =
-                "namespace System\r\nopen System.Reflection\r\n\r\n[<assembly: AssemblyProductAttribute(\"TestLib\")>]\r\n[<assembly: AssemblyVersionAttribute(\"1.0.0.0\")>]\r\ndo ()\r\n\r\nmodule internal AssemblyVersionInformation =\r\n    let [<Literal>] Version = \"1.0.0.0\"\r\n";
+            const string expected = "namespace System\r\nopen System.Reflection\r\n\r\n[<assembly: AssemblyProductAttribute(\"TestLib\")>]\r\n[<assembly: AssemblyVersionAttribute(\"1.0.0.0\")>]\r\ndo ()\r\n\r\nmodule internal AssemblyVersionInformation =\r\n    let [<Literal>] Version = \"1.0.0.0\"\r\n";
 
             File.ReadAllText(infoFile)
                 .ShouldEqual(expected.Replace("\r\n", Environment.NewLine));
@@ -42,8 +42,7 @@ namespace Test.FAKECore
                 AssemblyInfoFile.Attribute.Version("1.0.0.0")
             };
             AssemblyInfoFile.CreateFSharpAssemblyInfoWithConfig(infoFile, attributes, customConfig);
-            string expected =
-                "namespace Custom\r\nopen System.Reflection\r\n\r\n[<assembly: AssemblyProductAttribute(\"TestLib\")>]\r\n[<assembly: AssemblyVersionAttribute(\"1.0.0.0\")>]\r\ndo ()\r\n\r\n";
+            const string expected = "namespace Custom\r\nopen System.Reflection\r\n\r\n[<assembly: AssemblyProductAttribute(\"TestLib\")>]\r\n[<assembly: AssemblyVersionAttribute(\"1.0.0.0\")>]\r\ndo ()\r\n\r\n";
             File.ReadAllText(infoFile)
                 .ShouldEqual(expected.Replace("\r\n", Environment.NewLine));
         };
