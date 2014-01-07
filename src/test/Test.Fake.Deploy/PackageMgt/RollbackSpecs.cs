d7cc255c586cec1fca6fa9a360e2c69e14db9f40
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-07 11:07:46 +0100

    Fix test on mono

--- src/test/Test.Fake.Deploy/PackageMgt/RollbackSpecs.cs
+++ src/test/Test.Fake.Deploy/PackageMgt/RollbackSpecs.cs
@@ -6,7 +6,7 @@ namespace Test.Fake.Deploy.PackageMgt
     public class when_trying_to_get_the_previous_package
     {
         static string _selectedbackup;
-        Because of = () => _selectedbackup = DeploymentHelper.getPreviousPackageVersionFromBackup(TestData.TestDataDir, "JQuery", 1);
+        Because of = () => _selectedbackup = DeploymentHelper.getPreviousPackageVersionFromBackup(TestData.TestDataDir, "jQuery", 1);
 
         It should_find_the_predecessor_package = () => _selectedbackup.ShouldEqual("1.6");
     }
@@ -14,7 +14,7 @@ namespace Test.Fake.Deploy.PackageMgt
     public class when_trying_to_get_the_another_predecessor_package
     {
         static string _selectedbackup;
-        Because of = () => _selectedbackup = DeploymentHelper.getPreviousPackageVersionFromBackup(TestData.TestDataDir, "JQuery", 2);
+        Because of = () => _selectedbackup = DeploymentHelper.getPreviousPackageVersionFromBackup(TestData.TestDataDir, "jQuery", 2);
 
         It should_find_the_right_package = () => _selectedbackup.ShouldEqual("1.5");
     }
