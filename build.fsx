dcf17cca6b8167a039d83f76f504cc7b7422da19
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-01 13:15:18 +0100

    Create docs only for FakeLib - at least for now

--- build.fsx
+++ build.fsx
@@ -112,8 +112,8 @@ Target "GenerateDocs" (fun _ ->
         !! "./build/**/Fake.*.dll"
           ++ "./build/FakeLib.dll"
           -- "./build/**/Fake.Experimental.dll"
+          -- "./build/**/Fake.Deploy.Lib.dll"
     
-    let dllFiles = !! "./build/FakeLib.dll"
     CreateDocsForDlls apidocsDir templatesDir projInfo githubLink dllFiles
 
     WriteStringToFile false "./docs/.nojekyll" ""
