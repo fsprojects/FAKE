6cce74d7f6064b7f6ad8e27ac83d9f5b41a0a622
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-05 09:35:09 +0100

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -15,17 +15,3 @@ SET TARGET="Default"
 IF NOT [%1]==[] (set TARGET="%1")
   
 "tools\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%"
-
-rem Bail if we're running a TeamCity build.
-if defined TEAMCITY_PROJECT_NAME goto Quit
-
-rem Loop the build script.
-set CHOICE=nothing
-echo (Q)uit, (Enter) runs the build again
-set /P CHOICE= 
-if /i "%CHOICE%"=="Q" goto :Quit
-
-GOTO Build
-
-:Quit
-exit /b %errorlevel%
