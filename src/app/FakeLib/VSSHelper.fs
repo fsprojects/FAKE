9da4069221f8f81321cea771374913f10a2465d5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-06 10:18:26 +0100

    Fantomas cleanup

--- src/app/FakeLib/VSSHelper.fs
+++ src/app/FakeLib/VSSHelper.fs
@@ -3,23 +3,26 @@
 module Fake.VSSHelper
 
 /// Retrieves the given label of the given project from Microsoft Visual SourceSafe
-let getVSSProjectWithLabel toolPath srcSafeIni username password project (localSpec:string) label =    
+let getVSSProjectWithLabel toolPath srcSafeIni username password project (localSpec : string) label = 
     let args' = 
-        sprintf "get %s -R \"-GL%s\" -I-Y -Y%s,%s -GTM" 
-          project (localSpec |> FullName |> trimSeparator) username password
-
+        let s = 
+            localSpec
+            |> FullName
+            |> trimSeparator
+            
+        sprintf "get %s -R \"-GL%s\" -I-Y -Y%s,%s -GTM" project s username password
+    
     let args = 
-        if isNullOrEmpty label then args' else
-        sprintf "%s -Vl%s" args' label 
-
+        if isNullOrEmpty label then args'
+        else sprintf "%s -Vl%s" args' label
+    
     tracefn "%s %s" toolPath args
-    let result = ExecProcess (fun info ->
-        ["SSDIR", srcSafeIni]
-          |> setEnvironmentVariables info 
-
-        info.FileName <- toolPath
-        info.WorkingDirectory <- currentDirectory
-        info.Arguments <- args) System.TimeSpan.MaxValue
+    let result = 
+        ExecProcess (fun info -> 
+            [ "SSDIR", srcSafeIni ] |> setEnvironmentVariables info
+            info.FileName <- toolPath
+            info.WorkingDirectory <- currentDirectory
+            info.Arguments <- args) System.TimeSpan.MaxValue
     if result <> 0 then failwith "Could not get sources from VSS"
 
 /// Retrieves the given project from Microsoft Visual SourceSafe    
