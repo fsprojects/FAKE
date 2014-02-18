e6415bf0fa3859ac6609d68cb1004fdaa91151e9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-18 09:40:32 +0100

    Code cleanup in RegistryHelper

--- src/app/FakeLib/RegistryHelper.fs
+++ src/app/FakeLib/RegistryHelper.fs
@@ -5,29 +5,29 @@ module Fake.RegistryHelper
 open Microsoft.Win32
 
 /// Registry base keys.
-type RegistryBaseKey =
-| HKEYLocalMachine
-| HKEYClassesRoot
-| HKEYUsers
-| HKEYCurrentUser
-| HKEYCurrentConfig
-| HKEYPerformanceData
+type RegistryBaseKey = 
+    | HKEYLocalMachine
+    | HKEYClassesRoot
+    | HKEYUsers
+    | HKEYCurrentUser
+    | HKEYCurrentConfig
+    | HKEYPerformanceData
 
 /// Maps the RegistryBaseKey to a RegistryKey
 /// [omit]
-let getKey = function
-| HKEYLocalMachine -> Registry.LocalMachine
-| HKEYClassesRoot -> Registry.ClassesRoot
-| HKEYUsers -> Registry.Users
-| HKEYCurrentUser -> Registry.CurrentUser
-| HKEYCurrentConfig -> Registry.CurrentConfig
-| HKEYPerformanceData -> Registry.PerformanceData
+let getKey name = 
+    match name with
+    | HKEYLocalMachine -> Registry.LocalMachine
+    | HKEYClassesRoot -> Registry.ClassesRoot
+    | HKEYUsers -> Registry.Users
+    | HKEYCurrentUser -> Registry.CurrentUser
+    | HKEYCurrentConfig -> Registry.CurrentConfig
+    | HKEYPerformanceData -> Registry.PerformanceData
 
 /// Gets a registy key.
 let getRegistryKey baseKey subKey = (getKey baseKey).OpenSubKey subKey
 
 /// Gets a registy value as string
-let getRegistryValue baseKey subKey value =
+let getRegistryValue baseKey subKey value = 
     use key = getRegistryKey baseKey subKey
     (key.GetValue value).ToString()
-    
