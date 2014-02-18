7032deda6ef4e7f411bf5d3766fbbf7e9f3c8eb0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-18 09:35:25 +0100

    Code cleanup in SCP

--- src/app/FakeLib/SCPHelper.fs
+++ src/app/FakeLib/SCPHelper.fs
@@ -1,18 +1,18 @@
 ﻿[<AutoOpen>]
 /// Conatins a task which allows to perform file copies using [SCP](http://en.wikipedia.org/wiki/Secure_copy), which is based on the Secure Shell (SSH) protocol.
-module Fake.SCPHelper 
+module Fake.SCPHelper
 
 /// The SCP parameter type.
-type SCPParams =  { 
-    /// Path of the scp.exe 
-    ToolPath:string
-    /// Path of the private key file (optional)
-    PrivateKeyPath:string}
+type SCPParams = 
+    { /// Path of the scp.exe 
+      ToolPath : string
+      /// Path of the private key file (optional)
+      PrivateKeyPath : string }
 
 /// The SCP default parameters
-let SCPDefaults:SCPParams = { 
-    ToolPath = "scp.exe"
-    PrivateKeyPath = null }
+let SCPDefaults : SCPParams = 
+    { ToolPath = "scp.exe"
+      PrivateKeyPath = null }
 
 /// Performs a SCP copy from the given source directory to the target path.
 /// ## Parameters
@@ -24,20 +24,16 @@ let SCPDefaults:SCPParams = {
 /// ## Sample
 ///
 ///     SCP (fun p -> { p with ToolPath = "tools/scp.exe" }) source target
-let SCP setParams source target =
-    let (p:SCPParams) = setParams SCPDefaults
+let SCP setParams source target = 
+    let (p : SCPParams) = setParams SCPDefaults
     tracefn "SCP %s %s" source target
-    
     let args = 
-        sprintf "-r %s \".\" %s"
-            (if isNullOrEmpty p.PrivateKeyPath then "" else sprintf "-i \"%s\"" p.PrivateKeyPath)
-            (toParam target)
-
+        sprintf "-r %s \".\" %s" (if isNullOrEmpty p.PrivateKeyPath then ""
+                                  else sprintf "-i \"%s\"" p.PrivateKeyPath) (toParam target)
     tracefn "%s %s" p.ToolPath args
     let result = 
-        ExecProcess (fun info ->
+        ExecProcess (fun info -> 
             info.FileName <- p.ToolPath
             info.WorkingDirectory <- source |> FullName
             info.Arguments <- args) System.TimeSpan.MaxValue
-               
     if result <> 0 then failwithf "Error during SCP. Source: %s Target: %s" source target
