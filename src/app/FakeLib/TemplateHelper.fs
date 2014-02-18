c6fd680082f2a833e9aa279834c516c092febed3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-18 09:42:01 +0100

    Code cleanup in TemplateHelper

--- src/app/FakeLib/TemplateHelper.fs
+++ src/app/FakeLib/TemplateHelper.fs
@@ -3,25 +3,24 @@
 module Fake.TemplateHelper
 
 /// Loads all templates (lazy - line by line!)    
-let loadTemplates seq = Seq.map (fun fileName -> fileName,ReadFile fileName) seq
+let loadTemplates seq = Seq.map (fun fileName -> fileName, ReadFile fileName) seq
 
 /// Replaces a bunch of the keywords in all files (lazy - line by line!)
-let replaceKeywords replacements =
-    Seq.map (fun (fileName,file) -> 
+let replaceKeywords replacements = 
+    Seq.map (fun (fileName, file) -> 
         fileName, 
-        file 
-          |> Seq.map (fun (line:string) -> 
-               let mutable sb = new System.Text.StringBuilder(line)
-               for (k:string,r:string) in replacements do 
-                 sb <- sb.Replace(k,r)
-               sb.ToString()))
-      
+        file |> Seq.map (fun (line : string) -> 
+                    let mutable sb = new System.Text.StringBuilder(line)
+                    for (k : string, r : string) in replacements do
+                        sb <- sb.Replace(k, r)
+                    sb.ToString()))
+
 /// Saves all files (lazy - file by file!)
-let saveFiles = Seq.iter (fun (fileName,file) -> WriteFile fileName (Seq.toList file))
+let saveFiles = Seq.iter (fun (fileName, file) -> WriteFile fileName (Seq.toList file))
 
 /// Replaces the templates with the given replacements
-let processTemplates replacements files =
+let processTemplates replacements files = 
     files
-      |> loadTemplates
-      |> replaceKeywords replacements
-      |> saveFiles
+    |> loadTemplates
+    |> replaceKeywords replacements
+    |> saveFiles
