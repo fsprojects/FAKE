f13754da63a0f8a22e8464b701bd8587b0247b45
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-02-23 11:21:28 +0100

    New Nuget helper allows to install packages

--- src/app/FakeLib/NuGet/Install.fs
+++ src/app/FakeLib/NuGet/Install.fs
@@ -4,6 +4,11 @@ module Fake.NuGet.Install
 open System
 open Fake
 
+type NugetInstallVerbosity =
+| Normal
+| Quiet
+| Detailed
+
 /// Nuget install parameters. All optional.
 type NugetInstallParams =
     {
@@ -15,12 +20,20 @@ type NugetInstallParams =
       Retries: int
       /// Nuget feeds to search updates in. Use default if empty.
       Sources: string list
-      /// Folder to store packages in. Default `./packages`.
-      RepositoryPath: string
+      /// The version of the package to install.
+      Version: string option
+      /// If set, the destination directory will contain only the package name, not the version number. Default `false`.
+      ExcludeVersion: bool
       /// Allows updating to prerelease versions. Default `false`.
       Prerelease: bool
+      /// Specifies the directory in which packages will be installed. If none specified, uses the current directory.
+      OutputDirectory: string
+      /// Display this amount of details in the output: normal, quiet, detailed. Default `normal`.
+      Verbosity: NugetInstallVerbosity
       /// Do not prompt for user input or confirmations. Default `true`.
       NonInteractive: bool
+      /// Disable looking up packages from local machine cache. Default `false`.
+      NoCache: bool
       /// NuGet configuration file. Default `None`.
       ConfigFile: string option }
 
@@ -30,9 +43,13 @@ let NugetInstallDefaults =
       TimeOut = TimeSpan.FromMinutes 5.
       Retries = 5
       Sources = []
-      RepositoryPath = "./packages"
+      Version = None
+      ExcludeVersion = false
+      OutputDirectory = ""
       Prerelease = false
       NonInteractive = true
+      Verbosity = Normal
+      NoCache = false
       ConfigFile = None }
 
 /// [omit]
@@ -44,8 +61,12 @@ let argList name values =
 /// [omit]
 let buildArgs (param: NugetInstallParams) =
     [   param.Sources |> argList "source"
-        [param.RepositoryPath] |> argList "repositoryPath"
+        (match param.Version with | Some version -> sprintf "-version \"%s\"" version | _ -> "")
+        (if param.OutputDirectory <> "" then sprintf "-outputdirectory \"%s\"" param.OutputDirectory else "")
+        (match param.Verbosity with | Quiet -> "quiet" | Detailed -> "detailed" | Normal -> "")
+        (if param.ExcludeVersion then "-excludeversion" else "")
         (if param.Prerelease then "-prerelease" else "")
+        (if param.NoCache then "-nocache" else "")
         (if param.NonInteractive then "-nonInteractive" else "")
         param.ConfigFile |> Option.toList |> argList "configFile"
     ] |> Seq.filter (not << String.IsNullOrEmpty) |> String.concat " "
