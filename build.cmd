3ac99d2b3843ff12d0b2cfe1cc870bca8219fa99
Cameron Taggart <cameron.taggart@gmail.com>
2014-03-02 22:06:46 -0600

    start AppVeyor and SourceLink support

--- build.cmd
+++ build.cmd
@@ -10,8 +10,12 @@ if not exist tools\FSharp.Formatting.CommandTool\tools\fsformatting.exe (
     "tools\nuget\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease"
 )
 
+if not exist tools\SourceLink.Fake\tools\SourceLink.fsx (
+    "tools\nuget\nuget.exe" "install" "SourceLink.Fake" "-OutputDirectory" "tools" "-ExcludeVersion"
+)
+
 SET TARGET="Default"
 
 IF NOT [%1]==[] (set TARGET="%1")
-  
+
 "tools\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%"
