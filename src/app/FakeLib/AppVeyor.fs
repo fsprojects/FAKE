220d2f4c960d3a16e8b9111b02e847e506eccff1
Max Malook <community@malook.de>
2014-03-09 23:14:52 +0100

    add MSBuild messages to AppVeyor log

---
+++ src/app/FakeLib/AppVeyor.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+/// Contains code to configure FAKE for AppVeyor integration
+module Fake.AppVeyor
+
+open Fake.MSBuildHelper
+
+// Add MSBuildLogger to track build messages
+if buildServer = BuildServer.AppVeyor then
+    MSBuildLoggers <- @"""C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll""" :: MSBuildLoggers
