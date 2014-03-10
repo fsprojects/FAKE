60a3d8da7af62eb78fde0e7502db481df07f7b56
Max Malook <community@malook.de>
2014-03-10 01:57:34 +0100

    add trace listener for AppVeyor

--- src/app/FakeLib/AppVeyor.fs
+++ src/app/FakeLib/AppVeyor.fs
@@ -4,6 +4,32 @@ module Fake.AppVeyor
 
 open Fake.MSBuildHelper
 
+let private sendToAppVeyor args =
+    ExecProcess (fun info -> 
+                info.FileName <- "appveyor"
+                info.Arguments <- args) (System.TimeSpan.MaxValue)
+    |> ignore
+
+let private add msg category =
+    sprintf "AddMessage %s -Category %s" (quoteIfNeeded msg) (quoteIfNeeded category)
+    |> sendToAppVeyor
+
+let private addNoCategory msg =
+    sprintf "AddMessage %s" (quoteIfNeeded msg)
+    |> sendToAppVeyor
+
+// Add trace listener to track messages
+if buildServer = BuildServer.AppVeyor then
+    listeners.Add({new ITraceListener with
+        member this.Write msg =
+            match msg with
+            | ErrorMessage x -> add x "Error"
+            | ImportantMessage x -> add x "Warning"
+            | LogMessage (x, _) -> add x "Information"
+            | TraceMessage (x, _) -> if not enableProcessTracing then addNoCategory x
+            | StartMessage | FinishedMessage
+            | OpenTag (_, _) | CloseTag _ -> ()})
+
 // Add MSBuildLogger to track build messages
 if buildServer = BuildServer.AppVeyor then
     MSBuildLoggers <- @"""C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll""" :: MSBuildLoggers
