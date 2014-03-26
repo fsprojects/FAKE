ff2f630195478a3be21734c7d683c811b279f85c
Morgan Persson <lazydev@gmail.com>
2014-03-26 07:52:15 +0100

    environment administration now works with Nancy

--- src/deploy.web/Fake.Deploy.Web/Modules/Admin.fs
+++ src/deploy.web/Fake.Deploy.Web/Modules/Admin.fs
@@ -7,12 +7,14 @@ open Nancy.ModelBinding
 open Nancy.Authentication.Forms
 open Nancy.Security
 
-type Admin () as http =
+type Admin (data : IDataProvider, users : IMembershipProvider) as http =
     inherit FakeModule("/admin")
 
     do
-        http.get "/agent/{id}" (fun _ id -> "")
+        http.get "/agent/{id}" (fun _ id -> data.GetEnvironments([id]) |> Seq.head)
 
-        http.get "/environment" (fun _ -> "")
+        http.get "/environment" (fun p -> http.View.["Environment"])
 
-        http.get "/users" (fun _ -> "")
+        http.get "/users" (fun _ ->
+            http.View.["users"].WithModel (users.GetUsers())
+        )
