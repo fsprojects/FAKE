322ff2362501ecbb2f26628b71190a25c284aad7
thinkbeforecoding <jeremie.chassaing@thinkbeforecoding.com>
2014-03-27 18:10:28 +0100

    Add unit tests for getDependencies

---
+++ src/test/Test.FAKECore/PackageMgt/PackagesConfigSpecs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Fake;
+using Machine.Specifications;
+using Microsoft.FSharp.Collections;
+
+namespace Test.FAKECore.PackageMgt
+{
+    public class when_parsing_packages_config
+    {
+        public static FSharpList<Tuple<string, string>> Dependencies;
+
+        private Because of = () => Dependencies = NuGetHelper.getDependencies(Path.Combine(TestData.TestDataDir, "fake.packages.config"));
+
+        It should_containt_3_packages =
+            () => Dependencies.Count().ShouldEqual(3);
+
+        It should_contain_the_package_id =
+            () => Dependencies.First().Item1.ShouldEqual("Microsoft.Web.Xdt");
+
+        It should_contain_the_package_version =
+            () => Dependencies.First().Item2.ShouldEqual("1.0.0");
+    }
+   
+}
