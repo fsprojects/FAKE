1c54d9291ef2add58283bd5e4240b7cd082425e7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-03-28 13:44:29 +0100

    Install SourceLink on mono

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 if [ ! -f tools/FAKE/tools/Fake.exe ]; then
   mono --runtime=v4.0 tools/NuGet/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion  -Prerelease
   mono --runtime=v4.0 tools/NuGet/nuget.exe install FSharp.Formatting.CommandTool -OutputDirectory tools -ExcludeVersion -Prerelease
+  mono --runtime=v4.0 tools/NuGet/nuget.exe install SourceLink.Fake -OutputDirectory tools -ExcludeVersion
 fi
 mono --runtime=v4.0 tools/FAKE/tools/FAKE.exe build.fsx $@
