1e445f2270e9cb879d469e6f7a084c73aa24d1ed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-03-31 11:57:11 +0200

    Bump version to 2.12.0

--- src/app/Fake.SQL/AssemblyInfo.fs
+++ src/app/Fake.SQL/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make SQL Lib")>]
 [<assembly: GuidAttribute("A161EAAF-EFDA-4EF2-BD5A-4AD97439F1BE")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.11.50")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.11.50")>]
-[<assembly: AssemblyFileVersionAttribute("2.11.50")>]
+[<assembly: AssemblyVersionAttribute("2.12.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.12.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.12.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.11.50"
+    let [<Literal>] Version = "2.12.0"
