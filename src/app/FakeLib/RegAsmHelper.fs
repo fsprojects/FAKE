6e21c4126bdc623d27911e89b822d6ff73194a97
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-02 09:38:51 +0200

    Cleaning RegAsmHelper

--- src/app/FakeLib/RegAsmHelper.fs
+++ src/app/FakeLib/RegAsmHelper.fs
@@ -5,16 +5,16 @@ module Fake.RegAsmHelper
 open System
 
 /// RegAsm parameter type
-type RegAsmParams = { 
-    ToolPath: string
-    WorkingDir:string
-    TimeOut: TimeSpan}
+type RegAsmParams = 
+    { ToolPath : string
+      WorkingDir : string
+      TimeOut : TimeSpan }
 
 /// RegAsm default params
-let RegAsmDefaults = { 
-    ToolPath = @"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm.exe"
-    WorkingDir = "."
-    TimeOut = TimeSpan.FromMinutes 5. }
+let RegAsmDefaults = 
+    { ToolPath = @"C:\Windows\Microsoft.NET\Framework\v2.0.50727\regasm.exe"
+      WorkingDir = "."
+      TimeOut = TimeSpan.FromMinutes 5. }
 
 /// Runs regasm on the given lib
 /// ## Parameters
@@ -25,12 +25,9 @@ let RegAsm setParams lib =
     traceStartTask "RegAsm" lib
     let parameters = setParams RegAsmDefaults
     let args = sprintf "%s /tlb:%s" lib (replace ".dll" ".tlb" lib)
-    
-    if 0 <> ExecProcess (fun info ->
-        info.FileName <- parameters.ToolPath
-        info.WorkingDirectory <- parameters.WorkingDir
-        info.Arguments <- args) parameters.TimeOut
-    then
-        failwithf "RegAsm %s failed." args
-                  
+    if 0 <> ExecProcess (fun info -> 
+                info.FileName <- parameters.ToolPath
+                info.WorkingDirectory <- parameters.WorkingDir
+                info.Arguments <- args) parameters.TimeOut
+    then failwithf "RegAsm %s failed." args
     traceEndTask "RegAsm" lib
