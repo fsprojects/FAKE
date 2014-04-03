b102178fd5d74ecc1de8e920d0deba904e2e23e6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-03 07:02:11 +0200

    Bump version to 2.13.0-alpha1

--- src/app/Fake.Deploy.Lib/AssemblyInfo.fs
+++ src/app/Fake.Deploy.Lib/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Lib")>]
 [<assembly: GuidAttribute("AA284C42-1396-42CB-BCAC-D27F18D14AC7")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.12.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.12.2")>]
-[<assembly: AssemblyFileVersionAttribute("2.12.2")>]
+[<assembly: AssemblyVersionAttribute("2.13.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.13.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.13.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.12.2"
+    let [<Literal>] Version = "2.13.0"
