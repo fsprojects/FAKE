4af3a755c84ec755f563bddb268e5b056bfedd6f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-07 10:10:55 +0200

    Try out new nuget

--- build.cmd
+++ build.cmd
@@ -3,15 +3,15 @@
 cls
 
 if not exist tools\FAKE\tools\Fake.exe (
-    "tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease" -nocache
+    "tools\nuget\nuget.exe" "install" "FAKE" "-OutputDirectory" "tools" "-ExcludeVersion"
 )
 
 if not exist tools\FSharp.Formatting.CommandTool\tools\fsformatting.exe ( 
-    "tools\nuget\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease" -nocache
+    "tools\nuget\nuget.exe" "install" "FSharp.Formatting.CommandTool" "-OutputDirectory" "tools" "-ExcludeVersion" "-Prerelease" 
 )
 
 if not exist tools\SourceLink.Fake\tools\SourceLink.fsx (
-    "tools\nuget\nuget.exe" "install" "SourceLink.Fake" "-OutputDirectory" "tools" "-ExcludeVersion" -nocache
+    "tools\nuget\nuget.exe" "install" "SourceLink.Fake" "-OutputDirectory" "tools" "-ExcludeVersion"
 )
 
 SET TARGET="Default"
