41851d787a3fbfa705c71e509fcdf7240d534534
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-24 13:36:58 +0200

    Bump version to 2.15.2

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.15.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.15.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.15.1")>]
+[<assembly: AssemblyVersionAttribute("2.15.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.15.2")>]
+[<assembly: AssemblyFileVersionAttribute("2.15.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.15.1"
+    let [<Literal>] Version = "2.15.2"
--- src/app/FakeLib/AssemblyInfo.fs
+++ src/app/FakeLib/AssemblyInfo.fs
@@ -7,10 +7,10 @@ open System.Runtime.InteropServices
 [<assembly: InternalsVisibleToAttribute("Test.FAKECore")>]
 [<assembly: GuidAttribute("d6dd5aec-636d-4354-88d6-d66e094dadb5")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.15.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.15.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.15.1")>]
+[<assembly: AssemblyVersionAttribute("2.15.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.15.2")>]
+[<assembly: AssemblyFileVersionAttribute("2.15.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.15.1"
+    let [<Literal>] Version = "2.15.2"
