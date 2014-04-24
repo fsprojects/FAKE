07caac9e010ac879af6d3eab3fb08de205b93df7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-04-24 12:54:21 +0200

    Bump version to 2.15.1

--- src/app/FakeLib/AssemblyInfo.fs
+++ src/app/FakeLib/AssemblyInfo.fs
@@ -7,10 +7,10 @@ open System.Runtime.InteropServices
 [<assembly: InternalsVisibleToAttribute("Test.FAKECore")>]
 [<assembly: GuidAttribute("d6dd5aec-636d-4354-88d6-d66e094dadb5")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.15.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.15.0")>]
-[<assembly: AssemblyFileVersionAttribute("2.15.0")>]
+[<assembly: AssemblyVersionAttribute("2.15.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.15.1")>]
+[<assembly: AssemblyFileVersionAttribute("2.15.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.15.0"
+    let [<Literal>] Version = "2.15.1"
