860e29de00722ae141654442b3a10d0378770255
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-05-08 18:35:04 +0200

    Removed last line from packSymbols thus preventing the immediate
    deletion of the created symbols package
    
    Conflicts:
    	src/app/FakeLib/NuGetHelper.fs

--- src/app/FakeLib/NuGet/NugetHelper.fs
+++ src/app/FakeLib/NuGet/NugetHelper.fs
@@ -203,8 +203,6 @@ let private packSymbols parameters =
                 info.WorkingDirectory <- FullName parameters.WorkingDir
                 info.Arguments <- args) parameters.TimeOut
         if result <> 0 then failwithf "Error during NuGet symbols creation. %s %s" parameters.ToolPath args
-        parameters.OutputPath @@ (symbolsPackageFileName parameters)
-        |> DeleteFile
 
 /// create package
 let private pack parameters nuspecFile = 
