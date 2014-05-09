0c55665ef0e2c6ba34e1c3aeea586b732d204eb2
Marcin Deptuła <marcin.deptula@live.com>
2014-05-09 21:39:27 +0200

    Added AssemblyHelper.fs which allows to get assembly version easily

---
+++ src/app/FakeLib/AssemblyHelper.fs
@@ -0,0 +1,11 @@
+﻿[<AutoOpen>]
+module Fake.AssemblyHelper
+
+open System.Reflection
+
+/// Gets file assembly version in form of major.minor.build.revision.
+/// ## Parameters
+///  - `assemblyFile` - The assembly file path.
+let GetAssemblyVersion (assemblyFile: string) = 
+    AssemblyName.GetAssemblyName(assemblyFile).Version.ToString()
+
