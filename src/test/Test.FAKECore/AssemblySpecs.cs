f3147430ae65e35b52c2474d332a69162ba2150a
Marcin Deptuła <marcin.deptula@live.com>
2014-05-09 21:40:06 +0200

    Added tests for AssemblyHelper

---
+++ src/test/Test.FAKECore/AssemblySpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using Fake;
+using Machine.Specifications;
+
+namespace Test.FAKECore
+{
+    public class when_getting_assembly_version
+    {
+        It should_return_valid_version_number_in_expected_form = () =>
+        {
+            var currentAssemblyVersion = Assembly.GetCallingAssembly().GetName().Version;
+            var currentAssemblyFile = Assembly.GetCallingAssembly().Location;
+            
+            var fakeVersion = Fake.AssemblyHelper.GetAssemblyVersion(currentAssemblyFile);
+
+            fakeVersion.ShouldEqual(currentAssemblyVersion.Major + "." +
+                                    currentAssemblyVersion.Minor + "." +
+                                    currentAssemblyVersion.Build + "." +
+                                    currentAssemblyVersion.Revision);
+        };
+
+        It should_return_valid_version_number = () =>
+        {
+            var currentAssemblyVersion = Assembly.GetCallingAssembly().GetName().Version;
+            var currentAssemblyFile = Assembly.GetCallingAssembly().Location;
+
+            var fakeVersion = Fake.AssemblyHelper.GetAssemblyVersion(currentAssemblyFile);
+
+            fakeVersion.ShouldEqual(currentAssemblyVersion.ToString());
+        };
+    }
+}
