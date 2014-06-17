fc3390eec8b888340914200b77d2927d6facebd9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-06-17 19:51:29 +0200

    Bump version to 2.18.2

--- src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
+++ src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Web")>]
 [<assembly: GuidAttribute("27BA7705-3F57-47BE-B607-8A46B27AE876")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("2.18.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.18.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.18.1")>]
+[<assembly: AssemblyVersionAttribute("2.18.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.18.2")>]
+[<assembly: AssemblyFileVersionAttribute("2.18.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.18.1"
+    let [<Literal>] Version = "2.18.2"
