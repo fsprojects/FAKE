34b5587db0b54830ba5b4b50d26b19d232634c24
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-06-27 09:37:40 +0200

    Cleanup before release

--- src/test/testscripts/test1.fsx
+++ src/test/testscripts/test1.fsx
@@ -2,15 +2,6 @@
 
 open Fake
 
-Target "Default" DoNothing
-
-
-RunTargetOrDefault "Default"
 
 if hasBuildParam "test" |> not then
     failwith "test param is missing"
-else
-    trace <|  getBuildParam "test"
-
-
-System.Console.ReadKey() |> ignore
