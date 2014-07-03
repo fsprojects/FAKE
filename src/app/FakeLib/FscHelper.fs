0afd0c9dfed1bb5d1ecd7f6ca4fe7599a87f1df4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-07-03 12:59:33 +0200

    Extract FscDefaultParams and make the Fsc task take file set. - relates to #485

--- src/app/FakeLib/FscHelper.fs
+++ src/app/FakeLib/FscHelper.fs
@@ -9,9 +9,7 @@ type FscPlatform = X86 | Itanium | X64 | AnyCpu32BitPreferred | AnyCpu
 
 /// 'fsc.exe' command line parameters
 type FscParams =
-  { /// Specifies the files to compile.
-    Inputs: string list
-    /// Specifies the output file name and path.
+  { /// Specifies the output file name and path.
     Output: string
     /// Specifies the compiled artifact target type.
     FscTarget: FscTarget
@@ -24,18 +22,27 @@ type FscParams =
     /// Specifies other params for the compilation. Freeform strings.
     OtherParams: string list }
 
+let FscDefaultParams =    
+    // These are the default params to the compiler service.
+    { Output = ""
+      FscTarget = Exe
+      Platform = AnyCpu
+      References = []
+      Debug = false
+      OtherParams = [] }
+
 /// Compiles the given source file with the given options. If no options
 /// given (i.e. the second argument is an empty list), by default tries
 /// to behave the same way as would the command-line 'fsc.exe' tool.
 ///
 /// Example usage:
 /// Target "MyFile" (fun _ ->
-///   fsc "MyFile.fs" ["-a"; "-r"; "Common.dll"])
-let fscList (srcFile: string) (opts: string list): int =
+///   fsc ["MyFile.fs"] ["-a"; "-r"; "Common.dll"])
+let fscList (srcFiles: string list) (opts: string list): int =
   let optsArr =
     // If output file name is specified, pass it on to fsc.
     if Seq.exists (fun e -> e = "-o" || e.StartsWith("--out:")) opts
-      then opts @ [srcFile]
+      then opts @ srcFiles
       // But if it's not, then figure out what it should be.
       else
         let outExt =
@@ -44,7 +51,7 @@ let fscList (srcFile: string) (opts: string list): int =
             opts
             then ".dll"
             else ".exe"
-        "-o" :: FileHelper.changeExt outExt srcFile :: opts @ [srcFile]
+        "-o" :: FileHelper.changeExt outExt (List.head srcFiles) :: opts @ srcFiles
     |> Array.ofList
   let errors, exitCode = scs.Compile(optsArr)
 
@@ -55,26 +62,17 @@ let fscList (srcFile: string) (opts: string list): int =
 /// Compiles one or more F# source files with the specified parameters.
 /// Can be called as:
 ///
-/// fsc (fun params ->
+/// Fsc (fun params ->
 ///   { params with
 ///     Inputs = ...
 ///     Output = ...
 ///     FscTarget = ...
 ///     ... })
+///   ["file1.fsx"; "file2.fsx"]
 ///
 /// Returns the exit code of the compilation process.
-let fsc (fscParamSetter: (FscParams -> FscParams)): int =
-  let fscParams =
-    fscParamSetter
-      // These are the default params to the compiler service.
-      { Inputs = []
-        Output = ""
-        FscTarget = Exe
-        Platform = AnyCpu
-        References = []
-        Debug = false
-        OtherParams = [] }
-  let inputs = fscParams.Inputs
+let Fsc (fscParamSetter: FscParams -> FscParams) inputFiles =
+  let fscParams = fscParamSetter FscDefaultParams
   let output = fscParams.Output
   let argList =
     if output <> "" then ["-o"; output] else []
@@ -92,10 +90,5 @@ let fsc (fscParamSetter: (FscParams -> FscParams)): int =
     @ List.map (fun r -> "--reference:" + r) fscParams.References
     @ if fscParams.Debug then ["-g"] else []
     @ fscParams.OtherParams
-    @ inputs.Tail
 
-  fscList inputs.Head argList
-
-/// Same as fsc, but discards the exit code of the compilation process.
-/// Can be used in FAKE targets.
-let Fsc = fsc >> ignore
+  fscList (inputFiles |> Seq.toList) argList |> ignore
