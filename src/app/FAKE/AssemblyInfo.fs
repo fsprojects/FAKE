719b3a4b418614f1480b533509fbbf0be78880ec
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-07-22 17:14:20 +0200

    Bump version to 3.2.7

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.2.6")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.2.6")>]
-[<assembly: AssemblyFileVersionAttribute("3.2.6")>]
+[<assembly: AssemblyVersionAttribute("3.2.7")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.2.7")>]
+[<assembly: AssemblyFileVersionAttribute("3.2.7")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.2.6"
+    let [<Literal>] Version = "3.2.7"
