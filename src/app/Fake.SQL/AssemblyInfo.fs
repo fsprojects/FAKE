f50d71d130394900bb82cea34de019d68f7bd1ba
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-07-22 19:11:26 +0200

    Bump version to 3.2.10

--- src/app/Fake.SQL/AssemblyInfo.fs
+++ src/app/Fake.SQL/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make SQL Lib")>]
 [<assembly: GuidAttribute("A161EAAF-EFDA-4EF2-BD5A-4AD97439F1BE")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.2.9")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.2.9")>]
-[<assembly: AssemblyFileVersionAttribute("3.2.9")>]
+[<assembly: AssemblyVersionAttribute("3.2.10")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.2.10")>]
+[<assembly: AssemblyFileVersionAttribute("3.2.10")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.2.9"
+    let [<Literal>] Version = "3.2.10"
