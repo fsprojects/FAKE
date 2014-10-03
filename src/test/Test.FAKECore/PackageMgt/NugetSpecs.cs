fd7334ef5ebc07ab4f2f8ae57babc8eca7910924
Ilker Cetinkaya <ilker@ilker.de>
2014-10-03 13:28:03 +0200

    Add test for non-templated nuspec packaging

--- src/test/Test.FAKECore/PackageMgt/NugetSpecs.cs
+++ src/test/Test.FAKECore/PackageMgt/NugetSpecs.cs
@@ -62,4 +62,59 @@ namespace Test.FAKECore.PackageMgt
             File.Exists(pkgFile).ShouldBeTrue();
         };
     }
+
+    public class when_packing_with_a_complete_nuspec_file
+    {
+        static string tempDir, pkgFile, nuspecFile;
+        static FSharpFunc<NuGetHelper.NuGetParams, NuGetHelper.NuGetParams> nugetParams;
+
+        Establish context = () =>
+        {
+            tempDir = Path.GetTempPath();
+            pkgFile = Path.Combine(tempDir, "fake_complete.0.0.1.nupkg");
+            nuspecFile = Path.Combine(TestData.TestDataDir, "fake_complete.nuspec");
+
+            try { File.Delete(pkgFile); } catch (FileNotFoundException) {}
+
+            nugetParams = FSharpFuncUtil.ToFSharpFunc<NuGetHelper.NuGetParams, NuGetHelper.NuGetParams>(
+                p => new NuGetHelper.NuGetParams(
+                    authors: ListModule.OfSeq(new [] { "author" }),
+                    project: "fake",
+                    description: "description",
+                    outputPath: tempDir,
+                    summary: "summary",
+                    workingDir: TestData.TestDataDir,
+                    version: "0.0.1",
+
+                    files: ListModule.OfSeq(new [] { new Tuple<string, FSharpOption<string>, FSharpOption<string>>("*.*", FSharpOption<string>.None, FSharpOption<string>.None) }),
+
+                    accessKey: p.AccessKey,
+                    copyright: p.Copyright,
+                    dependencies: p.Dependencies,
+                    dependenciesByFramework: p.DependenciesByFramework,
+                    includeReferencedProjects: p.IncludeReferencedProjects,
+                    noPackageAnalysis: p.NoPackageAnalysis,
+                    projectFile: p.ProjectFile,
+                    properties: p.Properties,
+                    publish: p.Publish,
+                    publishTrials: p.PublishTrials,
+                    publishUrl: p.PublishUrl,
+                    references: p.References,
+                    referencesByFramework: p.ReferencesByFramework,
+                    releaseNotes: p.ReleaseNotes,
+                    symbolPackage: p.SymbolPackage,
+                    tags: p.Tags,
+                    timeOut: p.TimeOut,
+                    title: p.Title,
+                    toolPath: p.ToolPath
+                )
+            );
+        };
+
+        Because of = () => NuGetHelper.NuGetPack(nugetParams, nuspecFile);
+        It should_create_nupkg_file = () =>
+        {
+            File.Exists(pkgFile).ShouldBeTrue();
+        };
+    }
 }
