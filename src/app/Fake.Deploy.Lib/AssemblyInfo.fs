f24a01d478c53b33140cd0dfb04a31a919118b76
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-11 19:42:15 +0200

    Use paket restore

--- src/app/Fake.Deploy.Lib/AssemblyInfo.fs
+++ src/app/Fake.Deploy.Lib/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Lib")>]
 [<assembly: GuidAttribute("AA284C42-1396-42CB-BCAC-D27F18D14AC7")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.5.10")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.5.10")>]
-[<assembly: AssemblyFileVersionAttribute("3.5.10")>]
+[<assembly: AssemblyVersionAttribute("3.5.11")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.5.11")>]
+[<assembly: AssemblyFileVersionAttribute("3.5.11")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.5.10"
+    let [<Literal>] Version = "3.5.11"
