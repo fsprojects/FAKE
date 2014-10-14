f07ef972c0050f5e3a1509c037ff44e5dacd1d64
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-14 10:08:48 +0200

    Bump version to 3.5.12

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.5.11")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.5.11")>]
-[<assembly: AssemblyFileVersionAttribute("3.5.11")>]
+[<assembly: AssemblyVersionAttribute("3.5.12")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.5.12")>]
+[<assembly: AssemblyFileVersionAttribute("3.5.12")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.5.11"
+    let [<Literal>] Version = "3.5.12"
