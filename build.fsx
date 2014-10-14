fecb83dc0f85067b878925f96dc0b709aff9e06f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-14 12:20:15 +0200

    add tracing

--- build.fsx
+++ build.fsx
@@ -6,25 +6,30 @@ let buildDir = "./buildoutput/"
 let testDir  = "./test/"
 
 Target "Clean" (fun _ ->
+    tracefn "Hello from Clean"
     CleanDir buildDir
 )
 
 Target "Restore" (fun _ ->
+    tracefn "Hello from Restore"
     RestorePackages()
 )
 
 Target "BuildApp" (fun _ ->
+    tracefn "Hello from BuildApp"
     MSBuildRelease buildDir "Rebuild" ["./web.sln"]
     |> Log "AppBuild-Output: "
 )
 
 Target "BuildTest" (fun _ ->
+    tracefn "Hello from BuildTest"
     !! "./VQCore/*.Tests/*.csproj"
       |> MSBuildDebug testDir "Build"
       |> Log "TestBuild-Output: "
 )
 
 Target "RunTests" (fun _ ->
+    tracefn "Hello from RunTest"
     !! (testDir + "/*.Tests.dll")
       |> xUnit (fun p ->
           {p with
