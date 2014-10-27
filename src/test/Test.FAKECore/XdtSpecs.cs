06e22230b44d6038dccfd4e1d613fe314edfe87f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-10-27 18:08:45 +0100

    try to fix build on mono

--- src/test/Test.FAKECore/XdtSpecs.cs
+++ src/test/Test.FAKECore/XdtSpecs.cs
@@ -13,39 +13,39 @@ namespace Test.FAKECore.XDTHandling
     {
         static string TransformedFile;
 
-        Cleanup after = () => File.Delete(@"TestData\web.new.config");
+        Cleanup after = () => File.Delete(Path.Combine("TestData","web.new.config"));
 
         Establish context = () => TransformedFile = StringHelper.ReadFileAsString(Path.Combine("TestData","web.transformed.config"));
 
-        Because of = () => XDTHelper.TransformFile(@"TestData\web.config", @"TestData\web.test.config", @"TestData\web.new.config");
+        Because of = () => XDTHelper.TransformFile(Path.Combine("TestData","web.config"),Path.Combine("TestData","web.test.config"),Path.Combine("TestData","web.new.config"));
 
-        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(@"TestData\web.new.config").ShouldEqual(TransformedFile);
+        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(Path.Combine("TestData","web.new.config")).ShouldEqual(TransformedFile);
     }
 
     public class when_transforming_file_with_config_name
     {
         static string TransformedFile;
 
-        Cleanup after = () => File.Copy(@"TestData\web.original.config", @"TestData\web.config", true);
+        Cleanup after = () => File.Copy(Path.Combine("TestData","web.original.config"), Path.Combine("TestData","web.config"), true);
 
         Establish context = () => TransformedFile = StringHelper.ReadFileAsString(Path.Combine("TestData","web.transformed.config"));
 
-        Because of = () => XDTHelper.TransformFileWithConfigName("test", @"TestData\web.config");
+        Because of = () => XDTHelper.TransformFileWithConfigName("test", Path.Combine("TestData","web.config"));
 
-        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(@"TestData\web.config").ShouldEqual(TransformedFile);
+        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(Path.Combine("TestData","web.config")).ShouldEqual(TransformedFile);
     }
 
     public class when_transforming_files_with_config_name
     {
         static string TransformedFile;
 
-        Cleanup after = () => File.Copy(@"TestData\web.original.config", @"TestData\web.config", true);
+        Cleanup after = () => File.Copy(Path.Combine("TestData","web.original.config"), Path.Combine("TestData","web.config"), true);
 
         Establish context = () => TransformedFile = StringHelper.ReadFileAsString(Path.Combine("TestData","web.transformed.config"));
 
         Because of = () => XDTHelper.TransformFilesWithConfigName("test", new FileSystem.FileIncludes("TestData", 
             new FSharpList<string>("web.config", FSharpList<string>.Empty), FSharpList<string>.Empty));
 
-        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(@"TestData\web.config").ShouldEqual(TransformedFile);
+        It should_equal_the_transformed_file = () => StringHelper.ReadFileAsString(Path.Combine("TestData","web.config")).ShouldEqual(TransformedFile);
     }
 }
