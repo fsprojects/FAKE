054570a78d7771d2f19c15fbf0853cf6914778b3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-11-07 18:04:13 +0100

    x64tify all packages

--- build.fsx
+++ build.fsx
@@ -213,9 +213,8 @@ Target "CreateNuGet" (fun _ ->
                 Publish = hasBuildParam "nugetkey" }
 
         NuGet setParams "fake.nuspec"
-        if package = "FAKE.Core" || package = "FAKE" || package = "FAKE.Deploy.Lib" then
-            !! (nugetToolsDir @@ "FAKE.exe") |> set64BitCorFlags
-            NuGet (setParams >> x64ify) "fake.nuspec"
+        !! (nugetToolsDir @@ "FAKE.exe") |> set64BitCorFlags
+        NuGet (setParams >> x64ify) "fake.nuspec"
 )
 
 Target "ReleaseDocs" (fun _ ->
