f54d4fba861e2952e6879a5476c38b97f794de5b
Tony Williams <tonywilliams1986@gmail.com>
2014-12-18 11:46:22 +0000

    Added function to build the zips for websites

--- src/app/FakeLib/AzureWebJobs.fs
+++ src/app/FakeLib/AzureWebJobs.fs
@@ -1,6 +1,9 @@
 ﻿/// Contains tasks to package and deploy [Azure Web Jobs](http://azure.microsoft.com/en-gb/documentation/articles/web-sites-create-web-jobs/) via the [Kudu](https://github.com/projectkudu/kudu) Zip controller
 module Fake.AzureWebJobs
 
+open System.IO
+open System
+
 /// The running modes of webjobs
 [<RequireQualifiedAccess>]
 type WebJobType = 
@@ -21,10 +24,39 @@ type WebJob =
 type WebSite = 
     { 
       /// The url of the website, usually in the format of https://<yourwebsite>.scm.azurewebsites.net
-      Url : string
+      Url : Uri
       /// The FTP username, usually the $username from the site's publish profile
       UserName : string
       /// The FTP Password
       Password : string
       /// The webjobs to deploy to this web site
       WebJobs : WebJob list }
+
+/// TypeScript task parameter type
+type WebJobParams =
+    { 
+      /// Specifies the zip output path.
+      OutputPath : string }
+
+/// Default parameters for the WebJobs task
+let WebJobDefaultParams = 
+    { OutputPath = null}
+
+/// This task to can be used create a zip for each webjob to deploy to a website
+/// The output structure is: `outputpath/{websitename}/webjobs/{continuous/triggered}/{webjobname}.zip`
+/// ## Parameters
+///
+///  - `setParams` - Function used to overwrite webjobs outputpath.
+///  - `websites` - The websites and webjobs to build zips from.
+let BuildZip setParams websites =
+    let parameters = setParams WebJobDefaultParams 
+    let zipWebJob siteOutputPath webJob = 
+        let releaseDirectory = directoryInfo ("src/" + webJob.Project + "/bin/Release")
+        let zipDirectory = directoryInfo siteOutputPath
+        ensureDirExists zipDirectory
+        let zipName = Path.Combine(zipDirectory.FullName, webJob.Name + ".zip")
+        let fileToZip = releaseDirectory.GetFiles() |> Array.map (fun f -> f.FullName)
+        CreateZip releaseDirectory.FullName zipName "" 0 false fileToZip
+    websites |> List.iter (fun website -> 
+                    let siteOutputPath = parameters.OutputPath + "/" + website.Url.Host
+                    website.WebJobs |> List.iter (zipWebJob siteOutputPath))
