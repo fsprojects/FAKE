bdf7ef9f96cae4fdcb2a6e3aeea63163d836dca6
Jörg Preiß <joerg.preiss@slesa.de>
2015-01-07 13:04:04 +0100

    Use old style build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+:Build
 cls
 
 .paket\paket.bootstrapper.exe
@@ -17,3 +18,17 @@ SET TARGET="Default"
 IF NOT [%1]==[] (set TARGET="%1")
 
 "packages\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%"
+
+rem Bail if we're running a TeamCity build.
+if defined TEAMCITY_PROJECT_NAME goto Quit
+
+rem Loop the build script.
+set CHOICE=nothing
+echo (Q)uit, (Enter) runs the build again
+set /P CHOICE= 
+if /i "%CHOICE%"=="Q" goto :Quit
+
+GOTO Build
+
+:Quit
+exit /b %errorlevel%
