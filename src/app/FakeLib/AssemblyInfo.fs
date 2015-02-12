678ea1199c52823e93e3a6a6932687d729aaa91a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-12 14:35:43 +0100

    Bump version to 3.16.2

--- src/app/FakeLib/AssemblyInfo.fs
+++ src/app/FakeLib/AssemblyInfo.fs
@@ -7,10 +7,10 @@ open System.Runtime.InteropServices
 [<assembly: InternalsVisibleToAttribute("Test.FAKECore")>]
 [<assembly: GuidAttribute("d6dd5aec-636d-4354-88d6-d66e094dadb5")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.15.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.15.0")>]
-[<assembly: AssemblyFileVersionAttribute("3.15.0")>]
+[<assembly: AssemblyVersionAttribute("3.16.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.16.2")>]
+[<assembly: AssemblyFileVersionAttribute("3.16.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.15.0"
+    let [<Literal>] Version = "3.16.2"
