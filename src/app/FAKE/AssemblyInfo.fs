5cd8b1255449edb16205c9d877e52d22761741aa
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-25 13:50:16 +0100

    Bump version to 3.17.11

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.17.10")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.17.10")>]
-[<assembly: AssemblyFileVersionAttribute("3.17.10")>]
+[<assembly: AssemblyVersionAttribute("3.17.11")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.17.11")>]
+[<assembly: AssemblyFileVersionAttribute("3.17.11")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.17.10"
+    let [<Literal>] Version = "3.17.11"
--- src/app/Fake.SQL/AssemblyInfo.fs
+++ src/app/Fake.SQL/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make SQL Lib")>]
 [<assembly: GuidAttribute("A161EAAF-EFDA-4EF2-BD5A-4AD97439F1BE")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.17.10")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.17.10")>]
-[<assembly: AssemblyFileVersionAttribute("3.17.10")>]
+[<assembly: AssemblyVersionAttribute("3.17.11")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.17.11")>]
+[<assembly: AssemblyFileVersionAttribute("3.17.11")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.17.10"
+    let [<Literal>] Version = "3.17.11"
