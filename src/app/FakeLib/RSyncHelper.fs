8ff721142da43985806c866395a45f88dd04566d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-02 17:33:21 +0100

    Create a simple rsync command

---
+++ src/app/FakeLib/RSyncHelper.fs
@@ -0,0 +1,21 @@
+﻿[<AutoOpen>]
+/// Contains a task to use [rsync](https://rsync.samba.org/) on Linux.
+module Fake.RSync
+
+/// Executes a rsync command.
+/// ## Parameters
+///  - `source` - The source directory
+///  - `destination` - The target directory
+let RSync (source:string) (destination:string) =
+    let args =
+          "-r " +
+            (source |> toParam) +
+            (destination |> toParam) + " -v"
+
+    tracefn "%s" args
+    let result = ExecProcess (fun info ->  
+       info.FileName <- "rsync"
+       info.Arguments <- args) System.TimeSpan.MaxValue
+               
+    if result <> 0 then 
+        failwithf "Error during rsync From: %s To: %s - exit code: %d" source destination result
