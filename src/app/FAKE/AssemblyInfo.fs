9c61ff79fce68a16fc4b027c8a931d89d7fb7c6d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-04 13:56:11 +0100

    Bump version to 3.18.0

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("3.17.14")>]
-[<assembly: AssemblyInformationalVersionAttribute("3.17.14")>]
-[<assembly: AssemblyFileVersionAttribute("3.17.14")>]
+[<assembly: AssemblyVersionAttribute("3.18.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("3.18.0")>]
+[<assembly: AssemblyFileVersionAttribute("3.18.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "3.17.14"
+    let [<Literal>] Version = "3.18.0"
