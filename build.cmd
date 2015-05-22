2243f52de91d21663a832d874d198613a4b3a47e
Jörg Preiß <joerg.preiss@slesa.de>
2015-05-22 08:09:29 +0200

    Revert "Use old style build.cmd"
    This reverts commit bdf7ef9f96cae4fdcb2a6e3aeea63163d836dca6.

--- build.cmd
+++ build.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-:Build
 cls
 
 .paket\paket.bootstrapper.exe
@@ -18,17 +17,3 @@ SET TARGET="Default"
 IF NOT [%1]==[] (set TARGET="%1")
 
 "packages\FAKE\tools\Fake.exe" "build.fsx" "target=%TARGET%"
-
-rem Bail if we're running a TeamCity build.
-if defined TEAMCITY_PROJECT_NAME goto Quit
-
-rem Loop the build script.
-set CHOICE=nothing
-echo (Q)uit, (Enter) runs the build again
-set /P CHOICE= 
-if /i "%CHOICE%"=="Q" goto :Quit
-
-GOTO Build
-
-:Quit
-exit /b %errorlevel%
