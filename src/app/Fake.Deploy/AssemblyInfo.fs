f055c64fc471692aa86ae7ad899d70c7a482c117
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-08-10 11:09:54 +0200

    Revert "Failed with Razor resolution error on cmd. Needs to be investigated"
    This reverts commit 213ef21299a8d4fd5e8470a34e7a6dc342f1c022.

--- src/app/Fake.Deploy/AssemblyInfo.fs
+++ src/app/Fake.Deploy/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy tool")>]
 [<assembly: GuidAttribute("413E2050-BECC-4FA6-87AA-5A74ACE9B8E1")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.0.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("4.0.3")>]
+[<assembly: AssemblyVersionAttribute("4.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("4.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.0.3"
+    let [<Literal>] Version = "4.1.0"
