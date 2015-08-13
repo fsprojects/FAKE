ff5d3687892bdfb9c6580b56a89df9f1cb19d159
Daniel Chambers <daniel@digitallycreated.net>
2015-08-13 21:24:28 +1000

    Added tests for the calcDirsToWatch function to ensure it properly determines duplicate root paths now

---
+++ src/test/Test.FAKECore/ChangeWatcherSpecs.cs
@@ -0,0 +1,34 @@
+﻿using Fake;
+using Machine.Specifications;
+using Microsoft.FSharp.Collections;
+
+namespace Test.FAKECore
+{
+    public class when_calculating_directories_to_watch
+    {
+        It should_watch_multiple_directories =
+            () =>
+            {
+                var includes = ListModule.OfArray(new[] { @"test1\bin\*.dll", @"test2\bin\*.dll", });
+                var fileIncludes = new FileSystem.FileIncludes(@"C:\Project", includes, ListModule.Empty<string>());
+
+                var dirsToWatch = ChangeWatcher.calcDirsToWatch(fileIncludes);
+
+                dirsToWatch.Length.ShouldEqual(2);
+                dirsToWatch.ShouldContain(@"C:\Project\test1\bin");
+                dirsToWatch.ShouldContain(@"C:\Project\test2\bin");
+            };
+
+        It should_only_take_the_most_root_path_when_multiple_directories_share_a_root =
+            () =>
+            {
+                var includes = ListModule.OfArray(new[] { @"tests\**\test1\bin\*.dll", @"tests\test2\bin\*.dll", });
+                var fileIncludes = new FileSystem.FileIncludes(@"C:\Project", includes, ListModule.Empty<string>());
+
+                var dirsToWatch = ChangeWatcher.calcDirsToWatch(fileIncludes);
+
+                dirsToWatch.Length.ShouldEqual(1);
+                dirsToWatch.ShouldContain(@"C:\Project\tests");
+            };
+    }
+}
