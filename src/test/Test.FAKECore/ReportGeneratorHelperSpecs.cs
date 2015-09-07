7bd350f80d6b2901209a7a684dd8bb6b8e2ad925
Richard Kirby <rk755@york.ac.uk>
2015-09-07 21:35:18 +0100

    Additional tests added to ensure default correctness.

--- src/test/Test.FAKECore/ReportGeneratorHelperSpecs.cs
+++ src/test/Test.FAKECore/ReportGeneratorHelperSpecs.cs
@@ -1,7 +1,9 @@
 ﻿using Fake;
 using Machine.Specifications;
 using Microsoft.FSharp.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Test.FAKECore
 {
@@ -21,11 +23,35 @@ namespace Test.FAKECore
         {
             Arguments = ReportGeneratorHelper.buildReportGeneratorArgs(Parameters, Reports);
         };
+
+        protected static readonly IEnumerable<string> SupportedReportTypes = new List<string>
+        {
+            "Html",
+            "HtmlSummary",
+            "Xml",
+            "XmlSummary",
+            "Latex",
+            "LatexSummary",
+            "Badges"
+        };
     }
 
     internal class when_executing_with_default_arguments : BuildReportArgumentsSpecs
     {
         It should_use_current_directory_as_target_directory =
             () => Arguments.ShouldContain(Directory.GetCurrentDirectory());
+        It should_only_use_html_report_type =
+            () =>
+            {
+                Arguments.ShouldContain("-reporttypes:Html");
+                foreach (string reportType in SupportedReportTypes.Except(new List<string> { "Html" }))
+                {
+                    Arguments.ShouldNotContain(reportType);
+                }
+            };
+        It should_not_append_source_dirs =
+            () => Arguments.ShouldNotContain("-sourcedirs:");
+        It should_not_append_filters =
+            () => Arguments.ShouldNotContain("-filters:");
     }
 }
