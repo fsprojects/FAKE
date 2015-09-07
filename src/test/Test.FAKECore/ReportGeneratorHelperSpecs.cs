aefc9446a0cc2245fcda4ea54649087f96fc24cb
Richard Kirby <rk755@york.ac.uk>
2015-09-07 21:42:14 +0100

    Default expectations in place.

--- src/test/Test.FAKECore/ReportGeneratorHelperSpecs.cs
+++ src/test/Test.FAKECore/ReportGeneratorHelperSpecs.cs
@@ -39,7 +39,7 @@ namespace Test.FAKECore
     internal class when_executing_with_default_arguments : BuildReportArgumentsSpecs
     {
         It should_use_current_directory_as_target_directory =
-            () => Arguments.ShouldContain(Directory.GetCurrentDirectory());
+            () => Arguments.ShouldContain("-targetdir:" + Directory.GetCurrentDirectory());
         It should_only_use_html_report_type =
             () =>
             {
@@ -49,9 +49,8 @@ namespace Test.FAKECore
                     Arguments.ShouldNotContain(reportType);
                 }
             };
-        It should_not_append_source_dirs =
-            () => Arguments.ShouldNotContain("-sourcedirs:");
-        It should_not_append_filters =
-            () => Arguments.ShouldNotContain("-filters:");
+        It should_not_append_source_dirs = () => Arguments.ShouldNotContain("-sourcedirs:");
+        It should_not_append_filters = () => Arguments.ShouldNotContain("-filters:");
+        It should_have_a_log_verbosity_of_verbose = () => Arguments.ShouldContain("-verbosity:Verbose");
     }
 }
