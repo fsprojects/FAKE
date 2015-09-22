6ffe726bc96ef6d0fba93a7306cf414548f2ff01
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-09-22 10:32:50 +0200

    Bump version to 4.4.3

--- src/app/Fake.FluentMigrator/AssemblyInfo.fs
+++ src/app/Fake.FluentMigrator/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make FluentMigrator Lib")>]
 [<assembly: GuidAttribute("E18BDD6F-1AF8-42BB-AEB6-31CD1AC7E56D")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.4.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.4.2")>]
-[<assembly: AssemblyFileVersionAttribute("4.4.2")>]
+[<assembly: AssemblyVersionAttribute("4.4.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.4.3")>]
+[<assembly: AssemblyFileVersionAttribute("4.4.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.4.2"
+    let [<Literal>] Version = "4.4.3"
--- src/app/Fake.SQL/AssemblyInfo.fs
+++ src/app/Fake.SQL/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make SQL Lib")>]
 [<assembly: GuidAttribute("A161EAAF-EFDA-4EF2-BD5A-4AD97439F1BE")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.4.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.4.2")>]
-[<assembly: AssemblyFileVersionAttribute("4.4.2")>]
+[<assembly: AssemblyVersionAttribute("4.4.3")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.4.3")>]
+[<assembly: AssemblyFileVersionAttribute("4.4.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.4.2"
+    let [<Literal>] Version = "4.4.3"
