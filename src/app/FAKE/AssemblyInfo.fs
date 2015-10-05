ad61c84704ebe419ad64375f32bee0dd0ae9323a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-05 14:12:54 +0200

    Bump version to 4.4.6

--- src/app/FAKE/AssemblyInfo.fs
+++ src/app/FAKE/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Command line tool")>]
 [<assembly: GuidAttribute("fb2b540f-d97a-4660-972f-5eeff8120fba")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.4.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.4.5")>]
-[<assembly: AssemblyFileVersionAttribute("4.4.5")>]
+[<assembly: AssemblyVersionAttribute("4.4.6")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.4.6")>]
+[<assembly: AssemblyFileVersionAttribute("4.4.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.4.5"
+    let [<Literal>] Version = "4.4.6"
