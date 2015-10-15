cd18981adfd826886b5d2948e934664023612605
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-10-15 18:53:54 +0200

    Bump version to 4.7.1

--- src/app/Fake.FluentMigrator/AssemblyInfo.fs
+++ src/app/Fake.FluentMigrator/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make FluentMigrator Lib")>]
 [<assembly: GuidAttribute("E18BDD6F-1AF8-42BB-AEB6-31CD1AC7E56D")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.6.4")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.6.4")>]
-[<assembly: AssemblyFileVersionAttribute("4.6.4")>]
+[<assembly: AssemblyVersionAttribute("4.7.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.7.1")>]
+[<assembly: AssemblyFileVersionAttribute("4.7.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.6.4"
+    let [<Literal>] Version = "4.7.1"
