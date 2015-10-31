a9bcb4d4d20b33914976eea69053a5f6faf507df
Toni Kaihola <tonikaihola@users.noreply.github.com>
2015-10-31 08:09:05 +0200

    Removed AutoOpenAttribute from module.

--- src/app/FakeLib/RoboCopyHelper.fs
+++ src/app/FakeLib/RoboCopyHelper.fs
@@ -1,5 +1,4 @@
-﻿[<AutoOpen>]
-/// Contains a task to use [robocopy](https://en.wikipedia.org/wiki/Robocopy) on Windows.
+﻿/// Contains a task to use [robocopy](https://en.wikipedia.org/wiki/Robocopy) on Windows.
 module Fake.RoboCopyHelper
 
 type RoboCopyOptions = { Mirror: bool }
@@ -9,7 +8,7 @@ type RoboCopyOptions = { Mirror: bool }
 ///  - `source` - The source directory
 ///  - `destination` - The target directory
 ///  - `options` - The options to pass to robocopy
-let RoboCopyWithOptions (source:string) (destination:string) (options:RoboCopyOptions) =
+let private roboCopyWithOptions (source:string) (destination:string) (options:RoboCopyOptions) =
     let args =
           "/D /c robocopy " +
             (source.TrimEnd('\\') |> FullName |> toParam) +
@@ -28,11 +27,11 @@ let RoboCopyWithOptions (source:string) (destination:string) (options:RoboCopyOp
 ///  - `source` - The source directory
 ///  - `destination` - The target directory
 let RoboCopy (source:string) (destination:string) =
-    RoboCopyWithOptions source destination { Mirror = false }
+   roboCopyWithOptions source destination { Mirror = false }
 
 /// Executes a RoboCopy mirror command (potentially destructive)
 /// ## Parameters
 ///  - `source` - The source directory
 ///  - `destination` - The target directory
 let RoboCopyMirror (source:string) (destination:string) =
-    RoboCopyWithOptions source destination { Mirror = true }
+    roboCopyWithOptions source destination { Mirror = true }
