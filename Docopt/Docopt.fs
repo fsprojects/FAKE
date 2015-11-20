375796160fe6a951bd62fc4db1013ceaba547be9
szymoc-a <alexandre@szymocha.com>
2015-11-20 19:20:52 +0900

    Added a crap version of poptDesc

--- Docopt/Docopt.fs
+++ Docopt/Docopt.fs
@@ -11,10 +11,47 @@ type Parser<'t> = Parser<'t, UserState>;;
 *)
 
 open System
-
 open FParsec
 
-module Parsers =
+module Token =
+  begin
+    type Argument =
+      struct
+        val Name : string
+        val Type : System.Type
+        val Val : obj
+        new(name', type', val') = { Name=name'; Type=type'; Val=val'; }
+        new(name', type', val') = match type' with
+          | None        -> Argument(name', typeof<string>, val')
+          | Some(tname) -> match tname with
+            | "bool"    -> Argument(name', typeof<bool>, val')
+            | "int"     -> Argument(name', typeof<int32>, val')
+            | "uint"    -> Argument(name', typeof<uint32>, val')
+            | "float"   -> Argument(name', typeof<System.Single>, val')
+            | "double"  -> Argument(name', typeof<System.Double>, val')
+            | "decimal" -> Argument(name', typeof<decimal>, val')
+            | "string"  -> Argument(name', typeof<string>, val')
+            | "time"
+            | "date"    -> Argument(name', typeof<System.DateTime>, val')
+            | _         -> Argument(name', System.Type.GetType(tname), val')
+        override xx.ToString() =
+          sprintf "Argument { Name = %s; Type = %A }" xx.Name xx.Type
+      end
+
+    type Option =
+      struct
+        val Sname : char
+        val Lname : string
+        val Arg : Argument
+        new(sname', lname', arg') = { Sname=sname'; Lname=lname'; Arg=arg'; }
+        override xx.ToString() =
+          sprintf "Option { Sname = %c; Lname = %s; Arg = %A"
+            xx.Sname xx.Lname xx.Arg
+      end
+  end
+;;
+
+module Parser =
   begin
     type UserState = unit
     ;;
@@ -22,10 +59,7 @@ module Parsers =
     type Parser<'t> = Parser<'t, UserState>
     ;;
 
-    type Argument = string
-    ;;
-
-    let pupperArg:Parser<_> =
+    let pupperArg:Parser<string> =
       let start = isUpper in
       let cont c = (isUpper c) || (c = '-') in
       identifier (IdentifierOptions(isAsciiIdStart=start,
@@ -33,13 +67,29 @@ module Parsers =
                                     label="UPPER-CASE identifier"))
     ;;
 
-    let plowerArg:Parser<_> =
+    let plowerArg:Parser<string> =
       satisfyL (( = ) '<') "<lower-case> identifier"
   >>. many1SatisfyL (( <> ) '>') "any character except '>'"
   .>> pchar '>'
     ;;
 
-    let parg:Parser<Argument> = pupperArg <|> plowerArg
+    let ptype:Parser<string> =
+      many1SatisfyL (( <> ) ' ') "valid F# type"
+    ;;
+
+    let parg:Parser<string * string option> =
+      let pidentifier = pupperArg <|> plowerArg in
+      let poptType = opt (skipChar ':' >>. ptype) in
+      pidentifier .>>. poptType
+    ;;
+
+    let poptDesc:Parser<_> =
+      let psopt = satisfyL (( <> ) '-') "short option" in
+      let plopt = let pred c' = isLetter(c') || (c' = '-') in
+                  many1SatisfyL pred "long option" in
+      let space = skipChar ' ' <?> "space" in
+      let p2opt = psopt .>>. ((skipString "  " >>% None) <|> (opt (optional (skipChar ',') .>> space >>. (skipString "--" >>. plopt)))) in
+      spaces >>. skipChar '-' >>. (attempt ((skipChar '-' >>. plopt) |>> (fun s -> (EOS, s))) <|> (p2opt |>> (function c, Some(s) -> (c, s) | c, None -> (c, null))))
     ;;
 
     let popt = ()
