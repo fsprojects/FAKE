c165761e5329a6b350337a50929141cc2fb05bea
szymoc-a <alexandre@szymocha.com>
2015-11-23 11:54:16 +0900

    Ugly version of `poptDescLine`, but it works and it's fast!

--- Docopt/Docopt.fs
+++ Docopt/Docopt.fs
@@ -19,8 +19,8 @@ module Token =
       struct
         val Name : string
         val Type : System.Type
-        val Val : obj
-        new(name', type', val') = { Name=name'; Type=type'; Val=val'; }
+        val Default : obj
+        new(name', type', val') = { Name=name'; Type=type'; Default=val'; }
         new(name', type', val') = match type' with
           | None        -> Argument(name', typeof<string>, val')
           | Some(tname) -> match tname with
@@ -34,8 +34,10 @@ module Token =
             | "time"
             | "date"    -> Argument(name', typeof<System.DateTime>, val')
             | _         -> Argument(name', System.Type.GetType(tname), val')
+        new(name', type':option<_>) = Argument(name', type', null)
         override xx.ToString() =
-          sprintf "Argument { Name = %s; Type = %A }" xx.Name xx.Type
+          sprintf "Argument { Name = %s; Type = %A; Default = %A }"
+            xx.Name xx.Type xx.Default
       end
 
     type Option =
@@ -73,29 +75,97 @@ module Parser =
       .>> pchar '>'
     ;;
 
-    let ptype:Parser<string> =
-      many1SatisfyL (( <> ) ' ') "valid F# type"
-    ;;
-
-    let parg:Parser<string * string option> =
+    let parg:Parser<Token.Argument> =
       let pidentifier = pupperArg <|> plowerArg in
+      let ptype = many1SatisfyL (( <> ) ' ') "valid F# type" in
       let poptType = opt (skipChar ':' >>. ptype) in
-      pidentifier .>>. poptType
+      pidentifier .>>. poptType |>> Token.Argument
     ;;
 
-    let poptDescLine:Parser<_> =
-      let psopt = satisfyL (( <> ) '-') "short option" in
-      let plopt = let pred c' = isLetter(c') || (c' = '-') in
-                  many1SatisfyL pred "long option" in
-      let p2opt = psopt .>>. ((skipString "  " >>% None)
-                          <|> (opt (optional (skipChar ',')
-                                .>> skipChar ' ' <?> "space"
-                                >>. (skipString "--" >>. plopt)))) in
-      spaces
-      >>. skipChar '-'
-      >>. (((skipChar '-' >>. plopt) |>> (fun s -> (EOS, s)))
-           <|> (p2opt |>> (function c, Some(s) -> (c, s) | c, None -> (c, null))))
-      .>>. (spaces >>. restOfLine false)
+    let poptDescLine:Parser<char * string * Token.Argument option> =
+      let soptChars = "?"
+      in let replyErr err' = Reply(Error, ErrorMessageList(err'))
+      in let loptPredicate c' = isLetter(c') || isDigit(c') || c' = '-'
+
+      in let rec ``short or long option`` (stream':CharStream<_>) =
+        let c = stream'.SkipAndPeek() in
+        if isLetter(c) || (isAnyOf soptChars) c
+        then ``short option`` stream' (c, null, None)
+        elif c = '-' then ``expecting long option`` stream' (EOS, null, None)
+        else replyErr(Expected(sprintf "letters or any of «%s»" soptChars))
+
+      and ``short option`` stream' tuple' =
+        match stream'.SkipAndPeek() with
+          | ' ' -> ``argument or hyphens`` stream' tuple'
+          | '=' -> ``expecting arg (short)`` stream' tuple'
+          | ',' -> ``expecting space`` stream' tuple'
+          | _   -> Reply(tuple')
+
+      and ``expecting arg (short)`` stream' (s', l', _) =
+        let () = stream'.Skip() in
+        let r = parg stream' in match r.Status with
+          | Ok -> ``short option plus arg`` stream' (s', l', Some(r.Result))
+          | _  -> Reply(Error, r.Error)
+
+      and ``argument or hyphens`` stream' (s', l', a') =
+        if stream'.SkipAndPeek() = '-'
+        then ``expecting hyphen 2`` stream' (s', l', a')
+        else let r = parg stream' in match r.Status with
+          | Ok -> ``short option plus arg`` stream' (s', l', Some(r.Result))
+          | _  -> Reply(Error, ErrorMessageList(Expected("space"), r.Error))
+
+      and ``short option plus arg`` stream' tuple' =
+        match stream'.Peek() with
+          | ' ' -> ``expecting hyphen 1`` stream' tuple'
+          | ',' -> ``expecting space`` stream' tuple'
+          | _   -> Reply(tuple')
+
+      and ``expecting space`` stream' tuple' =
+        if stream'.SkipAndPeek() = ' '
+        then ``expecting hyphen 1`` stream' tuple'
+        else replyErr(Expected("space"))
+
+      and ``expecting hyphen 1`` stream' tuple' =
+        if stream'.SkipAndPeek() = '-'
+        then ``expecting hyphen 2`` stream' tuple'
+        else replyErr(Expected("'-'"))
+
+      and ``expecting hyphen 2`` stream' tuple' =
+        if stream'.SkipAndPeek() = '-'
+        then ``expecting long option`` stream' tuple'
+        else replyErr(Expected("'-'"))
+
+      and ``expecting long option`` stream' (s', _, a') =
+        let () = stream'.Skip() in
+        match stream'.ReadCharsOrNewlinesWhile(loptPredicate, true) with
+          | "" -> replyErr(Expected("long option (letters or hyphens)"))
+          | id -> ``long option (+short?)`` stream' (s', id, a')
+
+      and ``long option (+short?)`` stream' tuple' =
+        match stream'.Peek() with
+          | '=' -> ``expecting arg (long)`` stream' tuple'
+          | ' ' -> ``expecting arg or space (long)`` stream' tuple'
+          | _   -> Reply(tuple')
+
+      and ``expecting arg or space (long)`` stream' (s', l', a') =
+        if stream'.SkipAndPeek() = ' ' then Reply((s', l', a'))
+        else let r = parg stream' in match r.Status with
+          | Ok -> ``long option plus arg (+short?)`` (s', l', Some(r.Result))
+          | _  -> Reply(Error, ErrorMessageList(Expected("space"), r.Error))
+
+      and ``expecting arg (long)`` stream' (s', l', _) =
+        let () = stream'.Skip() in
+        let r = parg stream' in match r.Status with
+          | Ok -> ``long option plus arg (+short?)`` (s', l', Some(r.Result))
+          | _  -> Reply(Error, r.Error)
+
+      and ``long option plus arg (+short?)`` tuple' = Reply(tuple')
+      in fun stream' ->
+        let state = stream'.State in
+        let _ = stream'.SkipWhitespace() in
+        if stream'.Match('-')
+        then ``short or long option`` stream'
+        else replyErr(Expected("'-'"))
     ;;
 
     let parseOptDesc doc' =
