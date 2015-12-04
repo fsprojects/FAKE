397e2cda031d76cf3c44e3c8acfa017808643b1e
szymoc-a <alexandre@szymocha.com>
2015-12-04 14:29:00 +0900

    Usage parser skeleton finished

--- Build/wuuuh.fsx
+++ Build/wuuuh.fsx
@@ -13,11 +13,37 @@ open Docopt
 
 type Token =
   | Arg of Token.Argument
+  | Sop of string
+  | Lop of string
+  | Cmd of string
   | Xor of Token * Token
   | Ell of Token
+  | Req of Token
+  | Opt of Token
+  | Ano of unit
+  | Dsh of unit
+  | Ssh of unit
 
 let opp = OperatorPrecedenceParser<_, unit, unit>()
-let term = Parser.parg |>> Arg
+let pcmd = many1Satisfy (fun c' -> isLetter c' || isDigit c' || c' = '-' || c' = '_')
+let preq = between (pchar '(' >>. spaces) (pchar ')') opp.ExpressionParser
+let popt = between (pchar '[' >>. spaces) (pchar ']') opp.ExpressionParser
+let pano = skipString "[options]"
+let pdsh = skipString "[--]"
+let pssh = skipString "[-]"
+let term = choice [|Parser.parg |>> Arg;
+                    pcmd |>> Cmd;
+                    preq |>> Req;
+                    pano |>> Ano;
+                    pdsh |>> Dsh;
+                    pssh |>> Ssh;
+                    popt |>> Opt|]
 opp.TermParser <- term .>> spaces
 opp.AddOperator(InfixOperator("|", spaces, 10, Associativity.Left, (fun l' r' -> Xor(l', r'))))
 opp.AddOperator(PostfixOperator("...", spaces, 20, false, Ell))
+
+let eval argv' ast' =
+  let mutable i = 0 in
+  let rec eval = function
+    | _ -> ()
+  in eval ast'
