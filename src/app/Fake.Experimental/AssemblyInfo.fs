821e264e79dbe73a5e67f00defd60265646b37b9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-19 10:21:26 +0100

    Bump version to 4.11.0

--- src/app/Fake.Experimental/AssemblyInfo.fs
+++ src/app/Fake.Experimental/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Experimental Lib")>]
 [<assembly: GuidAttribute("5AA28AED-B9D8-4158-A594-32FE5ABC5713")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.10.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.10.5")>]
-[<assembly: AssemblyFileVersionAttribute("4.10.5")>]
+[<assembly: AssemblyVersionAttribute("4.11.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.11.0")>]
+[<assembly: AssemblyFileVersionAttribute("4.11.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.10.5"
+    let [<Literal>] Version = "4.11.0"
--- src/app/Fake.FluentMigrator/AssemblyInfo.fs
+++ src/app/Fake.FluentMigrator/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make FluentMigrator Lib")>]
 [<assembly: GuidAttribute("E18BDD6F-1AF8-42BB-AEB6-31CD1AC7E56D")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.10.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.10.5")>]
-[<assembly: AssemblyFileVersionAttribute("4.10.5")>]
+[<assembly: AssemblyVersionAttribute("4.11.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.11.0")>]
+[<assembly: AssemblyFileVersionAttribute("4.11.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.10.5"
+    let [<Literal>] Version = "4.11.0"
