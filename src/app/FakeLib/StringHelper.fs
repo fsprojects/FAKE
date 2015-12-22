ae3453c17e09c7f7cc2b02ff403e71aa9cc6ba91
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-22 14:51:17 +0100

    Fix indent error

--- src/app/FakeLib/StringHelper.fs
+++ src/app/FakeLib/StringHelper.fs
@@ -80,15 +80,19 @@ let inline appendIfFalse p = appendIfTrue (not p)
 
 /// Appends a text without quoting if the value is not null.
 let inline appendWithoutQuotesIfNotNull (value : Object) s = 
-    appendIfTrueWithoutQuotes (value <> null) (match value with
-                                  | :? String as sv -> (sprintf "%s%s" s sv)
-                                  | _ -> (sprintf "%s%A" s value))
+    appendIfTrueWithoutQuotes 
+        (value <> null) 
+        (match value with
+         | :? String as sv -> (sprintf "%s%s" s sv)
+         | _ -> (sprintf "%s%A" s value))
 
 /// Appends a text if the value is not null.
 let inline appendIfNotNull (value : Object) s = 
-    appendIfTrue (value <> null) (match value with
-                                  | :? String as sv -> (sprintf "%s%s" s sv)
-                                  | _ -> (sprintf "%s%A" s value))
+    appendIfTrue 
+        (value <> null) 
+        (match value with
+         | :? String as sv -> (sprintf "%s%s" s sv)
+         | _ -> (sprintf "%s%A" s value))
 
 /// Appends a quoted text if the value is not null.
 let inline appendQuotedIfNotNull (value : Object) s (builder : StringBuilder) = 
