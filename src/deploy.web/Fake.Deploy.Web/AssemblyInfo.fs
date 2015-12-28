27b156e2fd0249ce0391702fb6654629020f28e3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-12-28 11:25:02 +0100

    Bump version to 4.12.0

--- src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
+++ src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
@@ -5,10 +5,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Web")>]
 [<assembly: GuidAttribute("27BA7705-3F57-47BE-B607-8A46B27AE876")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.11.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.11.3")>]
-[<assembly: AssemblyFileVersionAttribute("4.11.3")>]
+[<assembly: AssemblyVersionAttribute("4.12.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.12.0")>]
+[<assembly: AssemblyFileVersionAttribute("4.12.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.11.3"
+    let [<Literal>] Version = "4.12.0"
