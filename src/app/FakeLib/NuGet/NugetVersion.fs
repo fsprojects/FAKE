39da38620184168c317be71aa2a5fe34a368f926
Flechner Romain <rflechner@romcyber.com>
2016-01-05 11:52:46 +0100

    document code

--- src/app/FakeLib/NuGet/NugetVersion.fs
+++ src/app/FakeLib/NuGet/NugetVersion.fs
@@ -14,35 +14,43 @@ type NugetSearchResponse =
     { d:NugetSearchResult }
 type NugetVersionIncrement = string -> Version
     
-let positive i = Math.Max(0, i)
+let private positive i = Math.Max(0, i)
 
+/// Increment build number of a version
 let IncBuild:NugetVersionIncrement = 
     fun (version:string) ->
         let v = Version version
         sprintf "%d.%d.%d" (positive v.Major) (positive v.Minor) (positive v.Build+1)
         |> Version
+
+/// Increment minor version
 let IncMinor:NugetVersionIncrement = 
     fun (version:string) ->
         let v = Version version
         let n = sprintf "%d.%d.%d" (positive v.Major) (positive v.Minor+1) (positive v.Build)
         Version n
+
+/// Increment major version
 let IncMajor:NugetVersionIncrement = 
     fun (version:string) ->
         let v = Version version
         sprintf "%d.%d.%d" (positive v.Major+1) (positive v.Minor) (positive v.Build)
         |> Version
-    
+
+/// Arguments for the next nuget version number computing
 type NugetVersionArg =
     { Server:string
       PackageName:string
       Increment:NugetVersionIncrement
       DefaultVersion:string }
+    /// Default arguments to compute next nuget version number
     static member Default() =
         { Server="https://www.nuget.org/api/v2"
           PackageName=""
           Increment=IncBuild
           DefaultVersion="1.0" }
 
+/// Retrieve current nuget version number
 let getlastNugetVersion server (packageName:string) = 
     let escape = Uri.EscapeDataString
     let url = 
@@ -60,7 +68,8 @@ let getlastNugetVersion server (packageName:string) =
     |> Seq.sortByDescending (fun i -> i.Published)
     |> Seq.tryHead
     |> fun i -> match i with | Some v -> Some v.Version | None -> None
-    
+
+/// Compute next nuget version number
 let nextVersion (f : NugetVersionArg -> NugetVersionArg) =
     let arg = f (NugetVersionArg.Default())
     match getlastNugetVersion arg.Server arg.PackageName with
