81d9f28dd49e2c3223711a6c2bcd6c9a511cedfd
Kunjan Dalal <kunjee17@gmail.com>
2016-01-21 21:21:15 +0530

    Commit message will come for old and new git version

--- src/app/FakeLib/Git/CommitMessage.fs
+++ src/app/FakeLib/Git/CommitMessage.fs
@@ -8,9 +8,9 @@ open System.Text
 open System.IO
 
 /// Returns the commit message file.
-let getCommitMessageFileInfo repositoryDir = 
-    (findGitDir repositoryDir).FullName + "\\COMMITMESSAGE"
-      |> fileInfo
+let getCommitMessageFileInfo repositoryDir =
+    let oldgitFileInfo = (findGitDir repositoryDir).FullName + "\\COMMITMESSAGE" |> fileInfo
+    if oldgitFileInfo.Exists then oldgitFileInfo else (findGitDir repositoryDir).FullName + "\\COMMIT_EDITMSG" |> fileInfo
 
 /// Gets the commit message
 let getCommitMessage repositoryDir = 
