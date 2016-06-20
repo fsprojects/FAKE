14854def3b15f28665aa0662666851cf6c63097e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-06-20 17:48:58 +0200

    Bump version to 4.29.1

--- src/app/Fake.SQL/AssemblyInfo.fs
+++ src/app/Fake.SQL/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make SQL Lib")>]
 [<assembly: GuidAttribute("A161EAAF-EFDA-4EF2-BD5A-4AD97439F1BE")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.29.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.29.0")>]
-[<assembly: AssemblyFileVersionAttribute("4.29.0")>]
+[<assembly: AssemblyVersionAttribute("4.29.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.29.1")>]
+[<assembly: AssemblyFileVersionAttribute("4.29.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.29.0"
-    let [<Literal>] InformationalVersion = "4.29.0"
+    let [<Literal>] Version = "4.29.1"
+    let [<Literal>] InformationalVersion = "4.29.1"
