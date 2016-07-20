b109ece6aa850fae5bc95d39e39bb686ea8fa929
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-07-20 12:47:24 +0200

    Find Path to SqlPackage in the default parameter

--- src/app/FakeLib/Sql.DacPac.fs
+++ src/app/FakeLib/Sql.DacPac.fs
@@ -16,8 +16,8 @@ type DeployAction =
 
 /// Configuration arguments for DacPac deploy
 type DeployDbArgs = {
-    /// The path to SqlPackage.exe. If none is supplied, a set of default locations will be probed.
-    SqlPackagePath : string option
+    /// The path to SqlPackage.exe.
+    SqlPackageToolPath : string
     /// Type of action to execute. Defaults to Deploy.
     Action : DeployAction
     /// Path to source (path to DACPAC or Connection String).
@@ -31,9 +31,18 @@ type DeployDbArgs = {
     /// Drops objects in the destination that do not exist in the source. Defaults to false.
     DropObjectsNotInSource : bool }
 
+let pathsToCheck =
+    [ ProgramFilesX86 </> @"Microsoft SQL Server\130\DAC\bin\SqlPackage.exe"
+      ProgramFilesX86 </> @"Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\SqlPackage.exe" ]
+
 /// The default DacPac deployment arguments.
 let defaultDeploymentArgs = 
-    { SqlPackagePath = None
+    { SqlPackageToolPath = 
+        pathsToCheck
+        |> List.tryFind File.Exists
+        |> function
+        | Some path -> path
+        | None -> ""
       Action = Deploy
       Source = ""
       Destination = ""
@@ -55,23 +64,18 @@ let deployDb setParams =
     let args = setParams defaultDeploymentArgs
     let action, outputPath = generateCommandLine args.Action
 
-    let pathsToCheck =
-        (args.SqlPackagePath |> Option.toList) @
-        [ ProgramFilesX86 </> @"Microsoft SQL Server\130\DAC\bin\SqlPackage.exe"
-          ProgramFilesX86 </> @"Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\SqlPackage.exe" ]
+    if System.String.IsNullOrWhiteSpace args.SqlPackageToolPath then
+        failwith "No SqlPackage.exe filename was given."
 
-    let sqlPackagePath =
-        pathsToCheck
-        |> List.tryFind File.Exists
-        |> function
-        | Some path -> path
-        | None -> failwith (sprintf "Unable to find a valid instance of SqlPackage.exe. Paths checked were: %A." pathsToCheck)
+    if not (File.Exists args.SqlPackageToolPath) then
+        failwithf "Unable to find a valid instance of SqlPackage.exe. Paths checked were: %A." pathsToCheck
           
     shellExec {
-        Program = sqlPackagePath
+        Program = args.SqlPackagePath
         CommandLine = sprintf """/Action:%s /SourceFile:"%s" /TargetConnectionString:"%s" %s /p:BlockOnPossibleDataLoss=%b /p:DropObjectsNotInSource=%b /p:CommandTimeout=%d""" action args.Source args.Destination outputPath args.BlockOnPossibleDataLoss args.DropObjectsNotInSource args.Timeout
         WorkingDirectory = ""
         Args = [] }
+
     |> function
     | 0 -> ()
     | _ -> failwith "Error executing DACPAC deployment. Please see output for error details."
