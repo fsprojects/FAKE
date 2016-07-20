b3ff3ec6cb0a882a6968cd533e67a955d17ea96d
Isaac Abraham <isaac.abraham@gmail.com>
2016-07-20 11:21:58 +0200

    Probe for both paths and throw helpful error if neither exists.

--- src/app/FakeLib/Sql.DacPac.fs
+++ src/app/FakeLib/Sql.DacPac.fs
@@ -3,6 +3,7 @@ module Fake.Sql.DacPac
 
 open Fake.EnvironmentHelper
 open Fake.ProcessHelper
+open System.IO
 
 /// The type of action to execute.
 type DeployAction =
@@ -40,12 +41,24 @@ let private generateCommandLine args =
     let outputPath = defaultArg(outputPath |> Option.map(sprintf """/OutputPath:"%s" """)) ""
     action, outputPath
 
+let private sqlPackagePaths =
+    [ ProgramFilesX86 </> @"Microsoft SQL Server\130\DAC\bin\SqlPackage.exe"
+      ProgramFilesX86 </> @"Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\SqlPackage.exe" ]
+
 /// Deploys a SQL DacPac or database to another database or DacPac.
 let deployDb modifier =
     let args = modifier defaultDeploymentArgs
     let action, outputPath = generateCommandLine args.Action
-    shellExec {        
-        Program = sprintf @"%s\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\SqlPackage.exe" ProgramFilesX86
+
+    let sqlPackagePath =
+        sqlPackagePaths
+        |> List.tryFind File.Exists
+        |> function
+        | Some path -> path
+        | None -> failwith (sprintf "Unable to find a valid instance of SqlPackage.exe. Valid paths are: %A." sqlPackagePaths)
+          
+    shellExec {
+        Program = sqlPackagePath
         CommandLine = sprintf """/Action:%s /SourceFile:"%s" /TargetConnectionString:"%s" %s /p:BlockOnPossibleDataLoss=%b /p:DropObjectsNotInSource=%b /p:CommandTimeout=%d""" action args.Source args.Destination outputPath args.BlockOnPossibleDataLoss args.DropObjectsNotInSource args.Timeout
         WorkingDirectory = ""
         Args = [] }
