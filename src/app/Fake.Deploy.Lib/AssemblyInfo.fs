8b1007ed0b1c9004c0913effa47e33039c7e570b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-07-21 13:19:25 +0200

    Bump version to 4.33.4

--- src/app/Fake.Deploy.Lib/AssemblyInfo.fs
+++ src/app/Fake.Deploy.Lib/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Lib")>]
 [<assembly: GuidAttribute("AA284C42-1396-42CB-BCAC-D27F18D14AC7")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.33.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.33.3")>]
-[<assembly: AssemblyFileVersionAttribute("4.33.3")>]
+[<assembly: AssemblyVersionAttribute("4.33.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.33.4")>]
+[<assembly: AssemblyFileVersionAttribute("4.33.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.33.3"
-    let [<Literal>] InformationalVersion = "4.33.3"
+    let [<Literal>] Version = "4.33.4"
+    let [<Literal>] InformationalVersion = "4.33.4"
