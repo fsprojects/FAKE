b804f78e419a57bd3e2d264bfe00a899c79bcb6e
Matthias Dittrich <matthi.d@gmail.com>
2016-08-17 00:03:44 +0200

    update build.fsx

--- build.fsx
+++ build.fsx
@@ -1,6 +1,6 @@
 (* -- Fake Dependencies paket-inline
 source https://nuget.org/api/v2
-source .fake/bin/core-v1.0-alpha-07/packages
+source .fake/bin/core-v1.0-alpha-08/packages
 
 nuget System.AppContext prerelease
 nuget Fake.Core.Targets prerelease
