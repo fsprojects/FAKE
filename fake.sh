a9871b09ed9a5678261d3e0fe121a4a57a5bf726
Matthias Dittrich <matthi.d@gmail.com>
2016-08-16 23:50:10 +0200

    bootstrap with alpha8

--- fake.sh
+++ fake.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERBOSE=${VERBOSE:-true}
-FAKE_VERSION=${FAKE_VERSION:-"core-v1.0-alpha-07"}
+FAKE_VERSION=${FAKE_VERSION:-"core-v1.0-alpha-08"}
 
 # Use this instead of the obtain_fake include
 # p=".fake";f="$p/obtain_fake.sh";if [ ! -f "$f" ]; then mkdir -p $p; curl --fail -L -s -o $f https://raw.githubusercontent.com/matthid/FAKE/coreclr/script/obtain_fake.sh; fi; . $f
