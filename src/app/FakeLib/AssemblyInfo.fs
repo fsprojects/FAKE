eaf899a7be2e2c0cb5c00a022f19f67c2d2c337e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-11-17 20:30:05 +0100

    Bump version to 4.45.2

--- src/app/FakeLib/AssemblyInfo.fs
+++ src/app/FakeLib/AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 [<assembly: InternalsVisibleToAttribute("Test.FAKECore")>]
 [<assembly: GuidAttribute("d6dd5aec-636d-4354-88d6-d66e094dadb5")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.45.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.45.1")>]
-[<assembly: AssemblyFileVersionAttribute("4.45.1")>]
+[<assembly: AssemblyVersionAttribute("4.45.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.45.2")>]
+[<assembly: AssemblyFileVersionAttribute("4.45.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "4.45.1"
-    let [<Literal>] InformationalVersion = "4.45.1"
+    let [<Literal>] Version = "4.45.2"
+    let [<Literal>] InformationalVersion = "4.45.2"
