58e5af6a6e96f0fb49dd8adf04ad6c2a82575712
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-04-11 11:27:39 +0200

    Bump version to 4.58.4

--- src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
+++ src/deploy.web/Fake.Deploy.Web/AssemblyInfo.fs
@@ -6,15 +6,15 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyTitleAttribute("FAKE - F# Make Deploy Web")>]
 [<assembly: GuidAttribute("27BA7705-3F57-47BE-B607-8A46B27AE876")>]
 [<assembly: AssemblyProductAttribute("FAKE - F# Make")>]
-[<assembly: AssemblyVersionAttribute("4.58.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("4.58.3")>]
-[<assembly: AssemblyFileVersionAttribute("4.58.3")>]
+[<assembly: AssemblyVersionAttribute("4.58.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("4.58.4")>]
+[<assembly: AssemblyFileVersionAttribute("4.58.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FAKE - F# Make Deploy Web"
     let [<Literal>] Guid = "27BA7705-3F57-47BE-B607-8A46B27AE876"
     let [<Literal>] AssemblyProduct = "FAKE - F# Make"
-    let [<Literal>] AssemblyVersion = "4.58.3"
-    let [<Literal>] AssemblyInformationalVersion = "4.58.3"
-    let [<Literal>] AssemblyFileVersion = "4.58.3"
+    let [<Literal>] AssemblyVersion = "4.58.4"
+    let [<Literal>] AssemblyInformationalVersion = "4.58.4"
+    let [<Literal>] AssemblyFileVersion = "4.58.4"
