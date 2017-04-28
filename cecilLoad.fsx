82b10b69b919bd44b23097ce07fd1e9c7bdc6ac5
Matthias Dittrich <matthi.d@gmail.com>
2017-04-28 23:24:32 +0200

    test with #load

---
+++ cecilLoad.fsx
@@ -0,0 +1,4 @@
+#r @"packages/Mono.Cecil/lib/net40/Mono.Cecil.dll"
+
+let readerParams = new Mono.Cecil.ReaderParameters(AssemblyResolver = null)
+printfn "echo cecilLoad"
--- test.fsx
+++ test.fsx
@@ -1,4 +1,2 @@
-#r @"packages/Mono.Cecil/lib/net40/Mono.Cecil.dll"
-
-let readerParams = new Mono.Cecil.ReaderParameters(AssemblyResolver = null)
-printfn "echo world"
+;;
+#load "cecilLoad.fsx"
