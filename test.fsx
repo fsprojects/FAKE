63080fec1ac173357ef8c021e36ce27b0aa93b97
Matthias Dittrich <matthi.d@gmail.com>
2017-04-29 09:42:05 +0200

    change test a bit

--- test.fsx
+++ test.fsx
@@ -1,3 +1,13 @@
 ;;
 #I @"packages/build/FAKE/tools/"
 #r @"FakeLib.dll"
+
+Target "Clean" (fun _ ->
+    !! "src/*/*/bin"
+    ++ "src/*/*/obj"
+    |> CleanDirs
+
+    CleanDirs [buildDir; testDir; docsDir; apidocsDir; nugetDir; reportDir])
+    
+let readerParams = new Mono.Cecil.ReaderParameters(AssemblyResolver = null)
+printfn "testcecilLoad"
