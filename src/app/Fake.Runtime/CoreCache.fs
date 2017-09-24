467e6fdd1461b06bd48b0d61b661e029b5bc6154
Matthias Dittrich <matthi.d@gmail.com>
2017-09-24 18:34:34 +0200

    add netstandard20

--- src/app/Fake.Runtime/CoreCache.fs
+++ src/app/Fake.Runtime/CoreCache.fs
@@ -260,10 +260,11 @@ let findAndLoadInRuntimeDeps (loadContext:AssemblyLoadContext) (name:AssemblyNam
                     false, None
     match result with
     | Some (location, a) ->
-        if isPerfectMatch then
-            if printDetails then tracefn "Redirect assembly load to known assembly: %s (%A)" strName location
-        else
-            traceFAKE "Redirect assembly from '%s' to '%s' (%A)" strName a.FullName location
+        if printDetails then 
+            if isPerfectMatch then
+                tracefn "Redirect assembly load to known assembly: %s (%A)" strName location
+            else
+                traceFAKE "Redirect assembly from '%s' to '%s' (%A)" strName a.FullName location
         a
     | _ ->
         if not (strName.StartsWith("FSharp.Compiler.Service.resources"))
@@ -281,10 +282,11 @@ let findAndLoadInRuntimeDepsCached =
         if not wasCalled then
             let loadedName = result.GetName()
             let isPerfectMatch = loadedName.Name = name.Name && loadedName.Version = name.Version
-            if not isPerfectMatch then
-                traceFAKE "Redirect assembly from '%A' to previous loaded assembly '%A'" name loadedName
-            else
-                if printDetails then tracefn "Redirect assembly load to previously loaded assembly: %A" loadedName         
+            if printDetails then 
+                if not isPerfectMatch then
+                    traceFAKE "Redirect assembly from '%A' to previous loaded assembly '%A'" name loadedName
+                else
+                    tracefn "Redirect assembly load to previously loaded assembly: %A" loadedName         
         result
 
 #if NETSTANDARD1_6
