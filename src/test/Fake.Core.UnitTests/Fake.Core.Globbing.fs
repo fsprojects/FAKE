1c5d18a4eaf2af1f50fcb1c8b627fc9923047ba4
Oskar Gewalli <oskar@gewalli.se>
2018-02-25 16:16:04 +0100

    Moving ChangeWatcher tests to Globbing tests

--- src/test/Fake.Core.UnitTests/Fake.Core.Globbing.fs
+++ src/test/Fake.Core.UnitTests/Fake.Core.Globbing.fs
@@ -19,4 +19,22 @@ let tests =
                 "folder/file2.exe" ] }
       Expect.equal (globExe.IsMatch "folder/test.exe") true "Glob should match relative paths"
       Expect.equal (globExe.IsMatch (Path.GetFullPath "folder/test.exe")) true "Glob should match full paths"
+    testCase "It should resolve multiple directories" <| fun _ ->
+        let fileIncludes:LazyGlobbingPattern = {
+            BaseDirectory =@"C:\Project"
+            Includes = [@"test1\bin\*.dll"; @"test2\bin\*.dll"]
+            Excludes = [] } 
+        let dirIncludes = GlobbingPattern.GetBaseDirectoryIncludes(fileIncludes)
+        Expect.equal 2 dirIncludes.Length "Should have 2 dirs"
+        Expect.contains dirIncludes Fake.EnvironmentHelper.normalizePath(@"C:\Project\test1\bin") "Should contain first folder"
+        Expect.contains dirIncludes Fake.EnvironmentHelper.normalizePath(@"C:\Project\test2\bin") "Should contain second folder"
+
+    testCase "should only take the most root path when multiple directories share a root" <| fun _ ->
+        let includes:LazyGlobbingPattern = {
+            BaseDirectory =@"C:\Project"
+            Includes = [@"tests\**\test1\bin\*.dll"; @"tests\test2\bin\*.dll"]
+            Excludes = [] } 
+        let dirIncludes = GlobbingPattern.GetBaseDirectoryIncludes(fileIncludes)
+        Expect.equal 1 dirIncludes.Length "Should have only 1 directory"
+        Expect.contains dirIncludes Fake.EnvironmentHelper.normalizePath(@"C:\Project\tests") "Should contain tests folder"
   ]
