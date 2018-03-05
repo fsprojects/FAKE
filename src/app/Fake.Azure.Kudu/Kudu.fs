01c4bb70914959713c166eb3e8a238275c083c00
Ryan Riley <ryanriley@live.com>
2018-03-05 14:42:35 -0800

    Add FAKE 5 Azure modules

--- src/app/Fake.Azure.Kudu/Kudu.fs
+++ src/app/Fake.Azure.Kudu/Kudu.fs
@@ -1,29 +1,32 @@
 ﻿/// Contains tasks to stage and deploy Azure website and webjobs using source code deployment with Kudu Sync.
 module Fake.Azure.Kudu
 
-open Fake
 open System
 open System.IO
+open Fake.Core
+open Fake.Core.Environment
+open Fake.Core.Trace
+open Fake.IO
 
 /// Location where staged outputs should go before synced up to the site.
-let deploymentTemp = getBuildParamOrDefault "DEPLOYMENT_TEMP" (Path.GetTempPath() + "kudutemp")
+let deploymentTemp = environVarOrDefault "DEPLOYMENT_TEMP" (Path.GetTempPath() + "kudutemp")
 /// Location where synced outputs should be deployed to.
-let deploymentTarget = getBuildParamOrDefault "DEPLOYMENT_TARGET" (Path.GetTempPath() + "kudutarget")
+let deploymentTarget = environVarOrDefault "DEPLOYMENT_TARGET" (Path.GetTempPath() + "kudutarget")
 /// Used by KuduSync for tracking and diffing deployments.
-let nextManifestPath = getBuildParam "NEXT_MANIFEST_PATH"
+let nextManifestPath = environVarOrDefault "NEXT_MANIFEST_PATH" String.Empty
 /// Used by KuduSync for tracking and diffing deployments.
-let previousManifestPath = getBuildParam "PREVIOUS_MANIFEST_PATH"
+let previousManifestPath = environVarOrDefault "PREVIOUS_MANIFEST_PATH" String.Empty
 /// The path to the KuduSync application.
-let kuduPath = (getBuildParamOrDefault "GO_WEB_CONFIG_TEMPLATE" ".") |> directory
+let kuduPath = (environVarOrDefault "GO_WEB_CONFIG_TEMPLATE" ".") |> DirectoryInfo.ofPath
 
 /// The different types of web jobs.
 type WebJobType = Scheduled | Continuous
 
 // Some initial cleanup / prep
 do
-    CreateDir deploymentTemp
-    CreateDir deploymentTarget
-    CleanDir deploymentTemp
+    Directory.CreateDirectory deploymentTemp |> ignore
+    Directory.CreateDirectory deploymentTarget |> ignore
+    Shell.CleanDir deploymentTemp
 
 /// <summary>
 /// Stages a folder and all subdirectories into the temp deployment area, ready for deployment into the website.
@@ -31,7 +34,7 @@ do
 /// <param name="source">The source folder to copy.</param>
 /// <param name="shouldInclude">A predicate which includes files from the folder. If the entire directory should be copied, this predicate should always return true.</param>
 let stageFolder source shouldInclude =
-    FileHelper.CopyRecursive source deploymentTemp true
+    Shell.CopyRecursive source deploymentTemp true
     |> Seq.filter (not << shouldInclude)
     |> Seq.iter File.Delete
 
@@ -43,16 +46,17 @@ let getWebJobPath webJobType webJobName =
 /// Stages a set of files into a WebJob folder in the temp deployment area, ready for deployment into the website as a webjob.
 let stageWebJob webJobType webJobName files =
     let webJobPath = getWebJobPath webJobType webJobName
-    CreateDir webJobPath
-    files |> FileHelper.CopyFiles webJobPath
+    Directory.CreateDirectory webJobPath |> ignore
+    files |> Shell.CopyFiles webJobPath
 
 /// Synchronises all staged files from the temporary deployment to the actual deployment, removing
 /// any obsolete files, updating changed files and adding new files.
 let kuduSync() =
     let succeeded, output =
-        ProcessHelper.ExecProcessRedirected(fun psi ->
-            psi.FileName <- combinePaths kuduPath "kudusync.cmd"
-            psi.Arguments <- sprintf """-v 50 -f "%s" -t "%s" -n "%s" -p "%s" -i ".git;.hg;.deployment;deploy.cmd""" deploymentTemp deploymentTarget nextManifestPath previousManifestPath)
+        Process.ExecRedirected(fun psi ->
+            { psi with
+                FileName = Path.Combine(kuduPath.FullName, "kudusync.cmd")
+                Arguments = sprintf """-v 50 -f "%s" -t "%s" -n "%s" -p "%s" -i ".git;.hg;.deployment;deploy.cmd""" deploymentTemp deploymentTarget nextManifestPath previousManifestPath })
             (TimeSpan.FromMinutes 5.)
     output |> Seq.iter (fun cm -> printfn "%O: %s" cm.Timestamp cm.Message)
     if not succeeded then failwith "Error occurred during Kudu Sync deployment."
