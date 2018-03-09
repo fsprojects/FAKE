8d9c0083614a7d3148d519c870bc76d1e948ffe4
Matthias Dittrich <matthi.d@gmail.com>
2018-03-09 22:05:15 +0100

    cleanup

--- build.fsx
+++ build.fsx
@@ -928,7 +928,6 @@ Target.Create "FastRelease" (fun _ ->
         | s when not (System.String.IsNullOrWhiteSpace s) -> s
         | _ -> failwith "please set the github_token environment variable to a github personal access token with repro access."
 
-// #if BOOTSTRAP
     let files = 
         runtimes @ [ "portable"; "packages" ]
         |> List.map (fun n -> sprintf "nuget/dotnetcore/Fake.netcore/fake-dotnetcore-%s.zip" n)
@@ -938,20 +937,6 @@ Target.Create "FastRelease" (fun _ ->
     |> GitHub.UploadFiles files    
     |> GitHub.PublishDraft
     |> Async.RunSynchronously
-// #else
-//     let draft =
-//         GitHub.createClientWithToken token
-//         |> GitHub.createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
-//     let draftWithFiles =
-//         runtimes @ [ "portable"; "packages" ]
-//         |> List.map (fun n -> sprintf "nuget/dotnetcore/Fake.netcore/fake-dotnetcore-%s.zip" n)
-//         |> List.fold (fun state item ->
-//             state
-//             |> GitHub.uploadFile item) draft
-//     draftWithFiles
-//     |> GitHub.releaseDraft
-//     |> Async.RunSynchronously
-// #endif
 )
 
 open System
