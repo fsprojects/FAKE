594e3ca509232a8416183acfc35579b78b7000ad
Vance Palacio <vanceism7@gmail.com>
2018-05-05 16:10:55 -0700

    Added PR requested changes

--- src/app/Fake.Windows.Registry/Registry.fs
+++ src/app/Fake.Windows.Registry/Registry.fs
@@ -1,4 +1,35 @@
 ﻿/// Contains functions which allow to read and write information from/to the registry.
+///
+/// ## Sample
+/// 
+/// #### Create a subkey
+/// let subkey = "Company/MyApp"
+/// Registry.createRegistrySubKey Registry.HKEYCurrentUser subkey
+///
+/// #### Write a key-value pair to a subkey
+/// Registry.setRegistryValue Registry.HKEYCurrentUser subkey "AppType" "Premium"
+/// Registry.setRegistryValue Registry.HKEYCurrentUser subkey "Version" "1.0.4"
+///
+/// #### Get a list of key-value names in a subkey
+/// let values = Registry.getRegistryValueNames Registry.HKEYCurrentUser subkey
+/// values |> Array.iter (Trace.trace << (sprintf "Found value name: %s!"))
+///
+/// #### Read the value of a key-value pair
+/// let AppType = Registry.getRegistryValue Registry.HKEYCurrentUser subkey values.[0]
+/// Trace.trace (sprintf "You are running the %s version" AppType)
+///
+/// #### Check if a value exists within a subkey
+/// let exists b = if b then Trace.trace "It exists!" else Trace.trace "It doesn't exist!"
+/// exists <| Registry.valueExistsForKey Registry.HKEYCurrentUser subkey "DateCreated"
+/// exists <| Registry.valueExistsForKey Registry.HKEYCurrentUser subkey "Version"
+///
+/// #### Delete a key-value pair from a subkey
+/// Registry.deleteRegistryValue Registry.HKEYCurrentUser subkey "AppType"
+///
+/// #### Delete a subkey
+/// Registry.deleteRegistrySubKey Registry.HKEYCurrentUser subkey
+
+[<RequireQualifiedAccess>]
 module Fake.Windows.Registry
 
 open Microsoft.Win32
@@ -12,6 +43,10 @@ type RegistryBaseKey =
     | HKEYCurrentConfig
     | HKEYPerformanceData
 
+(*
+Lower level Registry Queries
+(Should these be private?)
+*)
 /// Maps the RegistryBaseKey to a RegistryKey
 /// [omit]
 let getKey name =
@@ -55,6 +90,9 @@ let getRegistryKey baseKey subKey (writePermission : bool) =
     if (isNull >> not) x64BitKey then x64BitKey else
     (get32BitKey baseKey).OpenSubKey(subKey, writePermission)  // fall back to 32 bit
 
+(*
+Registry Value Commands
+*)
 /// Gets a registry value as string
 let getRegistryValue baseKey subKey name =
     use key = getRegistryKey baseKey subKey false
@@ -95,6 +133,9 @@ let valueExistsForKey = fun baseKey subKey name ->
     getRegistryValueNames baseKey subKey
     |> Seq.exists (fun n -> n = name)
 
+(*
+Subkey Commands
+*)
 /// Create a registry subKey
 let createRegistrySubKey baseKey subKey =
     use key = getKey baseKey
