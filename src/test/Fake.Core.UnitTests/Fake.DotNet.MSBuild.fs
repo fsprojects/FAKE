2aae4edb251b2f64fb2b628eda5e8a6aabb9f3c6
Matthias Dittrich <matthi.d@gmail.com>
2018-05-10 15:04:44 +0200

    improve cmd-line generation in msbuild

---
+++ src/test/Fake.Core.UnitTests/Fake.DotNet.MSBuild.fs
@@ -0,0 +1,17 @@
+module Fake.DotNet.MSBuildTests
+
+open Fake.Core
+open Fake.DotNet
+open Expecto
+
+[<Tests>]
+let tests =
+  testList "Fake.DotNet.MSBuild.Tests" [
+    testCase "Test that we can create simple msbuild cmdline" <| fun _ ->
+      let cmdLine =
+        { MSBuildParams.Create() with
+            Properties = ["OutputPath", "C:\\Test\\"] }
+        |> MSBuild.serializeMSBuildParams
+      let expected = "\"/m\" \"/nodeReuse:False\" \"/p:RestorePackages=False\" \"/p:OutputPath=C:\\Test\\\\\""    
+      Expect.equal cmdLine expected "Expected a given cmdline."
+  ]
