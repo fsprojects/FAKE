56ff779c2622f6caa76a72701ab0497496e13d92
Matthias Dittrich <matthi.d@gmail.com>
2018-05-21 12:53:08 +0200

    Use Console.WriteLine which hopefully prevents races in parallel mode.

--- src/app/Fake.BuildServer.TeamCity/TeamCityInternal.fs
+++ src/app/Fake.BuildServer.TeamCity/TeamCityInternal.fs
@@ -72,13 +72,19 @@ module internal TeamCityWriter =
 
     /// Send message to TeamCity
     let sendToTeamCity (format:PrintfFormat<string -> unit, _, _, unit>) message =
-        printf format (scrub message)
+        sprintf format (scrub message)
+        // printf is racing with others in parallel mode
+        |> fun s -> System.Console.WriteLine("\n{0}", s)
 
     let sendToTeamCity2 (format:PrintfFormat<string -> string -> unit, _, _, unit>) param1 param2 =
-        printf format (scrub param1) (scrub param2)
+        sprintf format (scrub param1) (scrub param2)
+        // printf is racing with others in parallel mode
+        |> fun s -> System.Console.WriteLine("\n{0}", s)
 
     let sendStrToTeamCity str =
-        printf "%s" str
+        sprintf "%s" str
+        // printf is racing with others in parallel mode
+        |> fun s -> System.Console.WriteLine("\n{0}", s)
 
     /// Open Named Block
     let sendOpenBlock name description = sendToTeamCity2 "##teamcity[blockOpened name='%s' description='%s']" name description
