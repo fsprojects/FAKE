6aeeed783a03965190ab9b34592579e7cc7090f8
Matthias Dittrich <matthi.d@gmail.com>
2018-05-21 15:15:06 +0200

    .

--- src/app/Fake.Runtime/Path.fs
+++ src/app/Fake.Runtime/Path.fs
@@ -13,26 +13,30 @@ let getCurrentDirectory () =
 
 let private nugetDir = Path.GetFullPath(Paket.Constants.UserNuGetPackagesFolder).TrimEnd([|'/' ;'\\'|]) + "/"
 let fixPathForCache scriptPath (s:string) =
-    let norm = Path.GetFullPath s
-    let scriptDir = Path.GetFullPath(Path.GetDirectoryName scriptPath)
-    if norm.StartsWith(nugetDir, if Paket.Utils.isWindows then StringComparison.OrdinalIgnoreCase else StringComparison.Ordinal) then
-      sprintf "nugetcache://%s" (norm.Substring (nugetDir.Length))
-    else
-      let scriptDir = Uri(Path.GetDirectoryName scriptDir + "/")
-      let other = Uri(s)
-      let rel = scriptDir.MakeRelativeUri(other)
-      if rel.IsAbsoluteUri then rel.AbsoluteUri
-      else
-        sprintf "scriptpath:///%s" rel.OriginalString
+    try
+        let norm = Path.GetFullPath s
+        let scriptDir = Path.GetDirectoryName (Path.GetFullPath scriptPath)
+        if norm.StartsWith(nugetDir, if Paket.Utils.isWindows then StringComparison.OrdinalIgnoreCase else StringComparison.Ordinal) then
+          sprintf "nugetcache://%s" (norm.Substring (nugetDir.Length))
+        else
+          let scriptDir = Uri(Path.GetDirectoryName scriptDir + "/")
+          let other = Uri(s)
+          let rel = scriptDir.MakeRelativeUri(other)
+          if rel.IsAbsoluteUri then rel.AbsoluteUri
+          else
+            sprintf "scriptpath:///%s" rel.OriginalString
+    with e -> raise <| exn (sprintf "Error in fixPathForCache '%s' '%s'" scriptPath s, e)
 
 let readPathFromCache scriptPath (s:string) =
-    let uri = Uri(s)
-    let scriptDir = Path.GetFullPath(Path.GetDirectoryName scriptPath)
-    if uri.Scheme = "nugetcache" then
-        let rel = uri.AbsolutePath.TrimStart [| '/'|]
-        Path.Combine (nugetDir, rel)
-    elif uri.Scheme = "scriptpath" then
-        let rel = uri.OriginalString.Substring("scriptpath:///".Length)
-        Path.Combine(Path.GetDirectoryName scriptDir, rel)
-    else
-        uri.AbsolutePath
+    try
+        let uri = Uri(s)
+        let scriptDir = Path.GetDirectoryName(Path.GetFullPath scriptPath)
+        if uri.Scheme = "nugetcache" then
+            let rel = uri.AbsolutePath.TrimStart [| '/'|]
+            Path.Combine (nugetDir, rel)
+        elif uri.Scheme = "scriptpath" then
+            let rel = uri.OriginalString.Substring("scriptpath:///".Length)
+            Path.Combine(Path.GetDirectoryName scriptDir, rel)
+        else
+            uri.AbsolutePath
+    with e -> raise <| exn (sprintf "Error in readPathFromCache '%s' '%s'" scriptPath s, e)
