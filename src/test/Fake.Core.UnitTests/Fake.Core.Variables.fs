d662d6349844ec776415cfff39393f34c95b6bd1
Matthias Dittrich <matthi.d@gmail.com>
2018-06-04 20:06:26 +0200

    Fix compilation

--- src/test/Fake.Core.UnitTests/Fake.Core.Variables.fs
+++ src/test/Fake.Core.UnitTests/Fake.Core.Variables.fs
@@ -1,42 +1,42 @@
 module Fake.Core.VariablesTests
 
-open Fake.Core.Variables
+open Fake.Core
 open Expecto
 
 [<Tests>]
 let tests = 
   testList "Fake.Core.Variables.Tests" [   
     Fake.ContextHelper.fakeContextTestCase "Ability to set and get fake variables" <| fun _ ->
-        set "Test" "TestValue" |> ignore
-        let value = get<string> "Test"
+        Variables.set "Test" "TestValue" |> ignore
+        let value = Variables.get<string> "Test"
         Expect.isSome value "Variable 'Test' is none"
         Expect.equal "TestValue" value.Value "Variable 'Test' is incorrect"
 
     Fake.ContextHelper.fakeContextTestCase "When type does not match, errors" <| fun _ ->
-        set "Test" "TestValue" |> ignore
+        Variables.set "Test" "TestValue" |> ignore
         try
-            get<bool> "Test" |> ignore
+            Variables.get<bool> "Test" |> ignore
             Tests.failtest "Expected exception"
         with e ->
             Expect.equal "Cast error on variable 'Test'" e.Message "Incorrect failure message for variable failure case"
 
     Fake.ContextHelper.fakeContextTestCase "Ability to set and get fake variables with default - when found" <| fun _ ->
-        set "Test" "TestValue" |> ignore
-        let value = getOrDefault<string> "Test" "DefaultValue"
+        Variables.set "Test" "TestValue" |> ignore
+        let value = Variables.getOrDefault<string> "Test" "DefaultValue"
         Expect.equal "TestValue" value "Variable 'Test' is incorrect"
 
     Fake.ContextHelper.fakeContextTestCase "Ability to set and get fake variables with default - when not found" <| fun _ ->
-        let value = getOrDefault<string> "Test" "DefaultValue"
+        let value = Variables.getOrDefault<string> "Test" "DefaultValue"
         Expect.equal "DefaultValue" value "Variable 'Test' is not the default"
 
     Fake.ContextHelper.fakeContextTestCase "Ability to set and get fake variables with failure - when found" <| fun _ ->
-        set "Test" "TestValue" |> ignore
-        let value = getOrFail<string> "Test"
+        Variables.set "Test" "TestValue" |> ignore
+        let value = Variables.getOrFail<string> "Test"
         Expect.equal "TestValue" value "Variable 'Test' is incorrect"
 
     Fake.ContextHelper.fakeContextTestCase "Ability to set and get fake variables with failure - when not found" <| fun _ ->
         try
-            getOrFail<string> "Test" |> ignore
+            Variables.getOrFail<string> "Test" |> ignore
             Tests.failtest "Expected exception"
         with e ->
             Expect.equal "Unable to find variable 'Test'" e.Message "Incorrect failure message for variable failure case"
