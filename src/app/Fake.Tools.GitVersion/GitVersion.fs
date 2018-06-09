ca149d80da15e50e0e325868b104fe04d10172e4
Jiří Landsman <landsman@cngroup.dk>
2018-06-09 12:24:34 +0200

    fixed formating

--- src/app/Fake.Tools.GitVersion/GitVersion.fs
+++ src/app/Fake.Tools.GitVersion/GitVersion.fs
@@ -7,53 +7,52 @@ open System.IO
 open Fake.IO.Globbing
 open Fake.Core
 
-[<CLIMutable>]
 type GitversionParams = {
     ToolPath : string
 }
 
-let GitversionDefaults = {
+let private gitversionDefaults = {
     ToolPath = Tools.findToolInSubPath "GitVersion.exe" (Environment.environVarOrDefault "ChocolateyInstall" (Directory.GetCurrentDirectory()))
 }
 
 type GitVersionProperties = {
-                                Major : int;
-                                Minor : int;
-                                Patch : int;
-                                PreReleaseTag : string;
-                                PreReleaseTagWithDash : string;
-                                PreReleaseLabel : string;
-                                PreReleaseNumber : Nullable<int>;
-                                BuildMetaData : string;
-                                BuildMetaDataPadded : string;
-                                FullBuildMetaData : string;
-                                MajorMinorPatch : string;
-                                SemVer : string;
-                                LegacySemVer : string;
-                                LegacySemVerPadded : string;
-                                AssemblySemVer : string;
-                                FullSemVer : string;
-                                InformationalVersion : string;
-                                BranchName : string;
-                                Sha : string;
-                                NuGetVersionV2 : string;
-                                NuGetVersion : string;
-                                CommitsSinceVersionSource : int;
-                                CommitsSinceVersionSourcePadded : string;
-                                CommitDate : string;
-                            }
+    Major : int
+    Minor : int
+    Patch : int
+    PreReleaseTag : string
+    PreReleaseTagWithDash : string
+    PreReleaseLabel : string
+    PreReleaseNumber : Nullable<int>
+    BuildMetaData : string
+    BuildMetaDataPadded : string
+    FullBuildMetaData : string
+    MajorMinorPatch : string
+    SemVer : string
+    LegacySemVer : string
+    LegacySemVerPadded : string
+    AssemblySemVer : string
+    FullSemVer : string
+    InformationalVersion : string
+    BranchName : string
+    Sha : string
+    NuGetVersionV2 : string
+    NuGetVersion : string
+    CommitsSinceVersionSource : int
+    CommitsSinceVersionSourcePadded : string
+    CommitDate : string
+}
 
 /// Runs [GitVersion](https://gitversion.readthedocs.io/en/latest/) on a .NET project file.
 /// ## Parameters
 ///
-///  - `setParams` - Function used to manipulate the GitversionDefaults value.
+///  - `setParams` - Function used to manipulate the gitversionDefaults value.
 ///
 /// ## Sample
 ///
 ///      generateProperties id // Use Defaults
 ///      generateProperties (fun p -> { p with ToolPath = "/path/to/directory" }
 let generateProperties (setParams : GitversionParams -> GitversionParams) =
-    let parameters = GitversionDefaults |> setParams
+    let parameters = gitversionDefaults |> setParams
     let timespan =  TimeSpan.FromMinutes 1.
 
     let result = Process.execWithResult (fun info ->
