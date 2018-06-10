2ddaca7ce490dc4a366bd4636fbe5141a016532f
Chet Husk <chusk3@gmail.com>
2018-06-10 10:44:24 -0500

    fix for shell script executable bit and scaffolding for tests

---
+++ src/test/Fake.Dotnet.Cli.IntegrationTests/TemplateTests.fs
@@ -0,0 +1,11 @@
+﻿module Fake.Dotnet.Cli.IntegrationTests.TemplateTests
+
+open Expecto
+open Expecto.Flip
+open System
+
+
+[<Tests>]
+let tests = 
+  testList "Fake.Dotnet.Cli.IntegrationTests.Template tests" [
+  ]
