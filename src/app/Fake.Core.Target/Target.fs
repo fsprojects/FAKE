f56588a78d20e685628f3c89b6a2ad2015fb26ee
Chris Blyth <blythmeister@gmail.com>
2018-07-02 20:48:41 +0100

    Use null rather than "NoDescription"

--- src/app/Fake.Core.Target/Target.fs
+++ src/app/Fake.Core.Target/Target.fs
@@ -318,7 +318,7 @@ module Target =
           |> Seq.map (fun kv -> kv.Key)
           |> Seq.fold (fun context name ->
                         let target = get name
-                        use t = Trace.traceFinalTarget target.Name (match target.Description with Some d -> d | _ -> "NoDescription") (dependencyString target)
+                        use t = Trace.traceFinalTarget target.Name (match target.Description with Some d -> d | _ -> null) (dependencyString target)
                         let res = runSimpleContextInternal target context
                         if res.HasError
                         then t.MarkFailed()
@@ -334,7 +334,7 @@ module Target =
           |> Seq.map (fun kv -> kv.Key)
           |> Seq.fold (fun context name ->
                         let target = get name
-                        use t = Trace.traceFailureTarget target.Name (match target.Description with Some d -> d | _ -> "NoDescription") (dependencyString target)
+                        use t = Trace.traceFailureTarget target.Name (match target.Description with Some d -> d | _ -> null) (dependencyString target)
                         let res = runSimpleContextInternal target context
                         if res.HasError
                         then t.MarkFailed()
