7885e3aaf75061801df51222458abad5003e5018
Martin Gondermann <magicmonty@pagansoft.de>
2018-10-01 14:44:28 +0200

    Update SpecFlow for Version 2.4

---
+++ src/app/Fake.DotNet.Testing.SpecFlow/SpecFlowNext.fs
@@ -0,0 +1,121 @@
+/// Contains a task which allows to run [SpecFlow](http://www.specflow.org/) tests with SpecFlow v2.4+.
+[<RequireQualifiedAccess>]
+module Fake.DotNet.Testing.SpecFlowNext
+
+open Fake.Core
+open Fake.IO
+open Fake.IO.Globbing
+open Fake.IO.FileSystemOperators
+open System.IO
+open System.Text
+
+type SubCommand =
+    | GenerateAll
+    | StepDefinitionReport
+    | NUnitExecutionReport
+    | MsTestExecutionReport
+
+    override x.ToString () =
+        match x with
+        | GenerateAll -> "GenerateAll"
+        | StepDefinitionReport -> "StepDefinitionReport"
+        | NUnitExecutionReport -> "NUnitExecutionReport"
+        | MsTestExecutionReport -> "MsTestExecutionReport"
+
+/// SpecFlow execution parameter type.
+type SpecFlowParams = { 
+    SubCommand:         SubCommand
+    ProjectFile:        string
+    ToolName:           string
+    ToolPath:           string
+    WorkingDir:         string
+    BinFolder:          string option
+    OutputFile:         string option
+    XmlTestResultFile:  string option
+    TestOutputFile:     string option
+    FeatureLanguage:    string option
+    Verbose:            bool
+    ForceRegeneration:  bool
+    XsltFile:           string option
+}
+
+let private toolname = "specflow.exe"
+let private currentDirectory = Directory.GetCurrentDirectory ()
+
+/// SpecFlow default execution parameters.
+let private SpecFlowDefaults = { 
+    SubCommand =        GenerateAll
+    ProjectFile =       null
+    ToolName =          toolname
+    ToolPath =          Tools.findToolFolderInSubPath toolname (currentDirectory </> "tools" </> "SpecFlow")
+    WorkingDir =        null
+    BinFolder =         None
+    OutputFile =        None
+    XmlTestResultFile = None
+    TestOutputFile =    None
+    FeatureLanguage =   None
+    Verbose =           false
+    ForceRegeneration = false
+    XsltFile =          None
+}
+
+// Runs SpecFlow on a project.
+/// ## Parameters
+///
+///  - `setParams` - Function used to manipulate the default SpecFlow parameter value.
+let run setParams =    
+    let parameters = setParams SpecFlowDefaults
+
+    use __ = Trace.traceTask "SpecFlow " (parameters.SubCommand |> string)
+    let tool = parameters.ToolPath </> parameters.ToolName
+
+    let yieldIfSome paramName value =
+        seq { match value with
+              | Some v ->
+                yield sprintf "--%s" paramName
+                yield v
+              | _ -> () }
+
+    let args = 
+        [
+            yield parameters.SubCommand |> string
+            
+            yield "--ProjectFile"
+            yield parameters.ProjectFile
+            
+            yield! parameters.BinFolder 
+                   |> yieldIfSome "binFolder"
+
+            yield! parameters.OutputFile 
+                   |> yieldIfSome "OutputFile"
+
+            yield! parameters.XmlTestResultFile 
+                   |> yieldIfSome (match parameters.SubCommand with
+                                   | MsTestExecutionReport -> "TestResult"
+                                   | _ -> "xmlTestResult")
+
+            yield! parameters.TestOutputFile
+                   |> yieldIfSome "testOutput"
+
+            yield! parameters.FeatureLanguage 
+                   |> yieldIfSome "FeatureLanguage"
+
+            if parameters.Verbose then yield "verbose"
+            if parameters.ForceRegeneration then yield "force"
+
+            yield! parameters.XsltFile
+                   |> yieldIfSome "XsltFile"
+        ]
+        |> Args.toWindowsCommandLine
+
+    Trace.trace (tool + " " + args)
+
+    let processStartInfo info = 
+         { info with FileName = tool
+                     WorkingDirectory = parameters.WorkingDir
+                     Arguments = args }
+
+    match Process.execSimple processStartInfo System.TimeSpan.MaxValue with
+    | 0 -> ()
+    | errorNumber -> failwithf "SpecFlow %s failed. Process finished with exit code %i" (parameters.SubCommand |> string) errorNumber
+    __.MarkSuccess()
