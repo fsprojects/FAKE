00e55f59364043803999579dbc28cdab4c7d8e31
Matthias Dittrich <matthi.d@gmail.com>
2018-10-05 22:39:32 +0200

    use v3 feed in integration test suite

--- integrationtests/core-simple-runtime-error/before/runtime-error.fsx
+++ integrationtests/core-simple-runtime-error/before/runtime-error.fsx
@@ -1,6 +1,6 @@
 #if FAKE_DEPENDENCIES
 #r "paket: storage: none"
-#r "paket: source https://nuget.org/api/v2"
+#r "paket: source https://api.nuget.org/v3/index.json"
 #r "paket: source ../../../release/dotnetcore"
 #r "paket: //source https://ci.appveyor.com/nuget/paket"
 #r "paket: nuget Fake.Runtime prerelease"
