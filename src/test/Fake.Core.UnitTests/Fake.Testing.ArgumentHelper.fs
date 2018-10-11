921f4ccd72fcd983b8ef47a4f3cac6103d3de938
Matthias Dittrich <matthi.d@gmail.com>
2018-10-11 23:53:06 +0200

    fix helper for a variable number of args

--- src/test/Fake.Core.UnitTests/Fake.Testing.ArgumentHelper.fs
+++ src/test/Fake.Core.UnitTests/Fake.Testing.ArgumentHelper.fs
@@ -6,8 +6,8 @@ open Expecto
 let checkIfMono (file, args) =
     match Environment.isWindows, Process.monoPath with
     | false, Some s when file = s ->
-      Expect.equal args.Args.Length 3 "Expected mono arguments"
-      Expect.equal args.Args.[0] "--debug" "Expected --debug flag"
+      Expect.isGreaterThanOrEqual args.Args.Length 3 "Expected mono arguments"
+      Expect.equal args.Args.[0] "--debug" "Expected first flag to be '--debug'"
       args.Args.[1], Arguments.OfArgs args.Args.[2..]
     | true, _ -> file, args
     | _ ->
