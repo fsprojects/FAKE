09f052c2663a490b92ce5fcacef7b6396a0b3d57
Martin Gondermann <magicmonty@pagansoft.de>
2018-10-12 15:49:38 +0200

    Made checkIfMono a bit more elegant

--- src/test/Fake.Core.UnitTests/Fake.Testing.ArgumentHelper.fs
+++ src/test/Fake.Core.UnitTests/Fake.Testing.ArgumentHelper.fs
@@ -6,13 +6,10 @@ open Expecto
 let checkIfMono (file, args) =
     match Environment.isWindows, Process.monoPath with
     | false, Some s when file = s ->
-      match args.Args with
-      | a when a.Length = 2 ->
-          Expect.equal a.[0] "--debug" "Expected first flag to be '--debug'"
-          a.[1], Arguments.OfArgs []
-      | a when a.Length > 2 ->
-          Expect.equal a.[0] "--debug" "Expected first flag to be '--debug'"
-          a.[1], Arguments.OfArgs a.[2..]
+      match args.Args |> Array.toList with
+      | debugFlag :: file :: rest ->
+          Expect.equal debugFlag "--debug" "Expected first flag to be '--debug'"
+          file, Arguments.OfArgs rest
       | a ->
         Expect.isGreaterThanOrEqual a.Length 2 "Expected mono arguments"
         file, args
