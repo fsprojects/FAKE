b1b37b92552b157335ba6dc3c550f78ca1c2a1eb
Matthias Dittrich <matthi.d@gmail.com>
2018-10-13 01:09:58 +0200

    documentation

--- src/app/Fake.Core.Process/CreateProcessExt.fs
+++ src/app/Fake.Core.Process/CreateProcessExt.fs
@@ -1,9 +1,19 @@
 namespace Fake.Core
 
+/// Some extensions for the `CreateProcess` module, opened automatically (use add `open Fake.Core`)
 [<AutoOpen>]
 module CreateProcessExt =
+    /// Extensions to [`CreateProcess`](apidocs/v5/fake-core-createprocess.html).
     module CreateProcess =
         /// Ensures the executable is run with the full framework. On non-windows platforms that means running the tool by invoking 'mono'.
+        /// 
+        /// ### Example
+        /// 
+        ///     Command.RawCommand("file.exe", Arguments.OfArgs ["arg1"; "arg2"])
+        ///     |> CreateProcess.fromCommand
+        ///     |> CreateProcess.withFramework // start with mono if needed.
+        ///     |> Proc.run
+        ///     |> ignore
         let withFramework (c:CreateProcess<_>) =
             match Environment.isWindows, c.Command, Process.monoPath with
             | false, RawCommand(file, args), Some monoPath when file.ToLowerInvariant().EndsWith(".exe") ->
