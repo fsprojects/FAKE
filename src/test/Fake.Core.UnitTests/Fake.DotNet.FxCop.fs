a03645fb09a84eb49dea7299821af8e2d94b31cb
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-10-26 18:26:57 +0100

    A failing unit test

---
+++ src/test/Fake.Core.UnitTests/Fake.DotNet.FxCop.fs
@@ -0,0 +1,13 @@
+﻿module Fake.DotNet.FxCop
+
+open Fake.Core
+open Fake.DotNet
+open Expecto
+
+[<Tests>]
+let tests =
+  testList "Fake.DotNet.FxCop.Tests" [
+    testCase "Test that we can run a test" <| fun _ ->
+      let p = FxCop.Params.Create()
+      Expect.isFalse (p.IncludeSummaryReport) "This test should fail"
+  ]
