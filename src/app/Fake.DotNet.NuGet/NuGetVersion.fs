096ee3b7f2dc68d9f792a277f1683a8efaa6ed42
Matthias Dittrich <matthi.d@gmail.com>
2019-04-28 16:27:36 +0200

    properly return latest

--- src/app/Fake.DotNet.NuGet/NuGetVersion.fs
+++ src/app/Fake.DotNet.NuGet/NuGetVersion.fs
@@ -67,7 +67,6 @@ open global.NuGet.Protocol
 
 /// Retrieve current NuGet version number
 let getLastNuGetVersion server (packageName:string) : SemVerInfo option =
-
     async {
         let logger = new NuGetLogger()
         let providers = new ResizeArray<Lazy<NuGet.Protocol.Core.Types.INuGetResourceProvider>>()
@@ -80,61 +79,11 @@ let getLastNuGetVersion server (packageName:string) : SemVerInfo option =
         let! searchMetadata =  packageMetadataResource.GetMetadataAsync(packageName, true, true, cacheContext, logger, CancellationToken.None) |> Async.AwaitTask
         return
             searchMetadata
-            |> Seq.tryPick (fun m -> Some <| SemVer.parse m.Identity.Version.OriginalVersion)
+            |> Seq.choose (fun m -> try Some <| SemVer.parse m.Identity.Version.OriginalVersion with _ -> None)
+            |> Seq.sortByDescending (fun v -> v)
+            |> Seq.tryHead
     }
     |> Async.RunSynchronously
-    
-    // maybe enable again after https://github.com/NuGet/NuGetGallery/issues/7085
-    // This enables us to remove Paket.Core...
-    (*
-    let escape = Uri.EscapeDataString
-    let url =
-      sprintf "%s/Search()?$filter=IsLatestVersion&searchTerm='%s'&includePrerelease=false"
-        server packageName
-    let headers, text =
-        Http.getWithHeaders null null (fun rh ->
-            rh.Add("Accept", "application/json, application/xml"))
-            url
-    let hasContentType = headers.ContainsKey "Content-Type"
-    let version =
-      if hasContentType && headers.["Content-Type"] |> List.exists (fun e -> e.Contains "application/json")
-      then
-        let json = JsonConvert.DeserializeObject<NuGetSearchResponse>(text)
-        json.d.results
-        |> Seq.filter (fun i -> i.Id = packageName)
-        |> Seq.sortByDescending (fun i -> i.Published)
-        |> Seq.tryHead
-        |> fun i ->
-            match i with
-            | Some v -> Some (SemVer.parse v.Version)
-            | None -> None
-      else
-        let xml = XDocument.Parse text
-        let xmlns = "http://www.w3.org/2005/Atom"
-        let xmlnsd="http://schemas.microsoft.com/ado/2007/08/dataservices"
-        let xmlnsm="http://schemas.microsoft.com/ado/2007/08/dataservices/metadata"
-        xml.Descendants(XName.Get("entry", xmlns))
-          |> Seq.filter (
-              fun entry ->
-                entry.Elements(XName.Get("title", xmlns))
-                  |> Seq.exists (
-                      fun t ->
-                        t.Attribute(XName.Get "type").Value = "text"
-                        && t.Value = packageName
-                     )
-             )
-          |> Seq.tryHead
-          |> function
-              | Some e ->
-                  e.Descendants(XName.Get ("properties", xmlnsm))
-                  |> fun props ->
-                      props.Elements(XName.Get ("Version", xmlnsd))
-                      |> Seq.tryHead
-                      |> function
-                          | Some n -> Some (SemVer.parse n.Value)
-                          | None -> None
-              | None -> None
-    version*)
 
 
 /// Compute next NuGet version number
