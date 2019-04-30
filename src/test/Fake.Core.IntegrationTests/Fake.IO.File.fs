05dcbb5ee53effdafd3ca087a27a76bfb1d1fbb0
Fangyi Zhou <me@fangyi.io>
2019-04-30 21:33:09 +0100

    Add integration test for Shell.mv
    Co-authored-by: Matthias Dittrich <matthi.d@gmail.com>

--- src/test/Fake.Core.IntegrationTests/Fake.IO.File.fs
+++ src/test/Fake.Core.IntegrationTests/Fake.IO.File.fs
@@ -4,6 +4,22 @@ open Fake.IO
 open System.IO
 open Expecto
 
+type TestDir =
+    { Dir : string }
+    interface System.IDisposable with
+        member x.Dispose() =
+            try
+                Directory.Delete(x.Dir, true)
+            with e ->
+                eprintf "Failed to delete '%s': %O" x.Dir e
+                ()
+
+ let createTestDir () =
+    let testFile = Path.combine (Path.GetTempPath ()) (Path.GetRandomFileName ())
+    Directory.CreateDirectory(testFile)
+        |> ignore<DirectoryInfo>
+    { Dir = testFile }
+
 [<Tests>]
 let tests =
     testList "Fake.IO.FileIntegraionTests" [
@@ -16,4 +32,78 @@ let tests =
             let actualContent = File.readAsString testFile
 
             Expect.equal actualContent "Test" "Unexpected content in test file"
+        testCase "Shell.mv works as expected when src is a file and dst doesnt exist - #2293" <| fun _ ->
+            use d = createTestDir()
+            let f1 = Path.Combine(d.Dir, "1")
+            let f2 = Path.Combine(d.Dir, "2")
+            File.WriteAllText(f1, "test")
+
+            Shell.mv f1 f2
+            Expect.isFalse (File.Exists f1) "f1 should no longer exists"
+            Expect.isTrue (File.Exists f2) "f2 should exist"
+            Expect.equal (File.ReadAllText(f2)) "test" "Unexpected content in test file"
+        testCase "Shell.mv works as expected when src is a file and dst is a file - #2293" <| fun _ ->
+            use d = createTestDir()
+            let f1 = Path.Combine(d.Dir, "1")
+            let f2 = Path.Combine(d.Dir, "2")
+            File.WriteAllText(f1, "test")
+            File.WriteAllText(f2, "other")
+
+            Shell.mv f1 f2
+            Expect.isFalse (File.Exists f1) "f1 should no longer exists"
+            Expect.isTrue (File.Exists f2) "f2 should exist"
+            Expect.equal (File.ReadAllText(f2)) "test" "Unexpected content in test file"
+        testCase "Shell.mv works as expected when src is a file dst is a dir - #2293" <| fun _ ->
+            use d = createTestDir()
+            let f1 = Path.Combine(d.Dir, "1")
+            let d2 = Path.Combine(d.Dir, "2")
+            Directory.CreateDirectory(d2) |> ignore<DirectoryInfo>
+            File.WriteAllText(f1, "test")
+
+            Shell.mv f1 d2
+            let f2 = Path.Combine(d2, "1")
+            Expect.isFalse (File.Exists f1) "f1 should no longer exists"
+            Expect.isTrue (File.Exists f2) "f2 should exist"
+            Expect.equal (File.ReadAllText(f2)) "test" "Unexpected content in test file"
+        testCase "Shell.mv works as expected when src is a dir and dst doesnt exist - #2293" <| fun _ ->
+            use d = createTestDir()
+            let d1 = Path.Combine(d.Dir, "1")
+            let d2 = Path.Combine(d.Dir, "2")
+            let f1 = Path.Combine(d1, "1")
+            let f2 = Path.Combine(d2, "1")
+            Directory.CreateDirectory(d1) |> ignore<DirectoryInfo>
+            File.WriteAllText(f1, "test")
+
+            Shell.mv d1 d2
+            Expect.isFalse (Directory.Exists d1) "d1 should no longer exists"
+            Expect.isTrue (Directory.Exists d2) "d2 should exist"
+            Expect.isFalse (File.Exists f1) "f1 should no longer exists"
+            Expect.isTrue (File.Exists f2) "f2 should exist"
+            Expect.equal (File.ReadAllText(f2)) "test" "Unexpected content in test file"
+        testCase "Shell.mv throws as expected when src is a dir and dst is a file - #2293" <| fun _ ->
+            use d = createTestDir()
+            let d1 = Path.Combine(d.Dir, "1")
+            let f2 = Path.Combine(d.Dir, "2")
+            let f1 = Path.Combine(d1, "1")
+            Directory.CreateDirectory(d1) |> ignore<DirectoryInfo>
+            File.WriteAllText(f1, "test")
+            File.WriteAllText(f2, "other")
+
+            Expect.throws (fun () -> Shell.mv d1 f2) "moving dir to file should throw"
+        testCase "Shell.mv works as expected when src is a dir dst is a dir - #2293" <| fun _ ->
+            use d = createTestDir()
+            let d1 = Path.Combine(d.Dir, "1")
+            let d2 = Path.Combine(d.Dir, "2")
+            let f1 = Path.Combine(d1, "1")
+            let f2 = Path.Combine(Path.Combine(d2, "1"), "1")
+            Directory.CreateDirectory(d1) |> ignore<DirectoryInfo>
+            Directory.CreateDirectory(d2) |> ignore<DirectoryInfo>
+            File.WriteAllText(f1, "test")
+
+            Shell.mv d1 d2
+            Expect.isFalse (Directory.Exists d1) "d1 should no longer exists"
+            Expect.isTrue (Directory.Exists d2) "d2 should exist"
+            Expect.isFalse (File.Exists f1) "f1 should no longer exists"
+            Expect.isTrue (File.Exists f2) "f2 should exist"
+            Expect.equal (File.ReadAllText(f2)) "test" "Unexpected content in test file"
     ]
