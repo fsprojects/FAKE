724780dcc7732fcac39e1d6ee761316c08801f50
Matthias Dittrich <matthi.d@gmail.com>
2019-06-17 00:42:13 +0200

    fix test

--- src/test/Fake.Core.IntegrationTests/SimpleHelloWorldTests.fs
+++ src/test/Fake.Core.IntegrationTests/SimpleHelloWorldTests.fs
@@ -98,6 +98,10 @@ let tests =
         // Check if --write-info <file> works
         let tempFile = Path.GetTempFileName()
         try
+            let tmpPath = scenarioTempPath "core-reference-fake-core-targets"
+            let fakeDir = Path.Combine(tmpPath, ".fake", "reference_fake-targets.fsx")
+            Directory.EnumerateFiles (fakeDir, "reference_fake-targets*")
+                |> Seq.iter File.Delete
             handleAndFormat <| fun () ->
                 directFake (sprintf "run --fsiargs \"--debug:portable --optimize-\" reference_fake-targets.fsx -- --write-info \"%s\"" tempFile) "core-reference-fake-core-targets" |> ignProc
             let json = File.ReadAllText tempFile
