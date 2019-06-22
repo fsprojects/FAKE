ad3ba2a757e946891a04f4f8ffdccba3302dd0bb
Matthias Dittrich <matthi.d@gmail.com>
2019-06-22 17:33:58 +0200

    restore paket & restore targets

--- build.fsx
+++ build.fsx
@@ -145,8 +145,6 @@ match TeamFoundation.Environment.SystemPullRequestIsFork with
 | _ ->
     Trace.traceFAKE "Not setting buildNumber to '%s', because of https://developercommunity.visualstudio.com/content/problem/350007/build-from-github-pr-fork-error-tf400813-the-user-1.html" nugetVersion
 
-do Git.CommandHelper.gitCommand "." "checkout .paket/Paket.Restore.targets"
-
 let dotnetSdk = lazy DotNet.install DotNet.Versions.FromGlobalJson
 let inline dtntWorkDir wd =
     DotNet.Options.lift dotnetSdk.Value
@@ -822,7 +820,7 @@ Target.create "_DotNetPackage" (fun _ ->
     Environment.setEnvironVar "Description" projectDescription
     Environment.setEnvironVar "PackageReleaseNotes" (release.Notes |> String.toLines)
     Environment.setEnvironVar "SourceLinkCreate" "false"
-    Environment.setEnvironVar "PackageTags" "build fake f# automation make scripting"
+    Environment.setEnvironVar "PackageTags" "build;fake;f#"
     Environment.setEnvironVar "PackageIconUrl" "https://raw.githubusercontent.com/fsharp/FAKE/7305422ea912e23c1c5300b23b3d0d7d8ec7d27f/help/content/pics/logo.png"
     Environment.setEnvironVar "PackageProjectUrl" "https://github.com/fsharp/Fake"
     Environment.setEnvironVar "PackageLicenseUrl" "https://github.com/fsharp/FAKE/blob/d86e9b5b8e7ebbb5a3d81c08d2e59518cf9d6da9/License.txt"
