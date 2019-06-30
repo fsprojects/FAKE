10f94d34713d2946334331d149146b008c8dec51
Matthias Dittrich <matthi.d@gmail.com>
2019-06-30 17:00:16 +0200

    Fix warning to fix test

--- integrationtests/core-reference-fake-core-targets/before/otherfile.fs
+++ integrationtests/core-reference-fake-core-targets/before/otherfile.fs
@@ -4,6 +4,6 @@ open Fake.Core
 
 let createTargets () =
 
-    Target.create "OtherFileTarget" (fun p ->
+    Target.create "OtherFileTarget" (fun _ ->
         printfn "Doing Something."
     )
--- integrationtests/core-reference-fake-core-targets/before/otherscript.fsx
+++ integrationtests/core-reference-fake-core-targets/before/otherscript.fsx
@@ -1,6 +1,6 @@
 open Fake.Core
 
 
-Target.create "OtherScriptTarget" (fun p ->
+Target.create "OtherScriptTarget" (fun _ ->
     printfn "Doing Something."
 )
