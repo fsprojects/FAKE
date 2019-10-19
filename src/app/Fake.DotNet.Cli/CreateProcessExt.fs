a2df6ec8a406e59e689d4cb9e326444d53a20c52
Matthias Dittrich <matthi.d@gmail.com>
2019-10-19 17:13:37 +0200

    Add some docs to explain the situation

--- src/app/Fake.DotNet.Cli/CreateProcessExt.fs
+++ src/app/Fake.DotNet.Cli/CreateProcessExt.fs
@@ -1,5 +1,28 @@
 namespace Fake.DotNet
 
+// Here are the different ways to execute a .NET applications:
+// Assume we have a tool called "mytool" we want to call it with arguments "--myargument"
+// Full framework on windows: "mytool.exe --myargument" <- Windows handles finding the correct framework
+// Full framework on unixes : "mono mytool.exe --myargument" <- mono needs to be installed to run a full framework executable
+// .NET Core application (netcoreapp): "dotnet mytool.dll --myargument" <- This is for Framework Dependent Deployments (FDD)
+// .NET Core SCD windows (netcoreapp): "mytool.exe --myargument" <- This is for self contained deployments
+// .NET Core SCD unixes  (netcoreapp): "mytool --myargument" <- This is for self contained deployments
+// .NET global tools (installed via "dotnet tool install -g "MyToolNuGetPackageName"): "<ToolCommandName>.exe --myargument" (or without ".exe" on non-windows)
+// .NET global tools (installed via "dotnet tool install --tool-path .tools "MyToolNuGetPackageName"): like -g global tools (but with '.tools/<ToolCommandName>.exe')
+// .NET local tools (installed via "dotnet new tool-manifest && dotnet tool install "MyToolNuGetPackageName"): "dotnet <ToolCommandName> --myargument"
+
+// For SCD and global tools we can just use "ToolPath"
+// for mono prefix `CreateProcess.withFramework` is already available
+// So the extensions in this file adds similar extensions for local tools and FDDs
+// And some extensions to combine some of the above.
+type DotNetExecution =
+    | FrameworkDependentDeployment
+    | SelfContainedDeployment
+    | FullFramework
+    | GlobalTool
+    | LocalTool
+
+
 /// Information about a dotnet tool
 type DotNetTool =
   { /// Parameters as for DotNet.exec
