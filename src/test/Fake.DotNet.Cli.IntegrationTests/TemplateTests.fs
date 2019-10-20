28b2eb3d461fa75490bc61a51358e9a154f29549
Matthias Dittrich <matthi.d@gmail.com>
2019-10-20 20:44:50 +0200

    disable test

--- src/test/Fake.DotNet.Cli.IntegrationTests/TemplateTests.fs
+++ src/test/Fake.DotNet.Cli.IntegrationTests/TemplateTests.fs
@@ -118,12 +118,22 @@ let tests =
             let buildFile = "build.fsx"
             let dependenciesFile = "paket.dependencies"
 
-            yield test "can install a project-style template" {
-                let tempDir = tempDir()
-                runTemplate tempDir Project File Fake
-                invokeScript tempDir scriptFile "--help" |> shouldSucceed "should invoke help"
-                Expect.isTrue (fileExists tempDir dependenciesFile) "the dependencies file should exist"
-            }
+            // temporarily disable due to (in Azure CI)
+            // stdout:   Retrying 'FindPackagesByIdAsync' for source 'https://api.nuget.org/v3-flatcontainer/dotnet-fake/index.json'.
+            // stdout:   A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
+            // stdout:   Retrying 'FindPackagesByIdAsync' for source 'https://api.nuget.org/v3-flatcontainer/dotnet-fake/index.json'.
+            // stdout:   A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond
+            // stdout: C:\Program Files\dotnet\sdk\2.1.508\NuGet.targets(114,5): error : Failed to retrieve information about 'dotnet-fake' from remote source 'https://api.nuget.org/v3-flatcontainer/dotnet-fake/index.json'. [D:\a\1\s\test\fake-template\emvcxqwy.2wf\build.proj]
+            // stdout: C:\Program Files\dotnet\sdk\2.1.508\NuGet.targets(114,5): error :   A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond [D:\a\1\s\test\fake-template\emvcxqwy.2wf\build.proj]
+            // stdout: 
+            // stdout: D:\a\1\s\test\fake-template\emvcxqwy.2wf>dotnet fake --help 
+            // stderr: Version for package `dotnet-fake` could not be resolved.
+            //yield test "can install a project-style template" {
+            //    let tempDir = tempDir()
+            //    runTemplate tempDir Project File Fake
+            //    invokeScript tempDir scriptFile "--help" |> shouldSucceed "should invoke help"
+            //    Expect.isTrue (fileExists tempDir dependenciesFile) "the dependencies file should exist"
+            //}
 
             yield test "can build with the project-style template" {
                 let tempDir = tempDir()
