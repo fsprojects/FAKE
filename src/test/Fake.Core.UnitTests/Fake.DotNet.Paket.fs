74e98fb0f9912ad948acff63960149710ff256c4
Matthias Dittrich <matthi.d@gmail.com>
2019-10-21 18:53:26 +0200

    fix push, pack and add test, fixes #2413

---
+++ src/test/Fake.Core.UnitTests/Fake.DotNet.Paket.fs
@@ -0,0 +1,46 @@
+module Fake.DotNet.PaketTests
+
+open System.IO
+open Fake.Core
+open Fake.DotNet
+open Fake.DotNet.Testing
+open Fake.Testing
+open Expecto
+
+[<Tests>]
+let tests =
+  testList "Fake.DotNet.Paket.Tests" [
+    testCase "Test restore is not missing, #2411" <| fun _ ->
+      let cp =
+        Paket.createProcess (Paket.StartType.Restore (Paket.PaketRestoreDefaults()))
+      let file, args =
+        match cp.Command with
+        | RawCommand(file, args) -> file, args
+        | _ -> failwithf "expected RawCommand"
+        |> ArgumentHelper.checkIfMono
+      let cmd = args |> Arguments.toStartInfo  
+      Expect.equal file ".paket\\paket.EXE" "Expected paket.exe"
+      Expect.equal cmd "restore" "expected restore argument"
+    testCase "Test pack is not missing, #2411" <| fun _ ->
+      let cp =
+        Paket.createProcess (Paket.StartType.Pack (Paket.PaketPackDefaults()))
+      let file, args =
+        match cp.Command with
+        | RawCommand(file, args) -> file, args
+        | _ -> failwithf "expected RawCommand"
+        |> ArgumentHelper.checkIfMono
+      let cmd = args |> Arguments.toStartInfo  
+      Expect.equal file ".paket\\paket.EXE" "Expected paket.exe"
+      Expect.equal cmd "pack" "expected pack argument"
+    testCase "Test push is not missing, #2411" <| fun _ ->
+      let cp =
+        Paket.createProcess (Paket.StartType.PushFile (Paket.PaketPushDefaults(), "testfile"))
+      let file, args =
+        match cp.Command with
+        | RawCommand(file, args) -> file, args
+        | _ -> failwithf "expected RawCommand"
+        |> ArgumentHelper.checkIfMono
+      let cmd = args |> Arguments.toStartInfo  
+      Expect.equal file ".paket\\paket.EXE" "Expected paket.exe"
+      Expect.equal cmd "push testfile" "expected push command line"
+  ]
