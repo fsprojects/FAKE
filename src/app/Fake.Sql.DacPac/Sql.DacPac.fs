9a3befbc53a2e0dcec0525420309cbed89be8926
Ryan Riley <ryanriley@live.com>
2019-11-19 22:41:42 -0600

    Use Microsoft.SqlServer.DacFx instead of sqlpackage.exe #2251

--- src/app/Fake.Sql.DacPac/Sql.DacPac.fs
+++ src/app/Fake.Sql.DacPac/Sql.DacPac.fs
@@ -4,11 +4,11 @@
 [<RequireQualifiedAccess>]
 module DacPac =
 
-    open Fake.Core
-    open Fake.IO.FileSystemOperators
-    open Fake.IO.Globbing.Operators
-    open System.IO
+    open Microsoft.SqlServer.Dac
     open System
+    open System.Data.SqlClient
+    open System.IO
+    open System.Reflection
 
     /// The type of action to execute.
     type DeployAction =
@@ -21,8 +21,6 @@ module DacPac =
 
     /// Configuration arguments for DacPac deploy
     type DeployDbArgs = {
-        /// The path to SqlPackage.exe.
-        SqlPackageToolPath : string
         /// Type of action to execute. Defaults to Deploy.
         Action : DeployAction
         /// Path to source (path to DACPAC or Connection String).
@@ -42,25 +40,9 @@ module DacPac =
         /// SQLCMD variables
         Variables : (string * string) list }
 
-    let validPaths =
-        let getSqlVersion (path:string) = path.Split '\\' |> Array.item 3 |> int
-        let getVsVersion path = (Path.GetDirectoryName path |> DirectoryInfo).Name |> int
-        let sql = !!(Environment.ProgramFilesX86 </> @"Microsoft SQL Server\**\DAC\bin\SqlPackage.exe") |> Seq.map(fun path -> path, getSqlVersion path)
-        let vs = !!(Environment.ProgramFilesX86 </> @"Microsoft Visual Studio*\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\*\SqlPackage.exe") |> Seq.map(fun path -> path, getVsVersion path)
-        let vs2017 = !!(Environment.ProgramFilesX86 </> @"Microsoft Visual Studio\**\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\*\SqlPackage.exe") |> Seq.map(fun path -> path, getVsVersion path)
-
-        [ sql; vs; vs2017 ]
-        |> List.collect Seq.toList
-        |> List.sortByDescending snd
-        |> List.map fst
-
     /// The default DacPac deployment arguments.
     let DefaultDeploymentArgs = 
-        { SqlPackageToolPath = 
-            validPaths
-            |> List.tryHead
-            |> defaultArg <| ""
-          Action = Deploy
+        { Action = Deploy
           Source = ""
           Destination = ""
           Timeout = 120
@@ -74,46 +56,72 @@ module DacPac =
         /// When creating a new SQL Azure database, specifies the database service tier to use e.g. S2, P1
         let sqlAzureDbSize = "DatabaseServiceObjective"
 
-    let private generateCommandLine args =
-        let action, outputPath =
-            match args with
-            | Deploy -> "Publish", None
-            | Script outputPath -> "Script", Some outputPath
-            | Report outputPath -> "DeployReport", Some outputPath
-        let outputPath = defaultArg(outputPath |> Option.map(sprintf """/OutputPath:"%s" """)) ""
-        action, outputPath
-
     /// Deploys a SQL DacPac or database to another database or DacPac.
     let deployDb setParams =
         let args = setParams DefaultDeploymentArgs
-        let action, outputPath = generateCommandLine args.Action
+        let connectionStringBuilder = SqlConnectionStringBuilder(args.Destination)
+        let package = DacPackage.Load(args.Source)
+        // https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.dac.dacservices?view=sql-dacfx-140.3881.1
+        let dacServices = DacServices(args.Destination)
 
-        let concat parameter =
-            List.map (fun (key, value) -> sprintf "/%s:%s=%s" parameter key value)
-            >> String.concat " "
+        // https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.dac.dacdeployoptions?view=sql-dacfx-140.3881.1
+        let deployOptions =
+            DacDeployOptions(
+               BlockOnPossibleDataLoss=args.BlockOnPossibleDataLoss,
+               DropObjectsNotInSource=args.DropObjectsNotInSource,
+               CommandTimeout=args.Timeout,
+               CreateNewDatabase=args.RecreateDb)
 
-        let additionalParameters = args.AdditionalSqlPackageProperties |> concat "p"
+        let additionalProps = dict args.AdditionalSqlPackageProperties
+        // Find the writeable, public properties.
+        let props =
+            typeof<DacDeployOptions>.GetProperties(BindingFlags.Instance ||| BindingFlags.Public)
+            |> Array.filter (fun p -> p.CanWrite)
 
-        let variables = args.Variables |> concat "v"
+        let boolProps = props |> Array.filter (fun p -> p.PropertyType = typeof<bool>)
+        for bp in boolProps do
+            if additionalProps.ContainsKey(bp.Name) then
+                deployOptions.IncludeCompositeObjects <- bool.Parse additionalProps.[bp.Name]
 
-        if System.String.IsNullOrWhiteSpace args.SqlPackageToolPath then
-            failwith "No SqlPackage.exe filename was given."
+        let objectTypeProps = props |> Array.filter (fun p -> p.PropertyType = typeof<ObjectType[]>)
+        for otp in objectTypeProps do
+            if additionalProps.ContainsKey(otp.Name) then
+                let objectTypes =
+                    additionalProps.[otp.Name].Split(',')
+                    |> Array.map (fun v -> Enum.Parse(typeof<ObjectType>,v) :?> ObjectType)
+                deployOptions.ExcludeObjectTypes <- objectTypes
 
-        if not (File.Exists args.SqlPackageToolPath) then
-            let paths =
-                if validPaths |> List.contains args.SqlPackageToolPath then validPaths
-                else [ args.SqlPackageToolPath ]
-            failwithf "Unable to find a valid instance of SqlPackage.exe. Paths checked were: %A." paths
-    
-        let result =
-            Process.execRaw
-                (fun psi -> { psi with Arguments = sprintf """/Action:%s /SourceFile:"%s" /TargetConnectionString:"%s" %s /p:BlockOnPossibleDataLoss=%b /p:DropObjectsNotInSource=%b /p:CommandTimeout=%d /p:CreateNewDatabase=%b %s %s""" action args.Source args.Destination outputPath args.BlockOnPossibleDataLoss args.DropObjectsNotInSource args.Timeout args.RecreateDb additionalParameters variables; FileName = args.SqlPackageToolPath })
-                TimeSpan.MaxValue
-                true
-                (printfn "SqlPackage error: %s")
-                (printfn "%s")
+        // TODO: support more properties?
 
-        match result with
-        | 0 -> ()
-        | _ -> failwith "Error executing DACPAC deployment. Please see output for error details."
+        // Set SqlCmd variable values.
+        for key, value in args.Variables do
+            deployOptions.SqlCommandVariableValues.Add(key, value)
 
+        try
+            match args.Action with
+            | Deploy ->
+                // https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.dac.dacservices.publish?view=sql-dacfx-140.3881.1
+                let publishOptions = PublishOptions(DeployOptions=deployOptions)
+                dacServices.Publish(package, connectionStringBuilder.InitialCatalog, publishOptions) |> ignore
+            | Script outputPath ->
+                // https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.dac.dacservices.script?view=sql-dacfx-140.3881.1
+                let publishOptions =
+                    PublishOptions(
+                        DeployOptions=deployOptions,
+                        GenerateDeploymentScript=true,
+                        DatabaseScriptPath=outputPath)
+                dacServices.Script(package, connectionStringBuilder.InitialCatalog, publishOptions) |> ignore
+            | Report outputPath ->
+                // https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.dac.dacservices.script?view=sql-dacfx-140.3881.1
+                let publishOptions =
+                    PublishOptions(
+                        DeployOptions=deployOptions,
+                        GenerateDeploymentReport=true)
+                let result = dacServices.Script(package, connectionStringBuilder.InitialCatalog, publishOptions)
+                use file = new IO.FileStream(outputPath, FileMode.Create, FileAccess.Write)
+                use writer = new IO.StreamWriter(file)
+                writer.Write(result.DeploymentReport)
+        with
+        | ex ->
+            eprintfn "SqlPackage error: %s" ex.Message
+            failwith "Error executing DACPAC deployment. Please see output for error details."
