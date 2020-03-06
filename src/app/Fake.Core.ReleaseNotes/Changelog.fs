280000cfdff8537d2a0d3a778e23b900382e6081
Andreas Vilinski <andreas.vilinski@ifm.com>
2020-03-06 16:55:35 +0100

    add changelog format sample

--- src/app/Fake.Core.ReleaseNotes/Changelog.fs
+++ src/app/Fake.Core.ReleaseNotes/Changelog.fs
@@ -1,6 +1,27 @@
 /// Contains helpers which allow to parse Change log text files.
 /// These files have to be in a format as described on http://keepachangelog.com/en/1.1.0/
 ///
+/// ## Format
+///
+/// ```markdown
+/// # Changelog
+///
+/// All notable changes to this project will be documented in this file.
+///
+/// The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+/// and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+///
+/// ## [Unreleased]
+///
+/// ## [0.1.0] - 2020-03-17
+/// First release
+///
+/// ### Added
+/// - This release already has lots of features
+///
+/// [0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
+/// ```
+///
 /// ## Sample
 ///
 ///     let changelogFile = "CHANGELOG.md"
