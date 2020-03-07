e5fe2a352067bf085e1e6a710907b58fa789d435
Chet Husk <chusk3@gmail.com>
2020-03-07 11:52:27 -0600

    revert changes to gitlab runner to restore old, ansi-color behavior

--- src/app/Fake.BuildServer.GitLab/GitLabInternal.fs
+++ src/app/Fake.BuildServer.GitLab/GitLabInternal.fs
@@ -3,9 +3,6 @@ namespace Fake.BuildServer
 
 open Fake.Core
 
-[<assembly:System.Runtime.CompilerServices.InternalsVisibleTo("Fake.Core.UnitTests")>]
-do ()
-
 module internal GitLabInternal =
     let environVar = Environment.environVar
     let getJobId () = environVar "CI_JOB_ID"
