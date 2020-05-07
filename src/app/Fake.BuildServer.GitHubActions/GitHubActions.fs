5d8282f49ced9f7b220716e506cfee34fccaa765
Stefan Reichel <dev@bomret.com>
2020-05-07 13:57:40 +0200

    feat: add build server installer for GitHub Actions

--- src/app/Fake.BuildServer.GitHubActions/GitHubActions.fs
+++ src/app/Fake.BuildServer.GitHubActions/GitHubActions.fs
@@ -59,3 +59,47 @@ module GitHubActions =
         static member HeadRef = Environment.environVar "GITHUB_HEAD_REF"
         /// Only set for forked repositories. The branch of the base repository.
         static member BaseRef = Environment.environVar "GITHUB_BASE_REF"
+
+    /// Implements a TraceListener for GitHub Actions.
+    type internal GitHubActionsTraceListener() =
+        interface ITraceListener with
+            /// Writes the given message to the Console.
+            member __.Write msg =
+                let color = ConsoleWriter.colorMap msg
+                let write = ConsoleWriter.writeAnsiColor
+
+                match msg with
+                | TraceData.ImportantMessage text
+                | TraceData.ErrorMessage text -> write true color true text
+                | TraceData.LogMessage (text, newLine)
+                | TraceData.TraceMessage (text, newLine) -> write false color newLine text
+                | TraceData.OpenTag (tag, descr) ->
+                    match descr with
+                    | Some d -> write false color true (sprintf "Starting %s '%s': %s" tag.Type tag.Name d)
+                    | _ -> write false color true (sprintf "Starting %s '%s'" tag.Type tag.Name)
+                | TraceData.CloseTag (tag, time, state) ->
+                    write false color true (sprintf "Finished (%A) '%s' in %O" state tag.Name time)
+                | TraceData.ImportData (typ, path) -> write false color true (sprintf "Import data '%O': %s" typ path)
+                | TraceData.TestOutput (test, out, err) ->
+                    write false color true (sprintf "Test '%s' output:\n\tOutput: %s\n\tError: %s" test out err)
+                | TraceData.BuildNumber number -> write false color true (sprintf "Build Number: %s" number)
+                | TraceData.TestStatus (test, status) ->
+                    write false color true (sprintf "Test '%s' status: %A" test status)
+                | TraceData.BuildState (state, _) -> write false color true (sprintf "Build State: %A" state)
+
+    let defaultTraceListener =
+        GitHubActionsTraceListener() :> ITraceListener
+
+    let detect () = BuildServer.buildServer = GitHubActions
+
+    let install (force: bool) =
+        if not (detect ())
+        then failwithf "Cannot run 'install()' on a non-GitHub Actions environment"
+        if force || not (CoreTracing.areListenersSet ())
+        then CoreTracing.setTraceListeners [ defaultTraceListener ]
+        ()
+
+    let Installer =
+        { new BuildServerInstaller() with
+            member __.Install() = install (false)
+            member __.Detect() = detect () }
