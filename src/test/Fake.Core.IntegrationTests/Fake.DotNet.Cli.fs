b5b4fc9d7c7369ec6d7af0234289145ee95ebd02
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-01-11 16:52:49 +0200

    revert move of build script deps to paket file. They are needed since they use prerelease version. fix in Build group deps. uncomment tests
    (cherry picked from commit cc4e9cbcdba80be804b570a5bdd7f197b09ca7e2)

--- src/test/Fake.Core.IntegrationTests/Fake.DotNet.Cli.fs
+++ src/test/Fake.Core.IntegrationTests/Fake.DotNet.Cli.fs
@@ -9,52 +9,52 @@ open Fake.Core.IntegrationTests.TestHelpers
 [<Tests>]
 let tests =
     testList "Fake.DotNet.CliIntegrationTests" [
-//        testCase "Make sure dotnet installer works in paths with spaces - #2319" <| fun _ ->
-//            use d = createTestDir()
-//            let installerDir = Path.Combine(d.Dir, "Temp Dir")
-//            Directory.create installerDir
-//            let preparedDir = Path.Combine(d.Dir, "Install Dir")
-//            Directory.create preparedDir
-//
-//            let f = DotNet.install (fun option ->
-//                { option with
-//                    InstallerOptions = fun o ->
-//                        { option.InstallerOptions o with
-//                            CustomDownloadDir = Some installerDir }
-//                    ForceInstall = true
-//                    CustomInstallDir = Some preparedDir
-//                    Channel = DotNet.CliChannel.Current
-//                    Version = DotNet.CliVersion.Latest })
-//
-//            let opts = f (DotNet.Options.Create())
-//            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
-//            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
-//        testCase "Make sure dotnet installer can install into path with spaces - #2319" <| fun _ ->
-//            use d = createTestDir()
-//            let preparedDir = Path.Combine(d.Dir, "Install Dir")
-//
-//            let f = DotNet.install (fun option ->
-//                { option with
-//                    ForceInstall = true
-//                    CustomInstallDir = Some preparedDir
-//                    Channel = DotNet.CliChannel.Current
-//                    Version = DotNet.CliVersion.Latest })
-//
-//            let opts = f (DotNet.Options.Create())
-//            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
-//            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
-//        testCase "Make sure dotnet installer works without spaces - #2319" <| fun _ ->
-//            use d = createTestDir()
-//            let preparedDir = Path.Combine(d.Dir, "InstallDir")
-//
-//            let f = DotNet.install (fun option ->
-//                { option with
-//                    ForceInstall = true
-//                    Channel = DotNet.CliChannel.Current
-//                    CustomInstallDir = Some preparedDir
-//                    Version = DotNet.CliVersion.Latest })
-//
-//            let opts = f (DotNet.Options.Create())
-//            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
-//            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
+        testCase "Make sure dotnet installer works in paths with spaces - #2319" <| fun _ ->
+            use d = createTestDir()
+            let installerDir = Path.Combine(d.Dir, "Temp Dir")
+            Directory.create installerDir
+            let preparedDir = Path.Combine(d.Dir, "Install Dir")
+            Directory.create preparedDir
+
+            let f = DotNet.install (fun option ->
+                { option with
+                    InstallerOptions = fun o ->
+                        { option.InstallerOptions o with
+                            CustomDownloadDir = Some installerDir }
+                    ForceInstall = true
+                    CustomInstallDir = Some preparedDir
+                    Channel = DotNet.CliChannel.Current
+                    Version = DotNet.CliVersion.Latest })
+
+            let opts = f (DotNet.Options.Create())
+            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
+            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
+        testCase "Make sure dotnet installer can install into path with spaces - #2319" <| fun _ ->
+            use d = createTestDir()
+            let preparedDir = Path.Combine(d.Dir, "Install Dir")
+
+            let f = DotNet.install (fun option ->
+                { option with
+                    ForceInstall = true
+                    CustomInstallDir = Some preparedDir
+                    Channel = DotNet.CliChannel.Current
+                    Version = DotNet.CliVersion.Latest })
+
+            let opts = f (DotNet.Options.Create())
+            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
+            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
+        testCase "Make sure dotnet installer works without spaces - #2319" <| fun _ ->
+            use d = createTestDir()
+            let preparedDir = Path.Combine(d.Dir, "InstallDir")
+
+            let f = DotNet.install (fun option ->
+                { option with
+                    ForceInstall = true
+                    Channel = DotNet.CliChannel.Current
+                    CustomInstallDir = Some preparedDir
+                    Version = DotNet.CliVersion.Latest })
+
+            let opts = f (DotNet.Options.Create())
+            Expect.isTrue (File.Exists opts.DotNetCliPath) "Expected dotnet executable to exist"
+            Expect.stringStarts opts.DotNetCliPath preparedDir "Expected dotnet cli to start with prepared directory"
     ]
