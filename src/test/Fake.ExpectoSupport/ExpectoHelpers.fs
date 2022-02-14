8de4404bfad8c095ffd99fe3c4a27c2aa0b36c40
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-02-14 21:16:21 +0200

    fix build hanging by reverting back to custom FAKE printer run build on v6 branch
    (cherry picked from commit 46a8a67dad8c775bd880ead8bbc6a02d762ed7e4)

--- src/test/Fake.ExpectoSupport/ExpectoHelpers.fs
+++ src/test/Fake.ExpectoSupport/ExpectoHelpers.fs
@@ -3,8 +3,97 @@
 open System
 open System.Threading
 open System.Threading.Tasks
+open Expecto.Impl
+open Expecto.Logging
 
 module ExpectoHelpers =
+
+    let inline internal commaString (i:int) = i.ToString("#,##0")
+
+    let mutable logger = Log.create "ExpectoFake"
+    let literateConsoleTarget = LiterateConsoleTarget([|"ExpectoFake"|], ExpectoConfig.defaultConfig.verbosity)
+
+    // because of https://github.com/haf/expecto/issues/367
+    let fakeDefaultPrinter : TestPrinters =
+      { beforeRun = fun _tests ->
+          printfn "EXPECTO? Running tests..."
+          async.Zero()
+
+        beforeEach = fun n ->
+          printfn $"{n} starting..."
+          async.Zero()
+
+        info = fun s ->
+          printfn $"{s}"
+          async.Zero()
+
+        passed = fun n d ->
+          printfn $"{n} passed in {d}."
+          async.Zero()
+
+        ignored = fun n m ->
+          printfn $"{n} was ignored. {m}"
+          async.Zero()
+
+        failed = fun n m d ->
+          printfn $"{n} failed in {d}. {m}"
+          async.Zero()
+
+        exn = fun n e d ->
+          printfn $"{n} errored in {d}: {e}"
+          async.Zero()
+
+        summary = fun _config summary ->
+          let splitSign = _config.joinWith.asString
+          let spirit =
+            if summary.successful then "Success!" else String.Empty
+          let commonAncestor =
+            let rec loop (ancestor: string) (descendants : string list) =
+              match descendants with
+              | [] -> ancestor
+              | hd::tl when hd.StartsWith(ancestor)->
+                loop ancestor tl
+              | _ ->
+                if ancestor.Contains(splitSign) then
+                  loop (ancestor.Substring(0, ancestor.LastIndexOf splitSign)) descendants
+                else
+                  "miscellaneous"
+
+            let parentNames =
+              summary.results
+              |> List.map (fun (flatTest, _)  ->
+                if flatTest.name.Length > 1 then
+                  let size = flatTest.name.Length - 1
+                  _config.joinWith.format flatTest.name.[0..size]
+                else
+                  _config.joinWith.format flatTest.name )
+
+            match parentNames with
+            | [x] -> x
+            | hd::tl ->
+              loop hd tl
+            | _ -> "miscellaneous" //we can't get here
+
+          printfn "EXPECTO! %s tests run in %O for %s – %s passed, %s ignored, %s failed, %s errored. %s"
+            (List.sumBy (fun (_,r: TestSummary) -> if r.result.isIgnored then 0 else r.count) summary.results |> commaString)
+            summary.duration
+            commonAncestor
+            (List.sumBy (fun (_,r) -> r.count) summary.passed |> commaString)
+            (List.sumBy (fun (_,r) -> r.count) summary.ignored |> commaString)
+            (List.sumBy (fun (_,r) -> r.count) summary.failed |> commaString)
+            (List.sumBy (fun (_,r) -> r.count) summary.errored |> commaString)
+            spirit
+          async.Zero()
+      }
+
+    let setPrinter printer (config:Expecto.Impl.ExpectoConfig) =
+        { config with printer = printer }
+    let setFakePrinter (config:Expecto.Impl.ExpectoConfig) =
+        setPrinter fakeDefaultPrinter config
+
+    let appendSummaryHandler summaryPrinter (config:Expecto.Impl.ExpectoConfig) =
+        config.appendSummaryHandler summaryPrinter
+
     let addFilter f (config:Expecto.Impl.ExpectoConfig) =
         { config with
              filter = fun test ->
