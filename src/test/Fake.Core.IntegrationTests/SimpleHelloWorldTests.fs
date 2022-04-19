9182250bfe2aed9ec46551ad36eaf7592dc1ce23
Melvyn Laïly <melvyn.laily@gmail.com>
2022-04-19 21:45:15 +0200

    Fix typos in tests

--- src/test/Fake.Core.IntegrationTests/SimpleHelloWorldTests.fs
+++ src/test/Fake.Core.IntegrationTests/SimpleHelloWorldTests.fs
@@ -88,7 +88,7 @@ let tests =
         let stdOut = String.Join("\n", result.Messages)
         let stdErr = String.Join("\n", result.Errors)
 
-        stdOut.Trim() |> Expect.stringContains "Hello FAKE exected" "Hello FAKE"
+        stdOut.Trim() |> Expect.stringContains "Hello FAKE expected" "Hello FAKE"
         stdErr.Trim() |> Expect.equal "empty expected" ""
 
     testCase "simple failed to compile" <| fun _ ->
