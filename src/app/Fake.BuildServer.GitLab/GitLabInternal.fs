6551d80df41fb628f93a5541864cee97034f756a
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-07-21 18:37:31 +0300

    Preparing BuildServer.GitLab module for FAKE v6.

--- src/app/Fake.BuildServer.GitLab/GitLabInternal.fs
+++
@@ -1,9 +0,0 @@
-﻿/// Contains support for various build servers
-namespace Fake.BuildServer
-
-open Fake.Core
-
-module internal GitLabInternal =
-    let environVar = Environment.environVar
-    let getJobId () = environVar "CI_JOB_ID"
-    
