da9f386aa840fba380ad3c2fbe5a1d58bc4d1a59
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-07-26 19:12:18 +0300

    remove globbing tool tests since they are deprecated and removed.

--- src/test/Fake.Core.IntegrationTests/Fake.IO.Globbing.fs
+++ src/test/Fake.Core.IntegrationTests/Fake.IO.Globbing.fs
@@ -4,47 +4,9 @@ open Fake.IO
 open System.IO
 open Expecto
 open Fake.Core.IntegrationTests.TestHelpers
-open System.Reflection
-open System
-open Fake.IO.Globbing
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 
-[<Tests>]
-let toolsTests = 
-
-  // Sequenced because otherwise folders conflict with the globbing pattern '!! "./**"' in the impl.
-  testSequencedGroup "Find tool paths" <|
-    testList "Fake.Core.Globbing.Tools.Tests" [
-      testCase "Test try find tool folder in sub path" <| fun _ ->
-        use testDir = createTestDirInCurrent()
-        let folder = testDir.Dir
-
-        let filepath = folder </> "sometool"
-        File.create filepath |> ignore
-
-        Tools.tryFindToolFolderInSubPath "sometool"
-        |> Flip.Expect.equal "Expected tools folder to be found" (Some folder)
-
-      testCase "Test cannot find tool folder in sub path" <| fun _ ->
-        Tools.tryFindToolFolderInSubPath "SomeMissingTool"
-        |> Flip.Expect.isNone "Expected tools folder not to be found"
-
-      testCase "Test find tool folder in sub path" <| fun _ ->
-        use testDir = createTestDirInCurrent()
-        let folder = testDir.Dir
-
-        let filepath = folder </> "sometool"
-        File.create filepath |> ignore
-
-        Tools.findToolFolderInSubPath "sometool" "defaultToolsPath"
-        |> Flip.Expect.equal "Expected tools folder to be found" folder
-
-      testCase "Test cannot find tool folder in sub path returns default" <| fun _ ->
-        Tools.findToolFolderInSubPath "SomeMissingTool" "defaultpath"
-        |> Flip.Expect.equal "Expected default path to be returned" "defaultpath"
-    ]
-    
 [<Tests>]
 let tests = 
     testList "Fake.Core.Globbing.IntegrationTests" [
