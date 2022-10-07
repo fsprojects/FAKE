d618de9af615165bdaa9ac5f29e2a28984987578
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-10-07 15:29:05 +0300

    UI styles and font

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./**.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            'fake': [
+                'Inter var',
+                'ui-sans-serif',
+                'system-ui',
+                '-apple-system',
+                'BlinkMacSystemFont','Segoe UI'
+                ,'Roboto',
+                'Helvetica Neue',
+                'Arial',
+                'Noto Sans',
+                'sans-serif',
+                'Apple Color Emoji',
+                'Segoe UI Emoji',
+                'Segoe UI Symbol',
+                'Noto Color Emoji'
+            ],
+          },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
