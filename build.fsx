2fdafd689255369472bd5f4b64ad3046389d547f
Yazeed Obaid <yazeeds.obaid@gmail.com>
2022-11-17 20:21:28 +0200

    build script tweaks
    Website links fix and remove version bump due to security on the master branch for GitHub release

--- build.fsx
+++ build.fsx
@@ -573,7 +573,7 @@ Target.create "GenerateDocs" (fun _ ->
         // renaming node_modules directory so that fsdocs skip it when generating site.
         Directory.Move("./docs/node_modules", "./docs/.node_modules")
 
-        let command = sprintf "build --clean --input ./docs --saveimages --properties Configuration=release --parameters root %s fsdocs-logo-src %s fsdocs-fake-version %s" docsDomain (docsDomain @@ "content/img/logo.svg") simpleVersion
+        let command = sprintf "build --clean --input ./docs --saveimages --properties Configuration=release --parameters fsdocs-logo-src %s fsdocs-fake-version %s" (docsDomain @@ "content/img/logo.svg") simpleVersion
         DotNet.exec id "fsdocs" command |> ignore
 
         // Fsdocs.build (fun p -> { p with
@@ -624,7 +624,7 @@ Target.create "HostDocs" (fun _ ->
         // renaming node_modules directory so that fsdocs skip it when generating site.
         Directory.Move("./docs/node_modules", "./docs/.node_modules")
 
-        let command = sprintf "watch --input ./docs --saveimages --properties Configuration=release --parameters root %s fsdocs-logo-src %s fsdocs-fake-version %s" docsDomain (docsDomain @@ "content/img/logo.svg") simpleVersion
+        let command = sprintf "watch --input ./docs --saveimages --properties Configuration=release --parameters fsdocs-logo-src %s fsdocs-fake-version %s" (docsDomain @@ "content/img/logo.svg") simpleVersion
         DotNet.exec id "fsdocs" command |> ignore
 
         // Fsdocs.watch id
@@ -1011,10 +1011,11 @@ Target.create "GitHubRelease" (fun _ ->
         Git.CommandHelper.directRunGitCommandAndFail gitDirectory "config user.email matthi.d@gmail.com"
         Git.CommandHelper.directRunGitCommandAndFail gitDirectory "config user.name \"Matthias Dittrich\""
 
-    Git.Staging.stageAll gitDirectory
-    Git.Commit.exec gitDirectory (sprintf "Bump version to %s" simpleVersion)
-    let branch = Git.Information.getBranchName gitDirectory
-    Git.Branches.pushBranch gitDirectory "origin" branch
+    // Git.Staging.stageAll gitDirectory
+    // Git.Commit.exec gitDirectory (sprintf "Bump version to %s" simpleVersion)
+    // let branch = "bump-version-to-" + simpleVersion
+    // Git.Branches.checkoutNewBranch gitDirectory "origin" branch
+    // Git.Branches.pushBranch gitDirectory "origin" branch
 
     Git.Branches.tag gitDirectory simpleVersion
     Git.Branches.pushTag gitDirectory url simpleVersion
