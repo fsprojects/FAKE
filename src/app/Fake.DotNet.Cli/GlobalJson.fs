0b68c7dd0f5bd052f4d7350be20c85546171d7a6
Richard Webb <richard.webb@helpsystems.com>
2024-09-19 20:00:49 +0100

    Bang some things into a different shape

---
+++ src/app/Fake.DotNet.Cli/GlobalJson.fs
@@ -0,0 +1,60 @@
+﻿module internal GlobalJson
+
+open System.IO
+open Newtonsoft.Json.Linq
+
+/// <summary>
+/// Tries to get the DotNet SDK from the global.json, starts searching in the given directory.
+/// Returns None if global.json is not found
+/// </summary>
+///
+/// <param name="startDir">The directory to start search from</param>
+let internal tryGetSDKVersionFromGlobalJsonDir startDir : string option =
+    let globalJsonPaths rootDir =
+        let rec loop (dir: DirectoryInfo) =
+            seq {
+                match dir.GetFiles "global.json" with
+                | [| json |] -> yield json
+                | _ -> ()
+
+                if not (isNull dir.Parent) then
+                    yield! loop dir.Parent
+            }
+
+        loop (DirectoryInfo rootDir)
+
+    match Seq.tryHead (globalJsonPaths startDir) with
+    | None -> None
+    | Some globalJson ->
+        try
+            let content = File.ReadAllText globalJson.FullName
+            let json = JObject.Parse content
+            let sdk = json.Item("sdk") :?> JObject
+
+            match sdk.Property("version") with
+            | null -> None
+            | version -> Some(version.Value.ToString())
+        with exn ->
+            failwithf "Could not parse `sdk.version` from global.json at '%s': %s" globalJson.FullName exn.Message
+
+
+/// <summary>
+/// Gets the DotNet SDK from the global.json, starts searching in the given directory.
+/// </summary>
+let internal getSDKVersionFromGlobalJsonDir startDir : string =
+    tryGetSDKVersionFromGlobalJsonDir startDir
+    |> function
+        | Some version -> version
+        | None -> failwithf "global.json not found"
+
+/// <summary>
+/// Tries the DotNet SDK from the global.json. This file can exist in the working
+/// directory or any of the parent directories Returns None if global.json is not found
+/// </summary>
+let tryGetSDKVersionFromGlobalJson () : string option = tryGetSDKVersionFromGlobalJsonDir "."
+
+/// <summary>
+/// Gets the DotNet SDK from the global.json. This file can exist in the working
+/// directory or any of the parent directories
+/// </summary>
+let getSDKVersionFromGlobalJson () : string = getSDKVersionFromGlobalJsonDir "."
