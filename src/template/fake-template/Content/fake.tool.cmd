5978274975be181c47d8a2ff91666bb4eb254512
Tuomas Hietanen <thorium@iki.fi>
2024-09-23 15:42:09 +0100

    Build with .NET 8.0:
    Change global.json .NET 6 to 8
    Search and replace targetframeworks from fsproj files to add net8.0
    Add net8.0 to paket.dependencies
    dotnet paket install to find .NET8 compatible dependencies
    Expecto had to be hardcoded for now, because some tests are running on netstandard2.0 library (hopefully we can update this separately later)
    MSBuild.StructuredLogger problem: DisableInternalBinLog = true had to be added to build.fsx NuGet commands (hopefully we can update this separately later)
    A few places of code had new overrides so had to explicitly type to strings
    SdkAssemblyResolver to default .NET 8 as well
    Readme update
    GitHub pipeline configs: Add .NET 8 install.

--- src/template/fake-template/Content/fake.tool.cmd
+++ src/template/fake-template/Content/fake.tool.cmd
@@ -2,9 +2,9 @@ SET TOOL_PATH=(ToolPath)
 
 IF NOT EXIST "%TOOL_PATH%\fake.exe" (
   rem #if (version == "latest")
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH%
+  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --prerelease --allow-downgrade
   rem #else 
-  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --version (version)
+  dotnet tool install fake-cli --tool-path ./%TOOL_PATH% --version (version) --allow-downgrade
   rem #endif
 )
 
--- src/template/fake-template/Content/fake.tool.sh
+++ src/template/fake-template/Content/fake.tool.sh
@@ -23,9 +23,9 @@ FAKE="$TOOL_PATH"/fake
 if ! [ -e "$FAKE" ]
 then
 #if (version == "latest")
-  dotnet tool install fake-cli --tool-path "$TOOL_PATH"
+  dotnet tool install fake-cli --tool-path "$TOOL_PATH" --prerelease --allow-downgrade
 #else
-  dotnet tool install fake-cli --tool-path "$TOOL_PATH" --version (version)
+  dotnet tool install fake-cli --tool-path "$TOOL_PATH" --version (version) --allow-downgrade
 #endif
 fi
 "$FAKE" "$@"
